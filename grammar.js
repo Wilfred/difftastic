7f9e652c2e6123f209c4951f086de7024f6e5f98
Rob Rix <rob.rix@github.com>
2016-02-18 10:25:03 -0500

    Define variables.

--- grammar.js
+++ grammar.js
@@ -2,6 +2,8 @@ module.exports = grammar({
   name: 'ruby',
 
   rules: {
+	_variable: $ => choice($.identifier , 'nil', 'self'),
+
 	identifier: $ => seq(rep(choice('@', '$')), /[a-zA-Z_][a-zA-Z0-9_]*/),
 
 	_terminator: $ => choice('\n', ';'),
