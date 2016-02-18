bed33c9f3b8e360a0cf2a05934b6fab5f519e03b
Rob Rix <rob.rix@github.com>
2016-02-18 10:25:49 -0500

    Define the program rule.

--- grammar.js
+++ grammar.js
@@ -2,6 +2,8 @@ module.exports = grammar({
   name: 'ruby',
 
   rules: {
+    program: $ => $._compound_statement,
+
 	_compound_statement: $ => seq($._statement, rep(seq($._terminator, $._expression)), optional($._terminator)),
 
 	_statement: $ => choice($._expression),
