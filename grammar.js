b16c008c5088aeb6105b6f10d15a133d1f04a3ed
Rob Rix <rob.rix@github.com>
2016-02-18 10:25:09 -0500

    Define primary expressions.

--- grammar.js
+++ grammar.js
@@ -2,6 +2,8 @@ module.exports = grammar({
   name: 'ruby',
 
   rules: {
+	_primary: $ => choice($._variable),
+
 	_variable: $ => choice($.identifier , 'nil', 'self'),
 
 	identifier: $ => seq(rep(choice('@', '$')), /[a-zA-Z_][a-zA-Z0-9_]*/),
