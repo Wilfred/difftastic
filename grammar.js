94f6f3f1b9dc9a6b3b69968c99b93f42c4a6d17d
Rob Rix <rob.rix@github.com>
2016-02-19 11:36:48 -0500

    Name the else block.

--- grammar.js
+++ grammar.js
@@ -56,18 +56,18 @@ module.exports = grammar({
       seq($._terminator, sep($._statement, $._terminator), "end")
     ),
     _do_block: $ => seq("do", sep($._statement, $._terminator), "end"),
+
     then_block: $ => seq(choice("then", $._terminator), sep($._statement, $._terminator)),
-    _then_else_block: $ => seq($.then_block, optional(seq("else", sep($._statement, $._terminator))), "end"),
+    else_block: $ => seq("else", sep($._statement, $._terminator)),
+
+    _then_else_block: $ => seq($.then_block, optional($.else_block), "end"),
     _then_elsif_else_block: $ => seq(
       $.then_block,
       repeat(seq(
         "elsif", $.condition, choice("then", $._terminator),
         sep($._statement, $._terminator)
       )),
-      optional(seq(
-        "else",
-        sep($._statement, $._terminator)
-      )),
+      optional($.else_block),
       "end"
     ),
 
