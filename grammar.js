df8b3ad77152911bbba08e7d90f091679a8f33e1
Rob Rix <rob.rix@github.com>
2016-02-21 17:26:47 -0500

    Use regexBody to define regex.

--- grammar.js
+++ grammar.js
@@ -299,16 +299,7 @@ module.exports = grammar({
     _array_items: $ => optional(seq($._expression, optional(seq(',', $._array_items)))),
 
     // via https://github.com/tree-sitter/tree-sitter-javascript/blob/31d8b3de9f839057d46c304982b9c245b987bf38/grammar.js#L417-L426
-    regex: $ => token(seq(
-      '/',
-      repeat(choice(
-        seq('[', /[^\]\n]*/, ']'), // square-bracket-delimited character class
-        seq('\\', /./),            // escaped character
-        /[^/\\\[\n]/               // any character besides '[', '\', '/', '\n'
-      )),
-      '/',
-      repeat(/a-z/)
-    )),
+    regex: $ => token(regexBody('/', '/')),
 
     _function_name: $ => choice($.identifier, choice.apply(null, operators)),
 
