0da279b72def0436d4ec170b19d4f2e1b16cb788
Rick Winfrey <rewinfrey@github.com>
2016-11-28 10:52:41 -0600

    Use comma_op in place of commaSep

--- grammar.js
+++ grammar.js
@@ -272,7 +272,7 @@ module.exports = grammar({
 
     return_statement: $ => seq(
       'return',
-      commaSep($._expression),
+      choice($._expression, $.comma_op),
       terminator()
     ),
 
