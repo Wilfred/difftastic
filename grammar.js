2917290ac7cf3d0604bb8a3a1bd62c139132ca18
Timothy Clem <timothy.clem@gmail.com>
2017-01-12 11:16:21 -0800

    Floats don't have to have . in them

--- grammar.js
+++ grammar.js
@@ -27,6 +27,7 @@ const PREC = {
 };
 
 const integerPattern = /0b[01](_?[01])*|0[oO]?[0-7](_?[0-7])*|(0d)?\d(_?\d)*|0x[0-9a-fA-F](_?[0-9a-fA-F])*/;
+const floatPattern = /\d(_?\d)*([eE]\d(_?\d)*)?/;
 const identifierPattern = /[a-zA-Z_][a-zA-Z0-9_]*(\?|\!)?/;
 // Global variables start with $ and can be:
 // - Regex back references (e.g. $&, $', $', and $+)
@@ -489,10 +490,13 @@ module.exports = grammar({
       )
     ),
 
-    integer: $ => (integerPattern),
+    integer: $ => integerPattern,
     // TODO: When we can backtrack, redefine float as regex instead of seq.
-    // float: $ => (/\d(_?\d)*\.\d(_?\d)*([eE]\d(_?\d)*)?/),
-    float: $ => seq(integerPattern, '.' ,/\d(_?\d)*([eE]\d(_?\d)*)?/),
+    // float: $ => (/\d(_?\d)*(\.\d)?(_?\d)*([eE]\d(_?\d)*)?/),
+    float: $ => choice(
+      seq(integerPattern, '.', floatPattern),
+      floatPattern
+    ),
     boolean: $ => token(choice('true', 'false', 'TRUE', 'FALSE')),
     self: $ => 'self',
     nil: $ => token(choice('nil', 'NIL')),
