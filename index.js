f45d12cfa68f249bb073a0dd6c8bb251e3c8c2bd
Max Brunsfeld <maxbrunsfeld@gmail.com>
2017-08-01 15:46:11 -0700

    Tweak identifier naming convention

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require("./build/Release/tree_sitter_javascript_binding");
+try {
+  module.exports = require("./build/Release/tree_sitter_javascript_binding");
+} catch (error) {
+  try {
+    module.exports = require("./build/Debug/tree_sitter_javascript_binding");
+  } catch (_) {
+    throw error
+  }
+}
