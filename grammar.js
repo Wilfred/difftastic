83259f537261905be859943a56a1a295934842e8
joshvera <josh@joshvera.com>
2017-10-31 17:16:04 -0400

    Allow this in parameters

--- grammar.js
+++ grammar.js
@@ -54,14 +54,7 @@ module.exports = grammar(require('tree-sitter-javascript/grammar'), {
     [$._expression, $._primary_type],
     [$._expression, $.generic_type],
     [$._expression, $.predefined_type],
-    [$.this_type, $.this_expression],
-    [$._expression, $.this_expression],
-    [$._expression, $.this_expression, $.this_type],
-    [$._expression, $.this_expression, $.required_parameter, $.this_type],
-    [$._expression, $.this_expression, $._property_name],
-    [$._expression, $.this_expression, $.required_parameter],
-    [$._expression, $.this_expression, $.optional_parameter],
-    [$.this_type, $.required_parameter],
+    [$.this_type, $.this],
     [$.function_type, $.call_signature],
     [$.constructor_type, $.call_signature],
 
@@ -392,7 +385,8 @@ module.exports = grammar(require('tree-sitter-javascript/grammar'), {
         choice(
           $.identifier,
           alias($._reserved_identifier, $.identifier),
-          $._destructuring_pattern
+          $._destructuring_pattern,
+          $.this
         ),
         optional($.type_annotation),
         optional($._initializer)
@@ -402,7 +396,12 @@ module.exports = grammar(require('tree-sitter-javascript/grammar'), {
     optional_parameter: $ => seq(
       optional($.accessibility_modifier),
       optional($.readonly),
-      choice($.identifier, alias($._reserved_identifier, $.identifier), $._destructuring_pattern),
+      choice(
+        $.identifier,
+        alias($._reserved_identifier, $.identifier),
+        $._destructuring_pattern,
+        $.this
+      ),
       '?',
       optional($.type_annotation),
       optional($._initializer)
@@ -604,7 +603,6 @@ module.exports = grammar(require('tree-sitter-javascript/grammar'), {
       'void',
       'import',
       'export',
-      'this',
       previous
     )
   }
