7841a1ca3f8199f370b7d00b3c129782c4fdb621
joshvera <josh@joshvera.com>
2017-11-02 18:52:53 -0400

    rename to typescript scanner

--- src/scanner.c
+++ src/scanner.c
@@ -5,11 +5,11 @@ enum TokenType {
   AUTOMATIC_SEMICOLON,
 };
 
-void *tree_sitter_javascript_external_scanner_create() { return NULL; }
-void tree_sitter_javascript_external_scanner_destroy(void *p) {}
-void tree_sitter_javascript_external_scanner_reset(void *p) {}
-unsigned tree_sitter_javascript_external_scanner_serialize(void *p, char *buffer) { return 0; }
-void tree_sitter_javascript_external_scanner_deserialize(void *p, const char *b, unsigned n) {}
+void *tree_sitter_typescript_external_scanner_create() { return NULL; }
+void tree_sitter_typescript_external_scanner_destroy(void *p) {}
+void tree_sitter_typescript_external_scanner_reset(void *p) {}
+unsigned tree_sitter_typescript_external_scanner_serialize(void *p, char *buffer) { return 0; }
+void tree_sitter_typescript_external_scanner_deserialize(void *p, const char *b, unsigned n) {}
 
 static inline void advance(TSLexer *lexer) { lexer->advance(lexer, false); }
 
@@ -49,7 +49,7 @@ static bool scan_whitespace_and_comments(TSLexer *lexer) {
   }
 }
 
-bool tree_sitter_javascript_external_scanner_scan(void *payload, TSLexer *lexer,
+bool tree_sitter_typescript_external_scanner_scan(void *payload, TSLexer *lexer,
                                                   const bool *valid_symbols) {
   lexer->result_symbol = AUTOMATIC_SEMICOLON;
   lexer->mark_end(lexer);
