3a3347f9b5d754ea8ffdb2a2161c00edacb67742
Ayman Nadeem <aymannadeem@github.com>
2017-11-28 11:10:46 -0800

    start defining grammar for integer literals

--- grammar.js
+++ grammar.js
@@ -2,5 +2,17 @@ module.exports = grammar({
   name: 'java',
 
   rules: {
+    program: $ => repeat($._statement),
+
+
+
+    integer: $ => choice(
+      $.DecimalIntegerLiteral,
+      $.HexIntegerLiteral,
+      $.OctalIntegerLiteral,
+      $.BinaryIntegerLiteral
+    ),
+
+    identifier: $ => /[\a_]\w*/
   }
 });
