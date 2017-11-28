3cd161b43363d816b6d657a1ef47557b1a3e3bbc
Ayman Nadeem <aymannadeem@github.com>
2017-11-28 11:12:21 -0800

    finish setup with npm and create test file to parse

---
+++ src/parser.c
@@ -0,0 +1,136 @@
+#include <tree_sitter/parser.h>
+
+#if defined(__GNUC__) || defined(__clang__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#endif
+
+#define LANGUAGE_VERSION 4
+#define STATE_COUNT 3
+#define SYMBOL_COUNT 3
+#define ALIAS_COUNT 0
+#define TOKEN_COUNT 3
+#define EXTERNAL_TOKEN_COUNT 0
+#define MAX_ALIAS_SEQUENCE_LENGTH 0
+
+enum {
+  sym_literal = 1,
+  sym_identifier = 2,
+};
+
+static const char *ts_symbol_names[] = {
+  [ts_builtin_sym_end] = "END",
+  [sym_literal] = "literal",
+  [sym_identifier] = "identifier",
+};
+
+static const TSSymbolMetadata ts_symbol_metadata[] = {
+  [ts_builtin_sym_end] = {
+    .visible = false,
+    .named = true,
+  },
+  [sym_literal] = {
+    .visible = true,
+    .named = true,
+  },
+  [sym_identifier] = {
+    .visible = true,
+    .named = true,
+  },
+};
+
+static bool ts_lex(TSLexer *lexer, TSStateId state) {
+  START_LEXER();
+  switch (state) {
+    case 0:
+      if (lookahead == 0)
+        ADVANCE(1);
+      if (lookahead == '1')
+        ADVANCE(2);
+      if (lookahead == '\t' ||
+          lookahead == '\n' ||
+          lookahead == '\r' ||
+          lookahead == ' ')
+        SKIP(0);
+      if (('A' <= lookahead && lookahead <= 'Z') ||
+          lookahead == '_' ||
+          ('a' <= lookahead && lookahead <= 'z'))
+        ADVANCE(3);
+      END_STATE();
+    case 1:
+      ACCEPT_TOKEN(ts_builtin_sym_end);
+      END_STATE();
+    case 2:
+      ACCEPT_TOKEN(sym_literal);
+      END_STATE();
+    case 3:
+      ACCEPT_TOKEN(sym_identifier);
+      if (('0' <= lookahead && lookahead <= '9') ||
+          ('A' <= lookahead && lookahead <= 'Z') ||
+          lookahead == '_' ||
+          ('a' <= lookahead && lookahead <= 'z'))
+        ADVANCE(3);
+      END_STATE();
+    case 4:
+      if (lookahead == '1')
+        ADVANCE(2);
+      if (lookahead == '\t' ||
+          lookahead == '\n' ||
+          lookahead == '\r' ||
+          lookahead == ' ')
+        SKIP(4);
+      END_STATE();
+    default:
+      return false;
+  }
+}
+
+static TSLexMode ts_lex_modes[STATE_COUNT] = {
+  [0] = {.lex_state = 0},
+  [1] = {.lex_state = 4},
+  [2] = {.lex_state = 0},
+};
+
+static uint16_t ts_parse_table[STATE_COUNT][SYMBOL_COUNT] = {
+  [0] = {
+    [ts_builtin_sym_end] = ACTIONS(1),
+    [sym_literal] = ACTIONS(1),
+    [sym_identifier] = ACTIONS(3),
+  },
+  [1] = {
+    [sym_literal] = ACTIONS(5),
+  },
+  [2] = {
+    [ts_builtin_sym_end] = ACTIONS(7),
+  },
+};
+
+static TSParseActionEntry ts_parse_actions[] = {
+  [0] = {.count = 0, .reusable = false, .depends_on_lookahead = false},
+  [1] = {.count = 1, .reusable = true, .depends_on_lookahead = false}, RECOVER(),
+  [3] = {.count = 1, .reusable = true, .depends_on_lookahead = true}, RECOVER(),
+  [5] = {.count = 1, .reusable = true, .depends_on_lookahead = false}, SHIFT(2),
+  [7] = {.count = 1, .reusable = true, .depends_on_lookahead = false}, ACCEPT_INPUT(),
+};
+
+#ifdef _WIN32
+#define extern __declspec(dllexport)
+#endif
+
+extern const TSLanguage *tree_sitter_java() {
+  static TSLanguage language = {
+    .version = LANGUAGE_VERSION,
+    .symbol_count = SYMBOL_COUNT,
+    .alias_count = ALIAS_COUNT,
+    .token_count = TOKEN_COUNT,
+    .symbol_metadata = ts_symbol_metadata,
+    .parse_table = (const unsigned short *)ts_parse_table,
+    .parse_actions = ts_parse_actions,
+    .lex_modes = ts_lex_modes,
+    .symbol_names = ts_symbol_names,
+    .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
+    .lex_fn = ts_lex,
+    .external_token_count = EXTERNAL_TOKEN_COUNT,
+  };
+  return &language;
+}
