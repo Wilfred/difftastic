e77ebcf791d5aab56e8a2cd03fb5619a417291a6
Ayman Nadeem <aymannadeem@github.com>
2017-11-30 12:20:07 -0800

    edit grammar

--- grammar.js
+++ grammar.js
@@ -16,11 +16,13 @@ module.exports = grammar({
     _literal: $ => choice(
       $.integer_literal,
       $.floating_point_literal,
-      $.boolean_literal,
-      $.character_literal,
-      $.string_literal,
-      $.null_literal
-    )
+      // $.boolean_literal,
+      // $.character_literal,
+      // $.string_literal,
+      // $.null_literal
+    ),
+
+    // integer literals
 
     integer_literal: $ => choice(
       $.decimal_integer_literal,
@@ -29,44 +31,61 @@ module.exports = grammar({
       $.binary_integer_literal
     ),
 
-    decimal_integer_literal: $ => repeat1(/[0-9]+_?/),
+    _digits: $ => token(sep1(/[0-9]+/, /_+/)),
 
-    hex_integer_literal: $ => seq(
+    decimal_integer_literal: $ => token($._digits),
+
+    _hex_digits: $ => token(sep1(/[A-Fa-f0-9]+/, '_')),
+
+    hex_integer_literal: $ => token(seq(
         choice('0x', '0X'),
-        repeat1(/_?[A-Fa-f0-9]+/)
-      ),
+        $._hex_digits
+      )),
 
     // how does this not get parsed as regular integers?
-    octal_integer_literal: $ => seq(
-        choice('0o', '0O'),
-        repeat1(/_?[0-7]+/)
-      ),
+    octal_integer_literal: $ => token(seq(
+      choice('0o', '0O'),
+      sep1(/[0-7]+/, '_')
+      )),
 
-    binary_integer_literal: $ => seq(
+    binary_integer_literal: $ => token(seq(
       choice('0b', '0B'),
-      repeat1(/_?[01]+/)
-    ),
+      sep1(/[01]+/, '_')
+    )),
+
+    // floating point literals
 
     floating_point_literal: $ => choice(
-      $.decimal_floating_point_literal
-      $.hexadecimal_floating_point_literal
+      $.decimal_floating_point_literal,
+      $.hex_floating_point_literal
     ),
 
-    decimal_floating_point_literal: $ => seq(
+    decimal_floating_point_literal: $ => token(
       choice(
-        seq(repeat(/[0-9]+_?/), '.', repeat(/[0-9]+_?/), optional(/[eE]/), optional(/[\+-]/), repeat(/[0-9]+_?/)),
-        seq(repeat(/[0-9]+_?/), /[eE]/, optional(/[\+-]/), repeat1(/[0-9]+_?/))
+        seq($._digits, '.', optional($._digits), optional((/[eE]/), optional(/[\+-]/), $._digits), optional(/[fFdD]/)),
+        seq('.', $._digits, optional((/[eE]/), optional(/[\+-]/), $._digits), optional(/[fFdD]/)),
+        seq($._digits, /[eE]/, optional(/[\+-]/), $._digits, optional(/[fFdD]/)),
+        seq($._digits, optional((/[eE]/), optional(/[\+-]/), $._digits), (/[fFdD]/))
       )
     ),
 
-    hexidecimal_floating_point_literal: $ => seq(
+    hex_floating_point_literal: $ => token(seq(
+      choice('0x', '0X'),
       choice(
-        seq(choice('0x', '0X'), repeat(/_?[A-Fa-f0-9]+/), '.', repeat(/_?[A-Fa-f0-9]+/), optional(/[eE]/), optional(/[\+-]/), repeat(/[0-9]+_?/)),
-        seq(choice('0x', '0X'), repeat(/_?[A-Fa-f0-9]+/), /[eE]/, optional(/[\+-]/), repeat(/_?[A-Fa-f0-9]+/))
-      )
-    )
+        seq($._hex_digits, optional('.')),
+        seq(optional($._hex_digits), '.', $._hex_digits)
+      ),
+      /[eE]/,
+      optional(/[+-]/),
+      $._digits,
+      optional(/[fFdD]/)
+    )),
 
     comment: $ => /\/\*.*\*\//,
 
   }
 });
+
+  function sep1 (rule, separator) {
+    return seq(rule, repeat(seq(separator, rule)))
+  }
