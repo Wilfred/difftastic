d7451ef4a0fe4b0e47be7abb64cc3645700e5cb8
Rick Winfrey <rewinfrey@github.com>
2018-01-22 15:28:48 -0800

    Make variable and constructor symbols have right precedence

--- grammar.js
+++ grammar.js
@@ -340,8 +340,8 @@ module.exports = grammar({
       )
     ),
 
-    variable_symbol: $ => seq($._variable_symbol, repeat($._variable_symbol)),
-    constructor_symbol: $ => seq(':', repeat($._constructor_symbol)),
+    variable_symbol: $ => prec.right(seq($._variable_symbol, repeat($._variable_symbol))),
+    constructor_symbol: $ => prec.right(seq(':', repeat($._constructor_symbol))),
 
     type_signature: $ => seq(
       $._identifier,
