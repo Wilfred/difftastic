2addb71c5fdcd8ddda9961b2f3f0c0aa3f2d2d3e
joshvera <josh@joshvera.com>
2018-01-26 10:46:16 -0500

    Stub in a few other functions

--- src/scanner.cc
+++ src/scanner.cc
@@ -1,23 +1,143 @@
 #include <tree_sitter/parser.h>
-#include <wctype.h>
+#include <vector>
+#include <string>
+#include <cwctype>
+
+namespace {
+
+using std::vector;
+using std::string;
 
 enum TokenType {
   AUTOMATIC_SEMICOLON,
 };
 
+struct Literal {
+  enum Type {
+    STRING,
+    SYMBOL,
+    SUBSHELL,
+    REGEX,
+    WORD_LIST
+  };
+
+  Type type;
+  int32_t open_delimiter;
+  int32_t close_delimiter;
+  uint32_t nesting_depth;
+  bool allows_interpolation;
+};
+
+struct Heredoc {
+  Heredoc() : end_word_indentation_allowed(false) {}
+
+  string word;
+  bool end_word_indentation_allowed;
+};
+
 struct Scanner {
+  bool has_leading_whitespace;
+  vector<Literal> literal_stack;
+  vector<Heredoc> open_heredocs;
+
+  Scanner() : has_leading_whitespace(false) {}
+
   void reset() {
     literal_stack.clear();
     open_heredocs.clear();
   }
 
   unsigned serialize(char *buffer) {
+    unsigned i = 0;
 
+    if (literal_stack.size() * 5 + 2 >= TREE_SITTER_SERIALIZATION_BUFFER_SIZE) return 0;
+    buffer[i++] = literal_stack.size();
+    for (
+      vector<Literal>::iterator iter = literal_stack.begin(),
+      end = literal_stack.end();
+      iter != end;
+      ++iter
+    ) {
+      buffer[i++] = iter->type;
+      buffer[i++] = iter->open_delimiter;
+      buffer[i++] = iter->close_delimiter;
+      buffer[i++] = iter->nesting_depth;
+      buffer[i++] = iter->allows_interpolation;
+    }
+
+    buffer[i++] = open_heredocs.size();
+    for (
+      vector<Heredoc>::iterator iter = open_heredocs.begin(),
+      end = open_heredocs.end();
+      iter != end;
+      ++iter
+    ) {
+      if (i + 2 + iter->word.size() >= TREE_SITTER_SERIALIZATION_BUFFER_SIZE) return 0;
+      buffer[i++] = iter->end_word_indentation_allowed;
+      buffer[i++] = iter->word.size();
+      iter->word.copy(&buffer[i], iter->word.size());
+      i += iter->word.size();
+    }
+
+    return i;
+  }
+
+  void deserialize(const char *buffer, unsigned length) {
+    unsigned i = 0;
+    has_leading_whitespace = false;
+    literal_stack.clear();
+    open_heredocs.clear();
+
+    if (length == 0) return;
+
+    uint8_t literal_depth = buffer[i++];
+    for (unsigned j = 0; j < literal_depth; j++) {
+      Literal literal;
+      literal.type = static_cast<Literal::Type>(buffer[i++]);
+      literal.open_delimiter = buffer[i++];
+      literal.close_delimiter = buffer[i++];
+      literal.nesting_depth = buffer[i++];
+      literal.allows_interpolation = buffer[i++];
+      literal_stack.push_back(literal);
+    }
+
+    uint8_t open_heredoc_count = buffer[i++];
+    for (unsigned j = 0; j < open_heredoc_count; j++) {
+      Heredoc heredoc;
+      heredoc.end_word_indentation_allowed = buffer[i++];
+      uint8_t word_length = buffer[i++];
+      heredoc.word.assign(buffer + i, buffer + i + word_length);
+      i += word_length;
+      open_heredocs.push_back(heredoc);
+    }
+
+    // assert(i == length);
+  }
+
+  void skip(TSLexer *lexer) {
+    has_leading_whitespace = true;
+    lexer->advance(lexer, true);
   }
 
   void advance(TSLexer *lexer) {
     lexer->advance(lexer, false);
   }
+
+
+  bool lookahead_is_line_end(TSLexer *lexer) {
+    if (lexer->lookahead == '\n') {
+      return true;
+    } else if (lexer->lookahead == '\r') {
+      skip(lexer);
+      if (lexer->lookahead == '\n') {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+};
 
 }
 
@@ -37,7 +157,7 @@ void tree_sitter_php_external_scanner_deserialize(void *payload, const char *buf
   scanner->deserialize(buffer, length);
 }
 
-void tree_sitter_php_external_scanner_destroy(void *p) {
+void tree_sitter_php_external_scanner_destroy(void *payload) {
   Scanner *scanner = static_cast<Scanner *>(payload);
   delete scanner;
 }
@@ -81,3 +201,4 @@ bool tree_sitter_php_external_scanner_scan(void *payload, TSLexer *lexer,
   return lexer->lookahead == '>';
 }
 
+}
