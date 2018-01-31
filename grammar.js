7ea2c1253b39aead0671d93e83ee47dbad5ba503
Rick Winfrey <rewinfrey@github.com>
2018-01-30 16:27:20 -0800

    Remove unnecessary constraint

--- grammar.js
+++ grammar.js
@@ -37,7 +37,6 @@ module.exports = grammar({
   ],
 
   conflicts: $ => [
-    [$.type_class, $.class],
     [$.qualified_variable_symbol, $.constructor_identifier],
     [$.qualified_variable_identifier, $.constructor_identifier],
     [$.tuple, $.parenthesized_constructor],
