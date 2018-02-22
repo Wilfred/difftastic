33e9435c7b758945a636873bab17cc0d08df08fa
Ayman Nadeem <aymannadeem@github.com>
2018-02-22 09:21:16 -0800

    update tests

--- corpus/declarations.java
+++ corpus/declarations.java
@@ -425,3 +425,22 @@ class Point {
     (normal_class_declaration (identifier)
       (class_body (class_body_declaration (class_member_declaration
         (method_declaration (modifier) (method_header (unann_type (unann_primitive_type (floating_point_type))) (method_declarator (identifier))) (method_body (block (class_instance_creation_expression (unqualified_class_instance_creation_expression (unann_class_or_interface_type (identifier)))))))))))))
+
+===
+variable declaration
+===
+
+class JayZ {
+  public void Beyonce() {
+    int blue_ivy_carter;
+  };
+}
+
+---
+
+(program
+  (class_declaration (normal_class_declaration (identifier)
+    (class_body (class_body_declaration (class_member_declaration
+      (method_declaration (modifier) (method_header (unann_type (unann_primitive_type)) (method_declarator (identifier)))
+        (method_body (block (local_variable_declaration_statement
+          (local_variable_declaration (unann_type (unann_primitive_type (integral_type))) (variable_declarator_list (variable_declarator (variable_declarator_id (identifier))))))))))) (class_body_declaration (class_member_declaration))))))
