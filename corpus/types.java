ef8fef23f04227f8b4ede63b399ebb68eb1def9c
Ayman Nadeem <aymannadeem@github.com>
2018-02-28 10:49:00 -0800

    update expression and type tests

--- corpus/types.java
+++ corpus/types.java
@@ -15,16 +15,58 @@ class Beyonce {
 ---
 
 (program
-  (class_declaration (identifier)
-    (class_body (class_body_declaration (class_member_declaration
-      (method_declaration (method_header (unann_type (unann_primitive_type (integral_type))) (method_declarator (identifier)))
-        (method_body
-          (block 
-            (constant_declaration (unann_type (unann_primitive_type (integral_type))) (variable_declarator_list (variable_declarator (variable_declarator_id (identifier)))))
-            (constant_declaration (unann_type (unann_primitive_type (integral_type))) (variable_declarator_list (variable_declarator (variable_declarator_id (identifier)))))
-            (constant_declaration (unann_type (unann_primitive_type (integral_type))) (variable_declarator_list (variable_declarator (variable_declarator_id (identifier)))))
-            (constant_declaration (unann_type (unann_primitive_type (integral_type))) (variable_declarator_list (variable_declarator (variable_declarator_id (identifier)))))
-            (constant_declaration (unann_type (unann_primitive_type (integral_type))) (variable_declarator_list (variable_declarator (variable_declarator_id (identifier)))))))))))))
+  (class_declaration
+    (identifier)
+      (class_body
+        (method_declaration
+          (method_header
+            (unann_type
+              (unann_primitive_type
+                (integral_type)))
+            (method_declarator
+              (identifier)))
+                (method_body
+                  (block
+                    (constant_declaration
+                      (unann_type
+                        (unann_primitive_type
+                          (integral_type)))
+                      (variable_declarator_list
+                        (variable_declarator
+                          (variable_declarator_id
+                            (identifier)))))
+                    (constant_declaration
+                      (unann_type
+                        (unann_primitive_type
+                          (integral_type)))
+                      (variable_declarator_list
+                        (variable_declarator
+                          (variable_declarator_id
+                            (identifier)))))
+                    (constant_declaration
+                      (unann_type
+                        (unann_primitive_type
+                          (integral_type)))
+                        (variable_declarator_list
+                          (variable_declarator
+                            (variable_declarator_id
+                              (identifier)))))
+                    (constant_declaration
+                        (unann_type
+                          (unann_primitive_type
+                            (integral_type)))
+                        (variable_declarator_list
+                          (variable_declarator
+                            (variable_declarator_id
+                              (identifier)))))
+                    (constant_declaration
+                      (unann_type
+                        (unann_primitive_type
+                          (integral_type)))
+                      (variable_declarator_list
+                        (variable_declarator
+                          (variable_declarator_id
+                            (identifier)))))))))))
 
 ===
 floating point type
@@ -40,16 +82,31 @@ class Beyonce {
 ---
 
 (program
-  (class_declaration (identifier)
-    (class_body
-      (class_body_declaration
-        (class_member_declaration
-          (method_declaration (method_header (unann_type (unann_primitive_type (integral_type))) (method_declarator (identifier)))
-            (method_body
-              (block
-                (constant_declaration
-                  (unann_type (unann_primitive_type (floating_point_type)))
-                    (variable_declarator_list (variable_declarator (variable_declarator_id (identifier)))))
-                (constant_declaration
-                  (unann_type (unann_primitive_type (floating_point_type)))
-                    (variable_declarator_list (variable_declarator (variable_declarator_id (identifier)))))))))))))
+  (class_declaration
+    (identifier)
+      (class_body
+        (method_declaration
+          (method_header
+            (unann_type
+              (unann_primitive_type
+                (integral_type)))
+            (method_declarator
+              (identifier)))
+          (method_body
+            (block
+              (constant_declaration
+                (unann_type
+                  (unann_primitive_type
+                    (floating_point_type)))
+                (variable_declarator_list
+                  (variable_declarator
+                    (variable_declarator_id
+                      (identifier)))))
+              (constant_declaration
+                (unann_type
+                  (unann_primitive_type
+                    (floating_point_type)))
+                (variable_declarator_list
+                  (variable_declarator
+                    (variable_declarator_id
+                      (identifier))))))))))) 
