6a4523004337ce01bcbb31b60ad3043c0e71b1aa
Pieter Goetschalckx <3.14.e.ter@gmail.com>
2018-03-27 14:12:52 +0200

    Use external for nested comments

---
+++ src/scanner.c
@@ -0,0 +1,62 @@
+#include <tree_sitter/parser.h>
+
+enum {
+  COMMENT
+};
+
+void *tree_sitter_ocaml_external_scanner_create() {
+  return malloc(sizeof(int));
+}
+
+void tree_sitter_ocaml_external_scanner_destroy(void *payload) {
+  free(payload);
+}
+
+void tree_sitter_external_extra_tokens_external_scanner_reset(void *payload) {}
+
+unsigned tree_sitter_ocaml_external_scanner_serialize(void *payload, char *buffer) {
+  return true;
+}
+
+void tree_sitter_ocaml_external_scanner_deserialize(void *payload, const char *buffer, unsigned length) {}
+
+bool tree_sitter_ocaml_external_scanner_scan(void *payload, TSLexer *lexer, const bool *whitelist) {
+  int *depth = (int*) payload;
+
+  if (whitelist[COMMENT]) {
+    while (lexer->lookahead == ' ' ||
+           lexer->lookahead == '\t' ||
+           lexer->lookahead == '\n') {
+      lexer->advance(lexer, true);
+    }
+
+    if (lexer->lookahead != '(') return false;
+    lexer->advance(lexer, false);
+    if (lexer->lookahead != '*') return false;
+    lexer->advance(lexer, false);
+
+    *depth = 1;
+
+    for (;;) {
+      if (*depth == 0) {
+        lexer->result_symbol = COMMENT;
+        return true;
+      }
+
+      switch (lexer->lookahead) {
+        case '(':
+          lexer->advance(lexer, false);
+          if (lexer->lookahead == '*') (*depth)++;
+          break;
+        case '*':
+          lexer->advance(lexer, false);
+          if (lexer->lookahead == ')') (*depth)--;
+          break;
+      }
+
+      lexer->advance(lexer, false);
+    }
+  }
+
+  return false;
+}
