b1999f9809678cfe18a0489bf564f073d35f46e2
Ayman Nadeem <aymannadeem@github.com>
2018-07-18 15:11:39 -0400

    restore word

--- grammar.js
+++ grammar.js
@@ -61,6 +61,8 @@ module.exports = grammar({
     [$._expression, $.for_init] // TODO: remove to allow non variable declarations in for_init to parse
   ],
 
+  word: $ => $.identifier,
+
   rules: {
     program: $ => repeat($._statement),
 
