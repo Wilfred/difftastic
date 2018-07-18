026ec27822295a2d4656b04dfbff4b485ddc4f0e
Max Brunsfeld <maxbrunsfeld@gmail.com>
2018-07-18 13:56:09 -0700

    :arrow_up: tree-sitter (new ABI version)

--- src/parser.c
+++ src/parser.c
@@ -5,7 +5,7 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
-#define LANGUAGE_VERSION 8
+#define LANGUAGE_VERSION 9
 #define STATE_COUNT 1983
 #define SYMBOL_COUNT 156
 #define ALIAS_COUNT 1
