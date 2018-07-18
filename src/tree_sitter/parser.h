089bc23c23d4513cce806f625ee1c759667fed23
Max Brunsfeld <maxbrunsfeld@gmail.com>
2018-07-18 13:32:13 -0700

    :arrow_up: tree-sitter (new ABI version)

--- src/tree_sitter/parser.h
+++ src/tree_sitter/parser.h
@@ -25,13 +25,16 @@ typedef struct {
   bool named : 1;
 } TSSymbolMetadata;
 
-typedef struct {
-  void (*advance)(void *, bool);
-  void (*mark_end)(void *);
-  uint32_t (*get_column)(void *);
+typedef struct TSLexer TSLexer;
+
+struct TSLexer {
   int32_t lookahead;
   TSSymbol result_symbol;
-} TSLexer;
+  void (*advance)(TSLexer *, bool);
+  void (*mark_end)(TSLexer *);
+  uint32_t (*get_column)(TSLexer *);
+  bool (*is_at_included_range_start)(TSLexer *);
+};
 
 typedef enum {
   TSParseActionTypeShift,
