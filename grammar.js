961b251c2c24e8e575565fe74986f937bba293c6
PrestonKnopp <prestonknopp@gmail.com>
2019-01-10 23:23:24 -0800

    ported python grammar to gdscript

--- grammar.js
+++ grammar.js
@@ -19,7 +19,10 @@ const PREC = {
 }
 
 module.exports = grammar({
+
   name: 'gdscript',
+
+  word: $ => $.identifier,
 
   extras: $ => [
     $.comment,
@@ -41,14 +44,83 @@ module.exports = grammar({
     $.keyword_identifier,
   ],
 
-  word: $ => $.identifier,
-
   rules: {
+
     source: $ => repeat($._statement),
+
+// -----------------------------------------------------------------------------
+// -                                     Atoms                                 -
+// -----------------------------------------------------------------------------
+
+    identifier: $ => /[a-zA-Z_][a-zA-Z_0-9]*/,
+    type: $ => $.identifier,
+    comment: $ => token(seq('#', /.*/)),
+    _semicolon: $ => ';',
+    true: $ => 'true',
+    false: $ => 'false',
+    null: $ => 'null',
+
+    escape_sequence: $ => token(seq(
+      '\\',
+      choice(
+        /u[a-fA-F\d]{4}/,
+        /U[a-fA-F\d]{8}/,
+        /x[a-fA-F\d]{2}/,
+        /o\d{3}/,
+        /\r\n/,
+        /[^uxo]/
+      )
+    )),
+
+    string: $ => seq(
+      alias($._string_start, '"'),
+      repeat(choice($.escape_sequence, $._string_content)),
+      alias($._string_end, '"')
+    ),
+
+    float: $ => {
+      const digits = repeat1(/[0-9]+_?/);
+      const exponent = seq(/[eE][\+-]?/, digits)
+
+      return token(choice(
+        seq(digits, '.', optional(digits), optional(exponent)),
+        seq(optional(digits), '.', digits, optional(exponent)),
+        seq(digits, exponent)
+      ))
+    },
+
+    integer: $ => token(choice(
+      seq(
+        choice('0x', '0X'),
+        repeat1(/_?[A-Fa-f0-9]+/)
+      ),
+      seq(
+        choice('0o', '0O'),
+        repeat1(/_?[0-7]+/),
+      ),
+      seq(
+        choice('0b', '0B'),
+        repeat1(/_?[0-1]+/),
+      ),
+      repeat1(/[0-9]+_?/)
+    )),
+
+// -----------------------------------------------------------------------------
+// -                                  Statements                               -
+// -----------------------------------------------------------------------------
 
     _statement: $ => choice(
       $._simple_statements,
       $._compound_statement
+    ),
+
+    _suite: $ => choice(
+      $._simple_statements,
+      seq(
+        $._indent,
+        repeat($._statement),
+        $._dedent
+      )
     ),
 
     // Simple statements
@@ -76,19 +148,17 @@ module.exports = grammar({
       seq(commaSep1($._expression), optional(',')),
       $.assignment,
       $.augmented_assignment,
-      $.yield
     ),
 
     return_statement: $ => seq(
       'return',
-      optional($.expression_list)
+      optional($._expression)
     ),
 
     pass_statement: $ => prec.left('pass'),
     break_statement: $ => prec.left('break'),
     continue_statement: $ => prec.left('continue'),
-
-    tool_statement: $ => 'tool'
+    tool_statement: $ => 'tool',
 
     class_name_statement: $ => seq(
       'class_name',
@@ -96,12 +166,14 @@ module.exports = grammar({
       optional(seq(',', /".*"/))
     ),
 
+    dotted_name: $ => sep1($.identifier, '.'),
     extends_statement: $ => seq(
       'extends',
-      $.identifier
+      choice(
+        $.identifier,
+        $.string,
+      )
     ),
-
-    // Compount statements
 
     _compound_statement: $ => choice(
       $.if_statement,
@@ -109,7 +181,6 @@ module.exports = grammar({
       $.while_statement,
       $.function_definition,
       $.class_definition,
-      $.decorated_definition
     ),
 
     if_statement: $ => seq(
@@ -135,69 +206,20 @@ module.exports = grammar({
     ),
 
     for_statement: $ => seq(
-      optional('async'),
       'for',
-      $.variables,
+      $.identifier,
       'in',
-      $.expression_list,
+      $._expression,
       ':',
-      $._suite,
-      optional($.else_clause)
+      $._suite
     ),
 
     while_statement: $ => seq(
       'while',
       $._expression,
       ':',
-      $._suite,
-      optional($.else_clause)
-    ),
-
-    function_definition: $ => seq(
-      optional('async'),
-      'def',
-      $.identifier,
-      $.parameters,
-      optional(
-        seq(
-          '->',
-          $.type
-        )
-      ),
-      ':',
       $._suite
     ),
-
-    parameters: $ => seq(
-      '(',
-      optional($._parameters),
-      ')'
-    ),
-
-    _parameters: $ => seq(
-      commaSep1(choice(
-        $.identifier,
-        $.keyword_identifier,
-        $.typed_parameter,
-        $.default_parameter,
-        $.typed_default_parameter,
-      )),
-      optional(',')
-    ),
-
-    default_parameter: $ => seq(
-      choice($.identifier, $.keyword_identifier),
-      '=',
-      $._expression
-    ),
-
-    typed_default_parameter: $ => prec(PREC.typed_parameter, seq(
-      choice($.identifier, $.keyword_identifier),
-      ':',
-      $.type,
-      '=',
-      $._expression
-    )),
 
     class_definition: $ => seq(
       'class',
@@ -207,60 +229,14 @@ module.exports = grammar({
       $._suite
     ),
 
-    argument_list: $ => seq(
-      '(',
-      optional(commaSep1(
-        choice(
-          $._expression,
-          $.keyword_argument
-        )
-      )),
-      optional(','),
-      ')'
-    ),
+// -----------------------------------------------------------------------------
+// -                                  Expressions                              -
+// -----------------------------------------------------------------------------
 
-    decorated_definition: $ => seq(
-      repeat1($.decorator),
-      choice(
-        $.class_definition,
-        $.function_definition
-      )
-    ),
-
-    decorator: $ => seq(
-      '@',
-      $.dotted_name,
-      optional($.argument_list),
-      $._newline
-    ),
-
-    _suite: $ => choice(
-      $._simple_statements,
-      seq(
-        $._indent,
-        repeat($._statement),
-        $._dedent
-      )
-    ),
-
-    variables: $ => seq(
-      commaSep1($._primary_expression),
-      optional(',')
-    ),
-
-    expression_list: $ => prec.right(seq(
-      commaSep1($._expression),
-      optional(',')
-    )),
-
-    dotted_name: $ => sep1($.identifier, '.'),
-
-    // Expressions
     _expression: $ => choice(
       $.comparison_operator,
       $.not_operator,
       $.boolean_operator,
-      $.await,
       $._primary_expression,
       $.conditional_expression
     ),
@@ -268,14 +244,12 @@ module.exports = grammar({
     _primary_expression: $ => choice(
       $.binary_operator,
       $.identifier,
-      $.keyword_identifier,
       $.string,
-      $.concatenated_string,
       $.integer,
       $.float,
       $.true,
       $.false,
-      $.none,
+      $.null,
       $.unary_operator,
       $.attribute,
       $.subscript,
@@ -285,6 +259,7 @@ module.exports = grammar({
       $.parenthesized_expression
     ),
 
+    // -- Operators
     not_operator: $ => prec(PREC.not, seq('not', $._expression)),
 
     boolean_operator: $ => choice(
@@ -296,11 +271,8 @@ module.exports = grammar({
       prec.left(PREC.plus, seq($._primary_expression, '+', $._primary_expression)),
       prec.left(PREC.plus, seq($._primary_expression, '-', $._primary_expression)),
       prec.left(PREC.times, seq($._primary_expression, '*', $._primary_expression)),
-      prec.left(PREC.times, seq($._primary_expression, '@', $._primary_expression)),
       prec.left(PREC.times, seq($._primary_expression, '/', $._primary_expression)),
       prec.left(PREC.times, seq($._primary_expression, '%', $._primary_expression)),
-      prec.left(PREC.times, seq($._primary_expression, '//', $._primary_expression)),
-      prec.left(PREC.power, seq($._primary_expression, '**', $._primary_expression)),
       prec.left(PREC.bitwise_or, seq($._primary_expression, '|', $._primary_expression)),
       prec.left(PREC.bitwise_and, seq($._primary_expression, '&', $._primary_expression)),
       prec.left(PREC.xor, seq($._primary_expression, '^', $._primary_expression)),
@@ -324,122 +296,25 @@ module.exports = grammar({
           '!=',
           '>=',
           '>',
-          '<>',
           'in',
-          seq('not', 'in'),
           'is',
-          seq('is', 'not')
         ),
         $._primary_expression
       ))
     )),
 
-    assignment: $ => seq(
-      $.expression_list,
-      choice(
-        seq('=', $._right_hand_side),
-        seq(':', $.type),
-        seq(':', $.type, '=', $._right_hand_side)
-      )
-    ),
-
-    augmented_assignment: $ => seq(
-      $.expression_list,
-      choice('+=', '-=', '*=', '/=', '@=', '//=', '%=', '**=', '>>=', '<<=', '&=', '^=', '|='),
-      $._right_hand_side
-    ),
-
-    _right_hand_side: $ => choice(
-      $.expression_list,
-      $.assignment,
-      $.augmented_assignment,
-      $.yield
-    ),
-
-    yield: $ => seq(
-      'yield',
-      choice(
-        seq(
-          'from',
-          $._expression
-        ),
-        optional($.expression_list)
-      )
+    // -- Accessors
+    subscript: $ => seq(
+      $._primary_expression,
+      '[',
+      $._expression,
+      ']'
     ),
 
     attribute: $ => seq(
       $._primary_expression,
       '.',
       $.identifier
-    ),
-
-    subscript: $ => seq(
-      $._primary_expression,
-      '[',
-      commaSep1($._expression),
-      optional(','),
-      ']'
-    ),
-
-    call: $ => prec(PREC.call, seq(
-      $._primary_expression,
-      $.argument_list
-    )),
-
-    typed_parameter: $ => prec(PREC.typed_parameter, seq(
-	  $.identifier,
-      ':',
-      $.type
-    )),
-
-    type: $ => $._expression,
-
-    keyword_argument: $ => seq(
-      choice($.identifier, $.keyword_identifier),
-      '=',
-      $._expression
-    ),
-
-    // Literals
-
-    list: $ => seq(
-      '[',
-      optional(commaSep1($._expression)),
-      optional(','),
-      ']'
-    ),
-
-    dictionary: $ => seq(
-      '{',
-      optional(commaSep1($.pair)),
-      optional(','),
-      '}'
-    ),
-
-    pair: $ => seq(
-      $._expression,
-      ':',
-      $._expression
-    ),
-
-    parenthesized_expression: $ => prec(PREC.parenthesized_expression, seq(
-      '(',
-      choice($._expression, $.yield),
-      ')'
-    )),
-
-    for_in_clause: $ => seq(
-      optional('async'),
-      'for',
-      $.variables,
-      'in',
-      commaSep1($._expression),
-      optional(',')
-    ),
-
-    if_clause: $ => seq(
-      'if',
-      $._expression
     ),
 
     conditional_expression: $ => prec.right(PREC.conditional, seq(
@@ -450,106 +325,148 @@ module.exports = grammar({
       $._expression
     )),
 
-    concatenated_string: $ => seq(
-      $.string,
-      repeat1($.string)
-    ),
-
-    string: $ => seq(
-      alias($._string_start, '"'),
-      repeat(choice($.interpolation, $.escape_sequence, $._string_content)),
-      alias($._string_end, '"')
-    ),
-
-    interpolation: $ => seq(
-      '{',
+    parenthesized_expression: $ => prec(PREC.parenthesized_expression, seq(
+      '(',
       $._expression,
-      optional($.type_conversion),
-      optional($.format_specifier),
+      ')'
+    )),
+
+// -----------------------------------------------------------------------------
+// -                                  Assignment                               -
+// -----------------------------------------------------------------------------
+
+    assignment: $ => seq(
+      choice(
+        seq('var', $.identifier),
+        $._expression
+      ),
+      choice(
+        seq('=', $._expression),
+        seq(':', '=', $._expression),
+        seq(':', $.type),
+        seq(':', $.type, '=', $._expression)
+      )
+    ),
+
+    augmented_assignment: $ => seq(
+      $._expression,
+      choice('+=', '-=', '*=', '/=', '%=', '>>=', '<<=', '&=', '^=', '|='),
+      $._expression
+    ),
+
+// -----------------------------------------------------------------------------
+// -                                 Data Structs                              -
+// -----------------------------------------------------------------------------
+
+    pair: $ => seq(
+      choice(
+        seq(
+          $._expression,
+          ':',
+        ),
+        seq(
+          $.identifier,
+          '=',
+        )),
+      $._expression
+    ),
+
+    dictionary: $ => seq(
+      '{',
+      optional(commaSep1($.pair)),
+      optional(','),
       '}'
     ),
 
-    escape_sequence: $ => token(seq(
-      '\\',
-      choice(
-        /u[a-fA-F\d]{4}/,
-        /U[a-fA-F\d]{8}/,
-        /x[a-fA-F\d]{2}/,
-        /o\d{3}/,
-        /\r\n/,
-        /[^uxo]/
-      )
-    )),
-
-    format_specifier: $ => seq(
-      ':',
-      repeat(choice(
-        /[^{}\n]+/,
-        $.format_expression
-      ))
+    list: $ => seq(
+      '[',
+      optional(commaSep1($._expression)),
+      optional(','),
+      ']'
     ),
 
-    format_expression: $ => seq('{', $._expression, '}'),
+// -----------------------------------------------------------------------------
+// -                              Function Definition                          -
+// -----------------------------------------------------------------------------
 
-    type_conversion: $ => /![a-z]/,
 
-    integer: $ => token(choice(
-      seq(
-        choice('0x', '0X'),
-        repeat1(/_?[A-Fa-f0-9]+/),
-        optional(/[Ll]/)
-      ),
-      seq(
-        choice('0o', '0O'),
-        repeat1(/_?[0-7]+/),
-        optional(/[Ll]/)
-      ),
-      seq(
-        choice('0b', '0B'),
-        repeat1(/_?[0-1]+/),
-        optional(/[Ll]/)
-      ),
-      seq(
-        repeat1(/[0-9]+_?/),
-        choice(
-          optional(/[Ll]/), // long numbers
-          optional(/[jJ]/) // complex numbers
-        )
-      )
+    typed_parameter: $ => prec(PREC.typed_parameter, seq(
+      $.identifier,
+      ':',
+      $.type
     )),
 
-    float: $ => {
-      const digits = repeat1(/[0-9]+_?/);
-      const exponent = seq(/[eE][\+-]?/, digits)
+    default_parameter: $ => seq(
+      $.identifier,
+      '=',
+      $._expression
+    ),
 
-      return token(seq(
-        choice(
-          seq(digits, '.', optional(digits), optional(exponent)),
-          seq(optional(digits), '.', digits, optional(exponent)),
-          seq(digits, exponent)
-        ),
-        optional(choice(/[Ll]/, /[jJ]/))
-      ))
-    },
-
-    identifier: $ => /[a-zA-Zα-ωΑ-Ω_][a-zA-Zα-ωΑ-Ω_0-9]*/,
-
-    keyword_identifier: $ => alias(choice('print', 'exec'), $.identifier),
-
-    true: $ => 'True',
-    false: $ => 'False',
-    none: $ => 'None',
-
-    await: $ => prec(PREC.unary, seq(
-      'await',
+    typed_default_parameter: $ => prec(PREC.typed_parameter, seq(
+      $.identifier,
+      ':',
+      $.type,
+      '=',
       $._expression
     )),
 
-    comment: $ => token(seq('#', /.*/)),
+    _parameters: $ => commaSep1(choice(
+        $.identifier,
+        $.typed_parameter,
+        $.default_parameter,
+        $.typed_default_parameter,
+    )),
 
-    _semicolon: $ => ';'
-  }
+    parameters: $ => seq(
+      '(',
+      optional($._parameters),
+      ')'
+    ),
+
+    return_type: $ => seq(
+      '->',
+      $.type
+    ),
+
+    function_definition: $ => seq(
+      'func',
+      $.identifier,
+      $.parameters,
+      optional($.return_type),
+      ':',
+      $._suite
+    ),
+
+
+// -----------------------------------------------------------------------------
+// -                                 Function Call                             -
+// -----------------------------------------------------------------------------
+
+    argument_list: $ => seq(
+      '(',
+      optional(commaSep1($._expression)),
+      ')'
+    ),
+
+    call: $ => prec(PREC.call, seq(
+      $._primary_expression,
+      $.argument_list
+    )),
+
+
+
+
+
+
+
+
+
+
+
+  } // end rules
+
 })
+
 
 function commaSep1 (rule) {
   return sep1(rule, ',')
