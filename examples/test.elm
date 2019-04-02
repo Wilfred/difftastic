b4419ca5fb49b145656fdabf7f5c2e848cf20f29
Kolja <razze@razze-convertable.fritz.box>
2019-04-02 23:00:03 +0200

    Add more test checkouts

--- examples/test.elm
+++ examples/test.elm
@@ -1,3 +1,16 @@
 
-tests = let f = (\a -> a) in
-            f
+updateReqs router cmds reqs =
+  case cmds of
+
+        Request req ->
+          Process.spawn (Elm.Kernel.Http.toTask router (Platform.sendToApp router) req)
+            |> Task.andThen (\pid ->
+                  case req.tracker of
+                    Nothing ->
+                      updateReqs router otherCmds reqs
+
+                    Just tracker ->
+                      updateReqs router otherCmds (Dict.insert tracker pid reqs)
+              )
+
+
