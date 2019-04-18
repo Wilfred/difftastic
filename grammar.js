903c53461ce5ce3a395b8607414a8db88e1c49a2
Max Brunsfeld <maxbrunsfeld@gmail.com>
2019-04-18 09:16:47 -0700

    Allow escaped newlines in single-line comments
    Fixes tree-sitter/tree-sitter-cpp#26

--- grammar.js
+++ grammar.js
@@ -732,7 +732,7 @@ module.exports = grammar({
 
     // http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
     comment: $ => token(choice(
-      seq('//', /.*/),
+      seq('//', /(\\[.\n]|[^\\\n])*/),
       seq(
         '/*',
         /[^*]*\*+([^/*][^*]*\*+)*/,
