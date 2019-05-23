f43018b8d50c46539d0de6b46cb4d37a9c1ff6ec
Nathan Sobo <nathan@github.com>
2019-05-23 09:56:07 -0700

    Fix require entrypoints
    Co-Authored-By: Antonio Scandurra <as-cii@github.com>

--- typescript/index.js
+++ typescript/index.js
@@ -1,8 +1,8 @@
 try {
-  module.exports = require("./build/Release/tree_sitter_typescript_binding");
+  module.exports = require("../build/Release/tree_sitter_typescript_binding");
 } catch (error) {
   try {
-    module.exports = require("./build/Debug/tree_sitter_typescript_binding");
+    module.exports = require("../build/Debug/tree_sitter_typescript_binding");
   } catch (_) {
     throw error
   }
