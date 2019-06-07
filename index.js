072fb3ec7cc9478d695da437bd434acc6dbcca92
Max Brunsfeld <maxbrunsfeld@gmail.com>
2019-06-07 15:11:47 -0700

    Upgrade to tree-sitter-cli 0.15.3

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-module.exports = require("./build/Release/tree_sitter_rust_binding");
+try {
+  module.exports = require("./build/Release/tree_sitter_rust_binding");
+} catch (error) {
+  try {
+    module.exports = require("./build/Debug/tree_sitter_rust_binding");
+  } catch (_) {
+    throw error
+  }
+}
+
+try {
+  module.exports.nodeTypeInfo = require("./src/node-types.json");
+} catch (_) {}
