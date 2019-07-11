5252a6b56960ab4b8d30ad7a883de48bcf118808
Ellis Berner <eberner@gmail.com>
2019-07-11 11:07:25 -0700

    package.json: expand to include electron 4.0.4 (#108)
    * package.json: expand to include electron 4.0.4
    
    https://github.com/lgeiger/node-abi/blob/master/index.js#L71
    https://github.com/rubyide/vscode-ruby/issues/486
    
    * Prebuild electron 4, 4.0.4, and 5
    
    * :arrow_up: tree-sitter-cli

--- src/binding.cc
+++ src/binding.cc
@@ -10,17 +10,17 @@ namespace {
 
 NAN_METHOD(New) {}
 
-void Init(Handle<Object> exports, Handle<Object> module) {
+void Init(Local<Object> exports, Local<Object> module) {
   Local<FunctionTemplate> tpl = Nan::New<FunctionTemplate>(New);
   tpl->SetClassName(Nan::New("Language").ToLocalChecked());
   tpl->InstanceTemplate()->SetInternalFieldCount(1);
 
-  Local<Function> constructor = tpl->GetFunction();
+  Local<Function> constructor = Nan::GetFunction(tpl).ToLocalChecked();
   Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
   Nan::SetInternalFieldPointer(instance, 0, tree_sitter_ruby());
 
-  instance->Set(Nan::New("name").ToLocalChecked(), Nan::New("ruby").ToLocalChecked());
-  module->Set(Nan::New("exports").ToLocalChecked(), instance);
+  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("ruby").ToLocalChecked());
+  Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
 }
 
 NODE_MODULE(tree_sitter_ruby_binding, Init)
--- src/tree_sitter/parser.h
+++ src/tree_sitter/parser.h
@@ -122,22 +122,26 @@ struct TSLanguage {
 
 #define START_LEXER()           \
   bool result = false;          \
+  bool skip = false;            \
   int32_t lookahead;            \
+  goto start;                   \
   next_state:                   \
+  lexer->advance(lexer, skip);  \
+  start:                        \
+  skip = false;                 \
   lookahead = lexer->lookahead;
 
-#define ADVANCE(state_value)      \
-  {                               \
-    lexer->advance(lexer, false); \
-    state = state_value;          \
-    goto next_state;              \
+#define ADVANCE(state_value) \
+  {                          \
+    state = state_value;     \
+    goto next_state;         \
   }
 
-#define SKIP(state_value)        \
-  {                              \
-    lexer->advance(lexer, true); \
-    state = state_value;         \
-    goto next_state;             \
+#define SKIP(state_value) \
+  {                       \
+    skip = true;          \
+    state = state_value;  \
+    goto next_state;      \
   }
 
 #define ACCEPT_TOKEN(symbol_value)     \
