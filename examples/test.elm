12372eed532f0441d67f0a2f90da2817d8c2e64d
Kolja Lampe <razzeee@gmail.com>
2019-09-01 02:01:41 +0200

    Change when block_comments are set, should now be better for annotations

--- examples/test.elm
+++ examples/test.elm
@@ -1,18 +1,8 @@
+enum key dict =
+  custom key <| \stringList ->
+    case stringList of
+      [str] ->
+        Dict.get str dict
 
-textures_skies_tim_hell_fragment_1 = 
-    [glsl|
-precision mediump float;
-
-varying vec2 vTextureCoord;
-varying vec2 vLightmapCoord;
-varying vec4 vColor;
-uniform sampler2D texture;
-uniform float time;
-void main(void) {
-	vec4 textureColor = texture2D(texture, vTextureCoord.st);
-	vec3 rgb = textureColor.rgb;
-	// alphaGen
-	float alpha = textureColor.a;
-	gl_FragColor = vec4(rgb, alpha);
-}
-    |]
+      _ ->
+        Nothing
