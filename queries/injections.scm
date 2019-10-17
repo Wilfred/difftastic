db4936d9cc37479f82141fad78364e5fa2ee3468
Max Brunsfeld <maxbrunsfeld@gmail.com>
2019-10-17 15:04:35 -0700

    Add queries for syntax highlighting (#12)
    * Add queries for syntax highlighting
    
    * :fire: property sheets

---
+++ queries/injections.scm
@@ -0,0 +1,7 @@
+((script_element
+  (raw_text) @injection.content) @injection.site
+(set! injection.language "javascript"))
+
+((style_element
+  (raw_text) @injection.content) @injection.site
+(set! injection.language "css"))
