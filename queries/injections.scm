ac7bb811f817a4a4d17b58152b4c7cd7f85ca273
Max Brunsfeld <maxbrunsfeld@gmail.com>
2019-10-17 15:04:13 -0700

    Add syntax highlighting queries (#55)
    * Add syntax highlighting queries
    
    * Add to the syntax highlighting query
    
    * :fire: property sheets

---
+++ queries/injections.scm
@@ -0,0 +1,9 @@
+((macro_invocation
+  (token_tree) @injection.content) @injection.site
+ (set! injection.language "rust")
+ (set! injection.include-children))
+
+((macro_rule
+  (token_tree) @injection.content) @injection.site
+ (set! injection.language "rust")
+ (set! injection.include-children))
