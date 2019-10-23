d8ddab8cadb54efb6e12d91e0d55164e5b2087e3
Max Brunsfeld <maxbrunsfeld@gmail.com>
2019-10-23 11:34:12 -0700

    Put jsx highlighting in separate query file
    This allows the main highlight file to be used by the TypeScript
    parser without error, since the TypeScript grammar does not have JSX.

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -68,14 +68,6 @@
   function: (member_expression
     property: (property_identifier) @function.method.call))
 
-; JSX
-;----
-
-(jsx_opening_element (identifier) @tag)
-(jsx_closing_element (identifier) @tag)
-(jsx_self_closing_element (identifier) @tag)
-(jsx_attribute (property_identifier) @attribute)
-
 ; Variables
 ;----------
 
