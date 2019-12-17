565244b6f6d7ed371537d246003ac585b61c53f0
calixteman <calixte.denizet@gmail.com>
2019-12-17 19:40:23 +0100

    Assignment LHS can be a scoped identifier (#68)
    * Assignment LHS can be a scoped identifier
    
    * Bad path in grammar
    
    * Forgot test and parser
    
    * Update for latest tree-sitter-c

--- grammar.js
+++ grammar.js
@@ -756,6 +756,11 @@ module.exports = grammar(C, {
       field('name', $._namespace_identifier)
     )),
 
+    _assignment_left_expression: ($, original) => choice(
+      original,
+      $.scoped_namespace_identifier,
+    ),
+
     operator_name: $ => token(seq(
       'operator',
       choice(
