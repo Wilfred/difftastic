0f6e7bb2f508325f75e9ee097afdc1a713b220e0
Max Brunsfeld <maxbrunsfeld@gmail.com>
2020-01-16 14:22:43 -0800

    Add highlight and injection query

---
+++ test/highlight/literals.php
@@ -0,0 +1,13 @@
+<?php
+
+
+echo <<<OMG
+  something
+OMG
+// <- string
+
+echo true, TRUE, false, FALSE
+//    ^ constant.builtin
+//          ^ constant.builtin
+//               ^ constant.builtin
+//                      ^ constant.builtin
