6222e7a214f47459a5b75b0bc9e883780aa676af
Max Brunsfeld <maxbrunsfeld@gmail.com>
2020-03-02 13:21:26 -0800

    :arrow_up: tree-sitter-cli

--- src/parser.c
+++ src/parser.c
@@ -50675,6 +50675,9 @@ static TSParseActionEntry ts_parse_actions[] = {
   [2956] = {.count = 1, .reusable = true}, SHIFT(899),
 };
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 void *tree_sitter_javascript_external_scanner_create(void);
 void tree_sitter_javascript_external_scanner_destroy(void *);
 bool tree_sitter_javascript_external_scanner_scan(void *, TSLexer *, const bool *);
@@ -50722,3 +50725,6 @@ extern const TSLanguage *tree_sitter_javascript(void) {
   };
   return &language;
 }
+#ifdef __cplusplus
+}
+#endif
