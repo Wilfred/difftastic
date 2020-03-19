Wilfred/difftastic

commit 9201abdff549a91ff32987c8b1dc021034039ada

Allow structured binding declarators in pmore places
Refs atom/language-c#325

grammar.js
@@ -22,8 +22,9 @@ module.exports = grammar(C, {
     [$.scoped_type_identifier, $.scoped_field_identifier],
     [$.comma_expression, $.initializer_list],
     [$._type_specifier, $.optional_type_parameter_declaration],
-    [$._type_specifier, $._declarator, $._expression],
-    [$._declarator, $._expression],
+    [$._expression, $._declarator],
+    [$._expression, $.structured_binding_declarator],
+    [$._expression, $._declarator, $._type_specifier],
     [$.parameter_list, $.argument_list],
   ]),
 
@@ -40,7 +41,7 @@ module.exports = grammar(C, {
       $.alias_declaration,
       $.template_declaration,
       $.template_instantiation,
-      $.structured_binding_declaration,
+      // $.structured_binding_declaration,
       alias($.constructor_or_destructor_definition, $.function_definition)
     ),
 
@@ -149,19 +150,19 @@ module.exports = grammar(C, {
       original
     ),
 
-    structured_binding_declaration: $ => seq(
-      $._declaration_specifiers,
-      choice(
-        alias($.structured_binding_reference_declarator, $.reference_declarator),
-        $.structured_binding_declarator,
-      ),
-      choice(
-        seq('=', choice($.initializer_list, $._expression)),
-        $.initializer_list,
-        $.argument_list
-      ),
-      ';'
-    ),
+    // structured_binding_declaration: $ => seq(
+    //   $._declaration_specifiers,
+    //   choice(
+    //     alias($.structured_binding_reference_declarator, $.reference_declarator),
+    //     $.structured_binding_declarator,
+    //   ),
+    //   choice(
+    //     seq('=', choice($.initializer_list, $._expression)),
+    //     $.initializer_list,
+    //     $.argument_list
+    //   ),
+    //   ';'
+    // ),
 
     template_declaration: $ => seq(
       'template',
@@ -361,8 +362,15 @@ module.exports = grammar(C, {
       $.scoped_identifier,
       $.template_function,
       $.operator_name,
-      $.destructor_name
+      $.destructor_name,
+      $.structured_binding_declarator
     ),
+
+    // structured_declarator: $ => seq(
+    //   '[',
+    //   commaSep1($.identifier),
+    //   ']'
+    // ),
 
     _field_declarator: ($, original) => choice(
       original,
@@ -380,7 +388,7 @@ module.exports = grammar(C, {
     reference_field_declarator: $ => prec.dynamic(1, prec.right(seq(choice('&', '&&'), $._field_declarator))),
     abstract_reference_declarator: $ => prec.right(seq(choice('&', '&&'), optional($._abstract_declarator))),
 
-    structured_binding_reference_declarator: $ => seq(choice('&', '&&'), $.structured_binding_declarator),
+    // structured_binding_reference_declarator: $ => seq(choice('&', '&&'), $.structured_binding_declarator),
     structured_binding_declarator: $ => seq('[', commaSep1($.identifier), ']'),
 
     function_declarator: ($, original) => seq(

commit 318aea6b3c27b94d862cad2febc441db7ddaf217

Fix array assignment expressions so they don't parse as structured bindings (#78)
* Fix array assignment expressions so they don't parse as structured bindings

Array assignment expressions were broken in 9201abd which added more
support for structured binding declarators. We need to add a dynamic
precedence that is lower than that of an assignment expression so that
code is parsed as an assignment expression before it's parsed as a
structured binding.

* Just use -1 for structured binding dynamic precedence

Co-authored-by: Max Brunsfeld <maxbrunsfeld@gmail.com>

grammar.js
@@ -3,6 +3,7 @@ const C = require("tree-sitter-c/grammar")
 const PREC = Object.assign(C.PREC, {
   LAMBDA: 18,
   NEW: C.PREC.CALL + 1,
+  STRUCTURED_BINDING: -1,
 })
 
 module.exports = grammar(C, {
@@ -40,7 +41,6 @@ module.exports = grammar(C, {
       $.static_assert_declaration,
       $.template_declaration,
       $.template_instantiation,
-      // $.structured_binding_declaration,
       alias($.constructor_or_destructor_definition, $.function_definition),
       alias($.operator_cast_definition, $.function_definition),
       alias($.operator_cast_declaration, $._declaration),
@@ -475,8 +475,9 @@ module.exports = grammar(C, {
     reference_field_declarator: $ => prec.dynamic(1, prec.right(seq(choice('&', '&&'), $._field_declarator))),
     abstract_reference_declarator: $ => prec.right(seq(choice('&', '&&'), optional($._abstract_declarator))),
 
-    // structured_binding_reference_declarator: $ => seq(choice('&', '&&'), $.structured_binding_declarator),
-    structured_binding_declarator: $ => seq('[', commaSep1($.identifier), ']'),
+    structured_binding_declarator: $ => prec.dynamic(PREC.STRUCTURED_BINDING, seq(
+      '[', commaSep1($.identifier), ']'
+    )),
 
     function_declarator: ($, original) => seq(
       original,