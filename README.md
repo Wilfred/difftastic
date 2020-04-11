3bbef0f780fe9dc68e1676230afe56c9ea650170
sogaiu <983021772@users.noreply.github.com>
2020-04-11 16:06:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,106 @@
+# tree-sitter-janet
+
+## Status
+
+Subject to change, grammar still evolving.
+
+## Prerequisites
+
+* [emsdk](https://emscripten.org/docs/getting_started/downloads.html#installation-instructions) -- emscripten via homebrew seems to work for macos
+* node >= 12 (nvm recommended) -- recently tested 12.9.1, 12,16,1
+
+## Fine Print
+
+* The instructions below assume emsdk has been installed, but `emcc` (tool that can be used to compile to wasm) is not necessarily on one's `PATH`.  If an appropriate `emcc` is on one's `PATH` (e.g. emscripten installed via homebrew), the emsdk steps (e.g. `source ~/src/emsdk/emsdk_env.sh`) below may be ignored.
+
+* `node-gyp` (tool for compiling native addon modules for Node.js) may fail on machines upgraded to macos Catalina. [This document](https://github.com/nodejs/node-gyp/blob/master/macOS_Catalina.md) may help cope with such a situation.
+
+## Initial Setup
+
+Suppose typical development sources are stored under `~/src`.
+
+```
+# clone repository
+cd ~/src
+git clone https://github.com/sogaiu/tree-sitter-janet
+cd tree-sitter-janet
+
+# create / populate
+#   `node_modules` with dependencies
+#   `src` with tree-sitter .c goodness
+#   `build`
+#   `build/Release` and build `tree_sitter_janet_binding.node`
+npm install
+
+# included in previous command
+#npx tree-sitter generate
+#npx node-gyp configure
+#npx node-gyp rebuild
+```
+
+## Grammar Development
+
+Hack on grammar and interactively test.
+
+```
+# prepare emsdk (specifically emcc) for building .wasm
+source ~/src/emsdk/emsdk_env.sh
+
+# edit grammar.js using some editor
+
+# rebuild tree-sitter stuff and invoke web-ui for interactive testing
+npx tree-sitter generate && \
+npx node-gyp rebuild && \
+npx tree-sitter build-wasm && \
+npx tree-sitter web-ui
+
+# in appropriate browser window, paste code in left pane
+
+# examine results in right pane -- can even click on nodes
+
+# find errors and loop back to edit step above...
+```
+
+Parse individual files.
+
+```
+# create and populate sample code file for parsing named `sample.clj`
+
+# parse sample file
+npx tree-sitter parse sample.clj
+
+# examine output similar to web-ui, but less convenient
+```
+
+## Measure Performance
+
+```
+# single measurement
+npx tree-sitter parse --time sample.clj
+
+# mutliple measurements with `multitime`
+multitime -n10 -s1 npx tree-sitter parse --time --quiet sample.clj
+```
+
+## Build .wasm
+
+Assuming emsdk is installed appropriately under `~/src/emsdk`.
+
+```
+# prepare emsdk (specifically emcc) for use
+source ~/src/emsdk/emsdk_env.sh
+
+# create `tree-sitter-janet.wasm`
+npx tree-sitter build-wasm
+```
+
+## Resources
+
+* [Guide to your first Tree-sitter grammar](https://gist.github.com/Aerijo/df27228d70c633e088b0591b8857eeef)
+* [tree-sitter](http://tree-sitter.github.io/tree-sitter/)
+
+## Acknowledgments
+
+* Aerijo - Guide to your first Tree-sitter grammar
+* bakpakin - janet
+* maxbrunsfeld - tree-sitter and related
---
+++ grammar.js
@@ -0,0 +1,216 @@
+// numbers
+const SIGN =
+      choice('-', '+');
+const DIGIT =
+      /[0-9]/;
+const HEX_DIGIT =
+      /[0-9A-F]/;
+const RADIX =
+      choice('2', '3', '4', '5', '6', '7', '8', '9', '10',
+             '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+             '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
+             '31', '32', '33', '34', '35', '36');
+const ALPHA_NUM =
+      /[a-zA-Z0-9]/;
+
+// symbols and keywords
+// janet/tools/symcharsgen.c
+const SYM_CHAR_NO_DIGIT_NO_COLON =
+      /[a-zA-Z!$%&*+\-./:<?=>@^_]/;
+const SYM_CHAR =
+      /[0-9:a-zA-Z!$%&*+\-./:<?=>@^_]/;
+
+// strings
+const STRING_DOUBLE_QUOTE_CONTENT =
+      repeat(choice(/[^\\"]/,
+                    /\\(.|\n)/)); // thanks to tree-sitter-haskell
+
+module.exports = grammar({
+  name: 'janet',
+
+  extras: $ =>
+    [/\s/, $.comment],
+
+  externals: $ => [
+    $.long_buffer,
+    $.long_string
+  ],
+
+  rules: {
+    // THIS MUST BE FIRST -- even though this doesn't look like it matters
+    source_file: $ =>
+      repeat($._form),
+
+    comment: $ =>
+      /#.*/,
+
+    _form: $ =>
+      choice($.boolean,
+             $.buffer,
+             $.keyword,
+             $.long_buffer,
+             $.long_string,
+             $.nil,
+             $.number,
+             $.string,
+             $.symbol,
+             //
+             $.array,
+             $.array_literal,
+             $.struct,
+             $.table,
+             $.tuple,
+             $.tuple_literal,
+             //
+             $.quasi_quote_form,
+             $.quote_form,
+             $.short_fn_form,
+             $.splice_form,
+             $.unquote_form),
+
+    // simplest things
+
+    boolean: $ =>
+      choice('false',
+             'true'),
+
+    keyword: $ =>
+      prec(2, token(seq(':',
+                        repeat(SYM_CHAR)))),
+
+    nil: $ =>
+      'nil',
+
+    number: $ =>
+      prec(5, choice($._dec,
+                     $._hex,
+                     $._radix)),
+
+    _dec: $ =>
+      token(seq(optional(SIGN),
+                choice(seq(repeat1(DIGIT),
+                           repeat('_'),
+                           optional('.'),
+                           repeat('_'),
+                           repeat(DIGIT),
+                           repeat('_')),
+                       seq(repeat(DIGIT),
+                           repeat('_'),
+                           optional('.'),
+                           repeat('_'),
+                           repeat1(DIGIT),
+                           repeat('_'))),
+                optional(seq(choice('e', 'E'),
+                             optional(SIGN),
+                             repeat1(DIGIT))))),
+
+    _hex: $ =>
+      token(seq(optional(SIGN),
+                '0',
+                'x',
+                choice(seq(repeat1(HEX_DIGIT),
+                           repeat('_'),
+                           optional('.'),
+                           repeat('_'),
+                           repeat(HEX_DIGIT),
+                           repeat('_')),
+                       seq(repeat(HEX_DIGIT),
+                           repeat('_'),
+                           optional('.'),
+                           repeat('_'),
+                           repeat1(HEX_DIGIT),
+                           repeat('_'))))),
+
+    _radix: $ =>
+      token(seq(optional(SIGN),
+                seq(RADIX,
+                    choice('r', 'R'),
+                    ALPHA_NUM,
+                    repeat(choice(repeat(ALPHA_NUM),
+                                  repeat('_'))),
+                    optional(seq('&',
+                                 optional(SIGN),
+                                 repeat1(DIGIT)))))),
+
+    string: $ =>
+      token(seq('"',
+                STRING_DOUBLE_QUOTE_CONTENT,
+                '"')),
+
+    buffer: $ =>
+      token(seq('@"',
+                STRING_DOUBLE_QUOTE_CONTENT,
+                '"')),
+
+    symbol: $ =>
+      token(seq(SYM_CHAR_NO_DIGIT_NO_COLON,
+                repeat(SYM_CHAR))),
+
+    // collection-ish things
+
+    array: $ =>
+      seq('@(',
+          repeat($._form),
+          ')'),
+
+    array_literal: $ =>
+      seq('@[',
+          repeat($._form),
+          ']'),
+
+    struct: $ =>
+      seq('{',
+          repeat($._form),
+          '}'),
+
+    table: $ =>
+      seq('@{',
+          repeat($._form),
+          '}'),
+
+    tuple: $ =>
+      seq('(',
+          repeat($._form),
+          ')'),
+
+    tuple_literal: $ =>
+      seq('[',
+          repeat($._form),
+          ']'),
+
+    // macro-related
+
+    quasi_quote_form: $ =>
+      seq('~',
+          $._form),
+
+    quote_form: $ =>
+      seq("'",
+          $._form),
+
+    // following all work at the repl..
+    //   |8, ||8, |||8, etc.
+    //   |~(:x)
+    //   |{:a 1}
+    //   |[1 2]
+    //   |"a"
+    //   |:w
+    //   |a-sym
+    //   |@[8 9]
+    //   |(= $ 1)
+    // XXX: |() doesn't work...but don't bother disallowing
+    short_fn_form: $ =>
+      seq('|',
+          $._form),
+
+    // XXX: ?
+    splice_form: $ =>
+      seq(';',
+          $._form),
+
+    unquote_form: $ =>
+      seq(',',
+          $._form),
+
+  }
+});
