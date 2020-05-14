6a8fa9b386ef843cc20f333805f1332e11d9f8c6
Max Brunsfeld <maxbrunsfeld@gmail.com>
2020-05-14 15:01:28 -0700

    Update queries to use new syntax

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -42,13 +42,13 @@
 (relative_scope) @variable.builtin
 
 ((name) @constant
- (match? @constant "^_?[A-Z][A-Z\d_]+$"))
+ (#match? @constant "^_?[A-Z][A-Z\d_]+$"))
 
 ((name) @constructor
- (match? @constructor "^[A-Z]"))
+ (#match? @constructor "^[A-Z]"))
 
 ((name) @variable.builtin
- (eq? @variable.builtin "this"))
+ (#eq? @variable.builtin "this"))
 
 (variable_name) @variable
 
