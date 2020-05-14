06e73bbe354252f6a73a7167f4bd09b214e7da2d
Max Brunsfeld <maxbrunsfeld@gmail.com>
2020-05-14 15:02:48 -0700

    Update queries to use new syntax

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,10 +1,10 @@
 ; Identifier naming conventions
 
 ((identifier) @constructor
- (match? @constructor "^[A-Z]"))
+ (#match? @constructor "^[A-Z]"))
 
 ((identifier) @constant
- (match? @constant "^[A-Z][A-Z_]*$"))
+ (#match? @constant "^[A-Z][A-Z_]*$"))
 
 ; Function calls
 
@@ -19,7 +19,7 @@
 
 ((call
   function: (identifier) @function.builtin)
- (match?
+ (#match?
    @function.builtin
    "^(abs|all|any|ascii|bin|bool|breakpoint|bytearray|bytes|callable|chr|classmethod|compile|complex|delattr|dict|dir|divmod|enumerate|eval|exec|filter|float|format|frozenset|getattr|globals|hasattr|hash|help|hex|id|input|int|isinstance|issubclass|iter|len|list|locals|map|max|memoryview|min|next|object|oct|open|ord|pow|print|property|range|repr|reversed|round|set|setattr|slice|sorted|staticmethod|str|sum|super|tuple|type|vars|zip|__import__)$"))
 
