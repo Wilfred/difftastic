be9bf5edcb327edcebe12a91057b002bcf466c5c
Max Brunsfeld <maxbrunsfeld@gmail.com>
2020-06-01 15:46:18 -0700

    Use new alternative syntax in tags query

--- queries/tags.scm
+++ queries/tags.scm
@@ -2,16 +2,12 @@
 
 (
   (comment)* @doc
-  (method
-    name: (*) @name) @method
-  (#strip! @doc "^#\\s*")
-  (#select-adjacent! @doc @method)
-)
-
-(
-  (comment)* @doc
-  (singleton_method
-    name: (*) @name) @method
+  [
+    (method
+      name: (*) @name) @method
+    (singleton_method
+      name: (*) @name) @method
+  ]
   (#strip! @doc "^#\\s*")
   (#select-adjacent! @doc @method)
 )
@@ -21,16 +17,11 @@
 (
   (comment)* @doc
   (class
-    name: (constant) @name) @class
-  (#strip! @doc "^#\\s*")
-  (#select-adjacent! @doc @class)
-)
-
-(
-  (comment)* @doc
-  (class
-    name: (scope_resolution
-      name: (*) @name)) @class
+    name: [
+      (constant) @name
+      (scope_resolution
+        name: (*) @name)
+    ]) @class
   (#strip! @doc "^#\\s*")
   (#select-adjacent! @doc @class)
 )
@@ -38,11 +29,12 @@
 ; Calls
 
 (method_call
-  method: (identifier) @name) @call
-(method_call
-  method: (call method: (identifier) @name)) @call
-(call
-  method: (identifier) @name) @call
+  method: [
+    (identifier) @name
+    (call method: (identifier) @name)
+  ]) @call
+
+(call method: (identifier) @name) @call
 
 (
   (identifier) @name @call
