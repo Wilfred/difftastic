cf972aa5870485f7adb145a283f13cbce20f9ba1
Ika <ikatyang@gmail.com>
2020-06-02 22:37:59 +0800

    chore(deps): upgrade to tree-sitter@0.16.7

--- scripts/setup-tree-sitter.sh
+++ scripts/setup-tree-sitter.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/ikatyang/tree-sitter --branch 0.15.9-custom --depth 1
+git clone https://github.com/ikatyang/tree-sitter --branch 0.16.7-custom --depth 1
 cd tree-sitter
 git submodule update --init
 ./script/build-wasm
