be05e4f1f56bc4f41c9353c49a89746c522750fd
Antonio de Jesus Ochoa Solano <antoniodejesusochoasolano@gmail.com>
2020-07-02 00:20:45 -0700

    Add more function_declaration tests

--- test/cases/declarations/function-declarations.php
+++ test/cases/declarations/function-declarations.php
@@ -1,1 +1,11 @@
 function func(): void {}
+
+function func() {}
+
+function func($arg) {}
+
+function func(int $arg) {}
+
+function func(int $arg): void {}
+
+function func(int $arg1, bool $arg2): void {}
