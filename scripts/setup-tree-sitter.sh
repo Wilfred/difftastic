d6a2fa38295bcc4e13eb3969634779e838fea11d
Ika <ikatyang@gmail.com>
2020-07-04 16:45:48 +0800

    chore: use git submodule

--- scripts/setup-tree-sitter.sh
+++ scripts/setup-tree-sitter.sh
@@ -1,5 +1,4 @@
-git clone https://github.com/ikatyang/tree-sitter --branch 0.16.7-custom --depth 1
-cd tree-sitter
 git submodule update --init
+cd tree-sitter
 ./script/build-wasm
 cargo build --release
