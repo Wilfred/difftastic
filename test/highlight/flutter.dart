5fe39d732dd8ac130821010149535de5ffbefee7
Tim Whiting <tim@whitings.org>
2020-07-07 09:32:54 -0600

    add in simple flutter example

---
+++ test/highlight/flutter.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
