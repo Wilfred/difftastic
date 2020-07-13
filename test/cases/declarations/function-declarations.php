95f9decbf0a9441a5ec871bcd1e16d7fe1324764
Antonio de Jesus Ochoa Solano <antoniodejesusochoasolano@gmail.com>
2020-07-13 02:52:30 -0700

    Fix test/corpus Hacklang errors

--- test/cases/declarations/function-declarations.php
+++ test/cases/declarations/function-declarations.php
@@ -1,11 +1,11 @@
-function func(): void {}
+function func0(): void {}
 
-function func() {}
+function func1() {}
 
-function func($arg) {}
+function func2($arg) {}
 
-function func(int $arg) {}
+function func3(int $arg) {}
 
-function func(int $arg): void {}
+function func4(int $arg): void {}
 
-function func(int $arg1, bool $arg2): void {}
+function func5(int $arg1, bool $arg2): void {}
