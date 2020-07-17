29c0d5175170b3f278efe1196843ba3d96c93536
Max Brunsfeld <maxbrunsfeld@gmail.com>
2020-07-17 15:21:58 -0700

    Update queries to make use of new features

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -6,6 +6,14 @@
 ((identifier) @constant
  (#match? @constant "^[A-Z][A-Z_]*$"))
 
+; Builtin functions
+
+((call
+  function: (identifier) @function.builtin)
+ (#match?
+   @function.builtin
+   "^(abs|all|any|ascii|bin|bool|breakpoint|bytearray|bytes|callable|chr|classmethod|compile|complex|delattr|dict|dir|divmod|enumerate|eval|exec|filter|float|format|frozenset|getattr|globals|hasattr|hash|help|hex|id|input|int|isinstance|issubclass|iter|len|list|locals|map|max|memoryview|min|next|object|oct|open|ord|pow|print|property|range|repr|reversed|round|set|setattr|slice|sorted|staticmethod|str|sum|super|tuple|type|vars|zip|__import__)$"))
+
 ; Function calls
 
 (decorator) @function
@@ -14,14 +22,6 @@
   function: (attribute attribute: (identifier) @function.method))
 (call
   function: (identifier) @function)
-
-; Builtin functions
-
-((call
-  function: (identifier) @function.builtin)
- (#match?
-   @function.builtin
-   "^(abs|all|any|ascii|bin|bool|breakpoint|bytearray|bytes|callable|chr|classmethod|compile|complex|delattr|dict|dir|divmod|enumerate|eval|exec|filter|float|format|frozenset|getattr|globals|hasattr|hash|help|hex|id|input|int|isinstance|issubclass|iter|len|list|locals|map|max|memoryview|min|next|object|oct|open|ord|pow|print|property|range|repr|reversed|round|set|setattr|slice|sorted|staticmethod|str|sum|super|tuple|type|vars|zip|__import__)$"))
 
 ; Function definitions
 
@@ -34,12 +34,16 @@
 
 ; Literals
 
-(none) @constant.builtin
-(true) @constant.builtin
-(false) @constant.builtin
+[
+  (none)
+  (true)
+  (false)
+] @constant.builtin
 
-(integer) @number
-(float) @number
+[
+  (integer)
+  (float)
+] @number
 
 (comment) @comment
 (string) @string
@@ -49,71 +53,71 @@
   "{" @punctuation.special
   "}" @punctuation.special) @embedded
 
-; Tokens
+[
+  "-"
+  "-="
+  "!="
+  "*"
+  "**"
+  "**="
+  "*="
+  "/"
+  "//"
+  "//="
+  "/="
+  "&"
+  "%"
+  "%="
+  "^"
+  "+"
+  "->"
+  "+="
+  "<"
+  "<<"
+  "<="
+  "<>"
+  "="
+  "=="
+  ">"
+  ">="
+  ">>"
+  "|"
+  "~"
+  "and"
+  "in"
+  "is"
+  "not"
+  "or"
+] @operator
 
-"-" @operator
-"-=" @operator
-"!=" @operator
-"*" @operator
-"**" @operator
-"**=" @operator
-"*=" @operator
-"/" @operator
-"//" @operator
-"//=" @operator
-"/=" @operator
-"&" @operator
-"%" @operator
-"%=" @operator
-"^" @operator
-"+" @operator
-"+=" @operator
-"<" @operator
-"<<" @operator
-"<=" @operator
-"<>" @operator
-"=" @operator
-"==" @operator
-">" @operator
-">=" @operator
-">>" @operator
-"|" @operator
-"~" @operator
-"->" @operator
-"and" @operator
-"in" @operator
-"is" @operator
-"not" @operator
-"or" @operator
-
-; Keywords
-
-"as" @keyword
-"assert" @keyword
-"async" @keyword
-"await" @keyword
-"break" @keyword
-"class" @keyword
-"continue" @keyword
-"def" @keyword
-"del" @keyword
-"elif" @keyword
-"else" @keyword
-"except" @keyword
-"exec" @keyword
-"finally" @keyword
-"for" @keyword
-"from" @keyword
-"global" @keyword
-"if" @keyword
-"import" @keyword
-"lambda" @keyword
-"nonlocal" @keyword
-"pass" @keyword
-"print" @keyword
-"raise" @keyword
-"return" @keyword
-"try" @keyword
-"while" @keyword
-"with" @keyword
-"yield" @keyword
+[
+  "as"
+  "assert"
+  "async"
+  "await"
+  "break"
+  "class"
+  "continue"
+  "def"
+  "del"
+  "elif"
+  "else"
+  "except"
+  "exec"
+  "finally"
+  "for"
+  "from"
+  "global"
+  "if"
+  "import"
+  "lambda"
+  "nonlocal"
+  "pass"
+  "print"
+  "raise"
+  "return"
+  "try"
+  "while"
+  "with"
+  "yield"
+] @keyword
