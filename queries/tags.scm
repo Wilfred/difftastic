cf2069f2da85431595634e65556996fa39c2a7c2
Timothy Clem <timothy.clem@gmail.com>
2020-07-20 16:31:52 -0700

    Update to include new prefixes (#34)
    * Use new prefixes
    
    * Use new query syntax in tags query
    
    Co-authored-by: Max Brunsfeld <maxbrunsfeld@gmail.com>

--- queries/tags.scm
+++ queries/tags.scm
@@ -1,31 +1,23 @@
 (
-    (comment)* @doc
-    (function_declaration
-        name: (identifier) @name) @function
-    (#strip! @doc "^//\\s*")
-    (#set-adjacent! @doc @function)
+  (comment)* @doc
+  (function_declaration
+    name: (identifier) @name) @definition.function
+  (#strip! @doc "^//\\s*")
+  (#set-adjacent! @doc @definition.function)
 )
 
 (
-    (comment)* @doc
-    (method_declaration
-        name: (field_identifier) @name) @method
-    (#strip! @doc "^//\\s*")
-    (#set-adjacent! @doc @method)
+  (comment)* @doc
+  (method_declaration
+    name: (field_identifier) @name) @definition.method
+  (#strip! @doc "^//\\s*")
+  (#set-adjacent! @doc @definition.method)
 )
 
 (call_expression
-    function: (identifier) @name) @call
-
-(call_expression
-    function: (selector_expression
-        field: (field_identifier) @name)) @call
-
-(call_expression
-    function: (parenthesized_expression
-        (identifier) @name)) @call
-
-(call_expression
-    function: (parenthesized_expression
-        (selector_expression
-            field: (field_identifier) @name))) @call
+  function: [
+    (identifier) @name
+    (parenthesized_expression (identifier) @name)
+    (selector_expression field: (field_identifier) @name)
+    (parenthesized_expression (selector_expression field: (field_identifier) @name))
+  ]) @reference.call
