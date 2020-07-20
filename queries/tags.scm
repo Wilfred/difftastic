0711e93eb265f729c88c5555989d60bee3011873
Timothy Clem <timothy.clem@gmail.com>
2020-07-20 14:46:01 -0700

    Update tags query, add new prefixes to capture names (#131)
    * Include function_declaration matches in tags queries.
    
    * Update to include new prefixes
    
    * Fix indent in tags query
    
    * tags: Add constructor call tag
    
    Co-authored-by: Patrick Thomson <patrickt@github.com>
    Co-authored-by: Max Brunsfeld <maxbrunsfeld@gmail.com>

--- queries/tags.scm
+++ queries/tags.scm
@@ -1,53 +1,55 @@
 (
   (comment)* @doc
   (method_definition
-    name: (property_identifier) @name) @method
+    name: (property_identifier) @name) @definition.method
   (#strip! @doc "^[\\s\\*/]+|^[\\s\\*/]$")
-  (#select-adjacent! @doc @method)
-)
-
-(
-
-  (comment)* @doc
-  (class
-    name: (identifier) @name) @class
-  (#strip! @doc "^[\\s\\*/]+|^[\\s\\*/]$")
-  (#select-adjacent! @doc @class)
+  (#select-adjacent! @doc @definition.method)
 )
 
 (
   (comment)* @doc
-  (class_declaration
-    name: (identifier) @name) @class
+  [
+    (class
+      name: (identifier) @name)
+    (class_declaration
+      name: (identifier) @name)
+  ] @definition.class
   (#strip! @doc "^[\\s\\*/]+|^[\\s\\*/]$")
-  (#select-adjacent! @doc @class)
+  (#select-adjacent! @doc @definition.class)
 )
 
+(
+  (comment)* @doc
+  [
+    (function
+      name: (identifier) @name)
+    (function_declaration
+      name: (identifier) @name)
+    (generator_function
+      name: (identifier) @name)
+    (generator_function_declaration
+      name: (identifier) @name)
+  ] @definition.function
+  (#strip! @doc "^[\\s\\*/]+|^[\\s\\*/]$")
+  (#select-adjacent! @doc @definition.function)
+)
 
 (
   (comment)+? @doc
   (lexical_declaration
     (variable_declarator
       name: (identifier) @name
-      value: (arrow_function)) @function)
+      value: [(arrow_function) (function)]) @definition.function)
   (#strip! @doc "^[\\s\\*/]+|^[\\s\\*/]$")
-  (#select-adjacent! @doc @function)
-)
-
-(
-  (comment)* @doc
-  (lexical_declaration
-    (variable_declarator
-      name: (identifier) @name
-      value: (function)) @function)
-  (#strip! @doc "^[\\s\\*/]+|^[\\s\\*/]$")
-  (#select-adjacent! @doc @function)
+  (#select-adjacent! @doc @definition.function)
 )
 
 (call_expression
-  function: (identifier) @name) @call
+  function: (identifier) @name) @reference.call
 
 (call_expression
   function: (member_expression
-    property: (property_identifier) @name)) @call
+    property: (property_identifier) @name)) @reference.call
 
+(new_expression
+  constructor: (identifier) @name) @reference.class
