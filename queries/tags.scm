33bdf63d016d8de0f6fa8d105cae3b39c2dad36c
Timothy Clem <timothy.clem@gmail.com>
2020-07-20 16:28:15 -0700

    Update to include new prefixes (#57)
    * Use the new prefixes
    
    * Add tags for interfaces, constructor calls
    
    Co-authored-by: Max Brunsfeld <maxbrunsfeld@gmail.com>

--- queries/tags.scm
+++ queries/tags.scm
@@ -1,8 +1,19 @@
 (class_declaration
-  name: (identifier) @name) @class
+  name: (identifier) @name) @definition.class
 
 (method_declaration
-  name: (identifier) @name) @method
+  name: (identifier) @name) @definition.method
 
 (method_invocation
-  name: (identifier) @name) @call
+  name: (identifier) @name) @reference.call
+
+(interface_declaration
+  name: (identifier) @name) @definition.interface
+
+(interface_type_list
+  (type_identifier) @name) @reference.interface
+
+(object_creation_expression
+  type: (type_identifier) @name) @reference.class
+
+(superclass (type_identifier) @name) @reference.class
