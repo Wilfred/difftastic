f289f293f092bf76fd6d052bce606b65633c9187
Max Brunsfeld <maxbrunsfeld@gmail.com>
2020-07-21 11:09:08 -0700

    Add tags query

---
+++ queries/tags.scm
@@ -0,0 +1,17 @@
+(function_signature
+  name: (identifier) @name) @definition.function
+
+(method_signature
+  name: (property_identifier) @name) @definition.method
+
+(abstract_method_signature
+  name: (property_identifier) @name) @definition.method
+
+(abstract_class_declaration
+  name: (type_identifier) @name) @definition.class
+
+(module
+  name: (identifier) @name) @definition.module
+
+(interface_declaration
+  name: (type_identifier) @name) @definition.interface
