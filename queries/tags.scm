b0c0367d4b7058921fdc4ba11e257441a64ab809
Max Brunsfeld <maxbrunsfeld@gmail.com>
2020-07-29 16:56:25 -0700

    Add more symbols to tags query

--- queries/tags.scm
+++ queries/tags.scm
@@ -5,10 +5,19 @@
   name: (name) @name) @definition.function
 
 (method_declaration
-  name: (name) @name) @definition.method
+  name: (name) @name) @definition.function
+
+(object_creation_expression
+  [
+    (qualified_name (name) @name)
+    (variable_name (name) @name)
+  ]) @reference.class
 
 (function_call_expression
-  function: (qualified_name (name)) @name) @reference.call
+  function: [
+    (qualified_name (name) @name)
+    (variable_name (name)) @name
+  ]) @reference.call
 
 (scoped_call_expression
   name: (name) @name) @reference.call
