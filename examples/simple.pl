ad2dbe4e9f31624dafbf0e4d8216284574b71d01
ganezdragon <ganesh.raja.93@gmail.com>
2020-08-01 13:30:57 +0530

    introducing data types

--- examples/simple.pl
+++ examples/simple.pl
@@ -7,9 +7,23 @@ my $a;
 
 my @array;
 
-my $b = 1;
+my $number = 3010; # number
 
-my $string = 'hello';
+my $negative_number = -18;
+
+my $floating_number = 5.5;
+
+my $floating_negative_number = -7.455;
+
+my $string = 'hello'; # single quoted string
+
+my $double_quoted_string = "meow";
+
+my $scientific_notation = 16.12E14;
+
+my $hexa_decimal = 0xffff;
+
+my $octal = 0577;
 
 
 # multi declarations and initialization
