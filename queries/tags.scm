34181774b3e86b7801c939c79c7b80a82df91a2b
Max Brunsfeld <maxbrunsfeld@gmail.com>
2020-08-13 12:21:50 -0700

    Emit tags for type definitions and references (#35)

--- queries/tags.scm
+++ queries/tags.scm
@@ -21,3 +21,8 @@
     (selector_expression field: (field_identifier) @name)
     (parenthesized_expression (selector_expression field: (field_identifier) @name))
   ]) @reference.call
+
+(type_spec
+  name: (type_identifier) @name) @definition.type
+
+(type_identifier) @name @reference.type
