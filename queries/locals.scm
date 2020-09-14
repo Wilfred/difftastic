b48fca5a4b26fa346e6386cc27d6c28e93b4ee3f
Pieter Goetschalckx <pieter@gtschlckx.be>
2020-09-14 03:40:16 +0200

    Add parameter tests

--- queries/locals.scm
+++ queries/locals.scm
@@ -1,19 +1,15 @@
 ; Scopes
 ;-------
 
-(let_binding) @local.scope
-
-(class_binding) @local.scope
-
-(class_function) @local.scope
-
-(method_definition) @local.scope
-
-(fun_expression) @local.scope
-
-(match_case) @local.scope
-
-(for_expression) @local.scope
+[
+  (let_binding)
+  (class_binding)
+  (class_function)
+  (method_definition)
+  (fun_expression)
+  (match_case)
+  (for_expression)
+] @local.scope
 
 ; Definitions
 ;------------
