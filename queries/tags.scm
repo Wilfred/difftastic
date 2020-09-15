42359242b8980d152594d46de380d845b3fc615a
Patrick Thomson <patrickt@github.com>
2020-09-15 14:09:01 -0400

    Pairing with Noelle on adding new queries.

--- queries/tags.scm
+++ queries/tags.scm
@@ -1,6 +1,21 @@
-(
- (class_declaration
-  name: [
-         (identifier) @name
-         ]) @definition.class
- )
+(class_declaration
+ name: [
+        (identifier) @name
+        ]
+ ) @definition.class
+
+(interface_declaration
+ name: [
+        (identifier) @name
+        ]
+ ) @definition.interface
+
+(method_declaration
+ name: [
+        (identifier) @name
+        ]
+ ) @definition.method
+
+(object_creation_expression
+ type: (identifier) @name
+ ) @reference.class
