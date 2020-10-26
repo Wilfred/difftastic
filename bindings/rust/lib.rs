9c2a9de530c18d4a61bfedbf70052cc5e37a6641
Douglas Creager <dcreager@dcreager.net>
2020-10-26 09:02:14 -0400

    Add Rust package
    This adds a Rust package for this language grammar.  This makes it
    easier to use the Rust tree-sitter bindings with a statically known set
    of languages, since you can just list them as normal Rust dependencies,
    instead of having to embed the grammar and set up a build.rs file
    yourself.

---
+++ bindings/rust/lib.rs
@@ -0,0 +1,67 @@
+// -*- coding: utf-8 -*-
+// ------------------------------------------------------------------------------------------------
+// Copyright © 2020, tree-sitter-python authors.
+// See the LICENSE file in this repo for license details.
+// ------------------------------------------------------------------------------------------------
+
+//! This crate provides a Python grammar for the [tree-sitter][] parsing library.
+//!
+//! Typically, you will use the [parser][] function to create a tree-sitter [Parser][] that is
+//! configured to use this grammar, and then use the parser to parse some code:
+//!
+//! ```
+//! # use indoc::indoc;
+//! let code = indoc! {"
+//!     def double(x):
+//!         return x * 2
+//! "};
+//! let mut parser = tree_sitter_python::parser();
+//! let parsed = parser.parse(code, None);
+//! # assert!(parsed.is_some());
+//! ```
+//!
+//! [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
+//! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
+//! [parser]: fn.parser.html
+//! [tree-sitter]: https://tree-sitter.github.io/
+
+use tree_sitter::Language;
+use tree_sitter::Parser;
+
+extern "C" {
+    fn tree_sitter_python() -> Language;
+}
+
+/// Returns a new tree-sitter [Parser][] preconfigured to parse Python code.
+///
+/// [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
+pub fn parser() -> Parser {
+    let mut parser = Parser::new();
+    parser
+        .set_language(language())
+        .expect("Error installing Python grammar");
+    parser
+}
+
+/// Returns the tree-sitter [Language][] for this grammar.
+///
+/// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
+pub fn language() -> Language {
+    unsafe { tree_sitter_python() }
+}
+
+/// The source of the Python tree-sitter grammar description.
+pub const GRAMMAR: &'static str = include_str!("../../grammar.js");
+
+/// The content of the [`node-types.json`][] file for this grammar.
+///
+/// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
+pub const NODE_TYPES: &'static str = include_str!("../../src/node-types.json");
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn can_load_grammar() {
+        let _parser = super::parser();
+    }
+}
