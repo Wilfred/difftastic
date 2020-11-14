b2e70507250c2491a55b419d4e038573d8bf40ca
Tim Whiting <tim@whitings.org>
2020-11-14 13:00:30 -0700

    update test

--- tester/test.dart
+++ tester/test.dart
@@ -11,11 +11,11 @@ import 'package:dshell/dshell.dart';
 ///
 /// For details on installing dshell.
 ///
- var lines = 0;
-  final sectionSize = 100;
-  final maxPrint = 500;
-  var linesPrinted = 0;
-  var errorLines = 0;
+var lines = 0;
+final sectionSize = 100;
+final maxPrint = 500;
+var linesPrinted = 0;
+var errorLines = 0;
 void main(List<String> args) async {
   if (args.length < 1) {
     print("Usage: dart test.dart /path/to/directory/for/testing");
@@ -24,12 +24,12 @@ void main(List<String> args) async {
   find('*.dart', root: args[0], includeHidden: false).forEach((file) {
     files.add(file);
   });
- 
+
   final results = <Future<void>>[];
   for (var i = 0; i < files.length; i += sectionSize) {
     final sublist = files.sublist(
-          i, i + sectionSize < files.length ? i + sectionSize : files.length);
-   results.add(runTreeSitter(sublist));
+        i, i + sectionSize < files.length ? i + sectionSize : files.length);
+    results.add(runTreeSitter(sublist));
   }
   await Future.wait(results);
   print('Processed $lines lines of tree-sitter output');
@@ -37,20 +37,18 @@ void main(List<String> args) async {
   print('Error percentage ${errorLines * 100 / lines}%');
 }
 
-
 Future<void> runTreeSitter(List<String> files) async {
-   final result = await Process.run('tree-sitter', [
-      'parse',
-      ...files
-    ]);
-    for (final line in result.stdout.split('\n')) {
-      lines++;
-      if (line.contains('ERROR')) {
-        errorLines++;
-        if (linesPrinted < maxPrint) {
-          linesPrinted++;
-          print(line);
-        }
+  final result = await Process.run(
+      absolute('node_modules/tree-sitter-cli/tree-sitter'),
+      ['parse', ...files]);
+  for (final line in result.stdout.split('\n')) {
+    lines++;
+    if (line.contains('ERROR')) {
+      errorLines++;
+      if (linesPrinted < maxPrint) {
+        linesPrinted++;
+        print(line);
       }
     }
+  }
 }
