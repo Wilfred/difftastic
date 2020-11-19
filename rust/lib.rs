a84082fecbf4905553ab758043a5775de82e1c09
Harry Sarson <harry.sarson@hotmail.co.uk>
2020-11-19 22:10:38 +0000

    add unit test

--- rust/lib.rs
+++ rust/lib.rs
@@ -11,3 +11,24 @@ extern "C" {
 pub fn tree_sitter_elm() -> Language {
     unsafe { raw_tree_sitter_elm() }
 }
+
+#[cfg(test)]
+mod tests {
+    use tree_sitter::Parser;
+
+    #[test]
+    fn smoke() {
+        let source_code = "test : Test.Test";
+        let tree = {
+            let mut parser = Parser::new();
+            let language = super::tree_sitter_elm();
+            parser.set_language(language).unwrap();
+            parser.parse(source_code, None).unwrap()
+        };
+        let root_node = tree.root_node();
+
+        assert_eq!(root_node.kind(), "file");
+        assert_eq!(root_node.start_position().column, 0);
+        assert_eq!(root_node.end_position().column, 16);
+    }
+}
