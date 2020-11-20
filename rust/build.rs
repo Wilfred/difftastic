f23852d7787c0135cad1c471017def6eda6da28c
Harry Sarson <harry.sarson@hotmail.co.uk>
2020-11-20 18:32:05 +0000

    revert accidentally included change build

--- rust/build.rs
+++ rust/build.rs
@@ -4,10 +4,14 @@ fn main() {
     let dir = Path::new("src");
 
     cc::Build::new()
-        .cpp(true)
         .include(&dir)
         .file(dir.join("parser.c"))
-        .file(dir.join("scanner.cc"))
         .flag_if_supported("-Wno-unused-but-set-variable")
         .compile("tree-sitter-parser");
+
+    cc::Build::new()
+        .cpp(true)
+        .include(&dir)
+        .file(dir.join("scanner.cc"))
+        .compile("tree-sitter-scanner");
 }
