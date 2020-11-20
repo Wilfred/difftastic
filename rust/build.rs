fa03c80b1ee22c964d3917847fc3e42e7c09d1ee
Harry Sarson <harry.sarson@hotmail.co.uk>
2020-11-20 18:27:02 +0000

    apply review feedback

--- rust/build.rs
+++ rust/build.rs
@@ -4,14 +4,10 @@ fn main() {
     let dir = Path::new("src");
 
     cc::Build::new()
-        .include(&dir)
-        .file(dir.join("parser.c"))
-        .flag_if_supported("-Wno-unused-but-set-variable")
-        .compile("tree-sitter-parser");
-
-    cc::Build::new()
         .cpp(true)
         .include(&dir)
+        .file(dir.join("parser.c"))
         .file(dir.join("scanner.cc"))
-        .compile("tree-sitter-scanner");
+        .flag_if_supported("-Wno-unused-but-set-variable")
+        .compile("tree-sitter-parser");
 }
