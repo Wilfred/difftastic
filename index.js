84df79be3a22d5201de8b603041b8b7c499d4dd8
Max Brunsfeld <maxbrunsfeld@gmail.com>
2021-03-04 14:55:48 -0800

    Bump tree-sitter-cli to 0.19

--- index.js
+++
@@ -1,9 +0,0 @@
-try {
-  module.exports = require("./build/Release/tree_sitter_json_binding");
-} catch (error) {
-  try {
-    module.exports = require("./build/Debug/tree_sitter_json_binding");
-  } catch (_) {
-    throw error
-  }
-}
