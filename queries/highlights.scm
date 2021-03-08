5e1aa4de0cbbd8a5d43c65dbde3385fa71585332
Max Brunsfeld <maxbrunsfeld@gmail.com>
2021-03-08 13:12:59 -0800

    Move param highlight into separate query file
    This way, the main highlights.scm file can be reused for Typescript.

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -60,19 +60,6 @@
 ; Variables
 ;----------
 
-(formal_parameters
-  [
-    (identifier) @variable.parameter
-    (array_pattern
-      (identifier) @variable.parameter)
-    (object_pattern
-      [
-        (pair_pattern value: (identifier) @variable.parameter)
-        (shorthand_property_identifier_pattern) @variable.parameter
-      ])
-  ]
-)
-
 (identifier) @variable
 
 ; Properties
