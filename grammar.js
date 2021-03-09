bd6186c24d5eb13b4623efac9d944dcc095c0dad
Max Brunsfeld <maxbrunsfeld@gmail.com>
2021-03-09 14:13:16 -0800

    Allow non-ascii identifiers
    Closes #60

--- grammar.js
+++ grammar.js
@@ -1041,7 +1041,7 @@ module.exports = grammar({
     super: $ => 'super',
 
     // https://docs.oracle.com/javase/specs/jls/se8/html/jls-3.html#jls-IdentifierChars
-    identifier: $ => /[A-Za-z_$][A-Za-z0-9_$]*/,
+    identifier: $ => /[\p{L}_$][\p{L}\p{Nd}_$]*/,
 
     // http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
     comment: $ => token(prec(PREC.COMMENT, choice(
