3f085b32068821797ec46bd767c7064841cde163
Anantha Kumaran <ananthakumaran@gmail.com>
2021-03-17 21:11:22 +0530

    struct dot call

--- grammar.js
+++ grammar.js
@@ -224,7 +224,7 @@ module.exports = grammar({
     )),
 
     _complex_dot_call: $ => prec.left(PREC.DOT_CALL, seq(
-      field('object', choice($.dot_call, $.access_call, $.paren_expr, $.map, $.anonymous_function)),
+      field('object', choice($.dot_call, $.access_call, $.paren_expr, $.map, $.struct, $.anonymous_function)),
       '.',
       $._dot_call_function_args
     )),
