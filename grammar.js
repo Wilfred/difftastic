a2efc82ccd37f9c7ac7434f27353da993fd66cfc
Uy Ha <hchanuy@gmail.com>
2021-04-06 23:38:23 +0200

    Add some cmake grammars

--- grammar.js
+++ grammar.js
@@ -2,6 +2,24 @@ module.exports = grammar({
   name: 'CMake',
 
   rules: {
-    source_file: $ => 'hello'
+    source_file: $ => repeat($.command_invocation),
+
+    line_ending: $ => $.newline,
+    space: $ => /[ \t]+/,
+    newline: $ => /\n/,
+    identifier: $ => /[A-Za-z_][A-Za-z0-9_]*/,
+    argument: $ => /[^ ()#\"\\]+/,
+
+    command_invocation: $ => seq(
+      repeat($.space),
+      $.identifier,
+      repeat($.space),
+      $.parameter_list,
+    ),
+    parameter_list: $ => seq(
+      '(',
+      repeat($.argument),
+      ')'
+    )
   }
 })
