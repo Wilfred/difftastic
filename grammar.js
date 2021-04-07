263fb2a5ffba722c28e4656223152a0e6c6f4bf0
Uy Ha <hchanuy@gmail.com>
2021-04-08 00:17:56 +0200

    Complete grammar for unquoted arguments

--- grammar.js
+++ grammar.js
@@ -9,17 +9,22 @@ module.exports = grammar({
     newline: $ => /\n/,
     identifier: $ => /[A-Za-z_][A-Za-z0-9_]*/,
     argument: $ => /[^ ()#\"\\]+/,
+    seperation: $ => choice($.space, $.line_ending),
+
+    arguments: $ => seq($.argument, repeat($._seperated_arguments)),
+    _seperated_arguments: $ => prec.left(1, seq(
+      repeat1($.seperation),
+      optional($.argument)
+    )),
 
     command_invocation: $ => seq(
       repeat($.space),
       $.identifier,
       repeat($.space),
-      $.parameter_list,
-    ),
-    parameter_list: $ => seq(
       '(',
-      repeat($.argument),
+      optional($.arguments),
       ')'
-    )
+    ),
+
   }
 })
