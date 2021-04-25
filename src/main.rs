02b258491a3c281b676159ecc645a3164a130f5a
Wilfred Hughes <me@wilfred.me.uk>
2021-04-24 17:06:27 -0700

    Test top-level atoms

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,53 @@
 mod tree_diff;
 
+use crate::tree_diff::set_changed;
 use crate::tree_diff::ChangeKind::*;
 use crate::tree_diff::Syntax::*;
-use crate::tree_diff::set_changed;
 
 fn main() {
-    let mut lhs = vec![Items {
-        change: Unchanged,
-        start_content: "(".into(),
-        end_content: ")".into(),
-        children: vec![
-            Atom {
+    let mut lhs = vec![
+        Items {
+            change: Unchanged,
+            start_content: "(".into(),
+            end_content: ")".into(),
+            children: vec![Atom {
                 change: Unchanged,
-                content: "foo".into()
-            }
-        ],
-    }];
-    let mut rhs = vec![Items {
-        change: Unchanged,
-        start_content: "(".into(),
-        end_content: ")".into(),
-        children: vec![
-            Atom {
-                change: Unchanged,
-                content: "foo".into()
-            },
-            Atom {
-                change: Unchanged,
-                content: "bar".into()
-            }
-        ],
-    }];
+                content: "foo".into(),
+            }],
+        },
+        Atom {
+            change: Unchanged,
+            content: "a".into(),
+        },
+    ];
+    let mut rhs = vec![
+        Items {
+            change: Unchanged,
+            start_content: "(".into(),
+            end_content: ")".into(),
+            children: vec![
+                Atom {
+                    change: Unchanged,
+                    content: "foo".into(),
+                },
+                Atom {
+                    change: Unchanged,
+                    content: "bar".into(),
+                },
+            ],
+        },
+        Atom {
+            change: Unchanged,
+            content: "a".into(),
+        },
+        Atom {
+            change: Unchanged,
+            content: "b".into(),
+        },
+    ];
 
     set_changed(&mut lhs, &mut rhs);
 
-    print!("{:#?}", lhs);
-    print!("{:#?}", rhs);
+    print!("{:#?}\n", lhs);
+    print!("{:#?}\n", rhs);
 }
