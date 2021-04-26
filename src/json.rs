08249421a863a9b287cc7608d3295f9c0a0de8a0
Wilfred Hughes <me@wilfred.me.uk>
2021-04-25 18:48:01 -0700

    Parse string literal in JSON

--- src/json.rs
+++ src/json.rs
@@ -4,10 +4,25 @@ use regex::Regex;
 
 fn parse_json_from(s: &str, mut i: usize) -> Vec<Syntax> {
     let num_atom = Regex::new(r#"[0-9]+"#).unwrap();
+    let str_atom = Regex::new(r#""[^"]+""#).unwrap();
 
     let mut result = vec![];
     while i < s.len() {
         match num_atom.find_at(s, i) {
+            Some(m) => {
+                let atom = Syntax::Atom {
+                    content: m.as_str().into(),
+                    change: ChangeKind::Unchanged,
+                };
+                result.push(atom);
+
+                i += m.end();
+                continue;
+            }
+            None => {}
+        };
+
+        match str_atom.find_at(s, i) {
             Some(m) => {
                 let atom = Syntax::Atom {
                     content: m.as_str().into(),
@@ -43,6 +58,19 @@ mod tests {
             vec![{
                 Syntax::Atom {
                     content: "123".into(),
+                    change: ChangeKind::Unchanged,
+                }
+            }]
+        );
+    }
+
+    #[test]
+    fn test_parse_string() {
+        assert_eq!(
+            parse_json("\"abc\""),
+            vec![{
+                Syntax::Atom {
+                    content: "\"abc\"".into(),
                     change: ChangeKind::Unchanged,
                 }
             }]
