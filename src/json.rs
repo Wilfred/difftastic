02cd300aa0f2e027cb64921d206aeefe957bd2ca
Wilfred Hughes <me@wilfred.me.uk>
2021-04-26 00:03:39 -0700

    Handle parsing tokens not at the start

--- src/json.rs
+++ src/json.rs
@@ -2,30 +2,37 @@ use crate::tree_diff::ChangeKind;
 use crate::tree_diff::Syntax;
 use regex::Regex;
 
-fn parse_json_from(s: &str, mut i: usize) -> (Syntax, usize) {
-    let num_atom = Regex::new(r#"^[0-9]+"#).unwrap();
-    let str_atom = Regex::new(r#"^"[^"]+""#).unwrap();
+fn parse_json_from(s: &str, mut i: usize) -> (Vec<Syntax>, usize) {
+    let num_atom = Regex::new(r#"[0-9]+"#).unwrap();
+    let str_atom = Regex::new(r#""[^"]+""#).unwrap();
+    let open_brace = Regex::new(r#"\["#).unwrap();
+    let close_brace = Regex::new(r#"\]"#).unwrap();
+
+    let mut result = vec![];
 
     while i < s.len() {
-        match num_atom.find_at(s, i) {
+        match num_atom.find(&s[i..]) {
             Some(m) => {
                 let atom = Syntax::Atom {
                     content: m.as_str().into(),
                     change: ChangeKind::Unchanged,
                 };
-
-                return (atom, m.end());
+                result.push(atom);
+                i = m.end();
+                continue;
             }
             None => {}
         };
 
-        match str_atom.find_at(s, i) {
+        match str_atom.find(&s[i..]) {
             Some(m) => {
                 let atom = Syntax::Atom {
                     content: m.as_str().into(),
                     change: ChangeKind::Unchanged,
                 };
-                return (atom, m.end());
+                result.push(atom);
+                i = m.end();
+                continue;
             }
             None => {}
         };
@@ -57,6 +64,19 @@ mod tests {
     fn test_parse_integer() {
         assert_eq!(
             parse_json("123"),
+            vec![{
+                Syntax::Atom {
+                    content: "123".into(),
+                    change: ChangeKind::Unchanged,
+                }
+            }]
+        );
+    }
+
+    #[test]
+    fn test_parse_integer_with_whitespace() {
+        assert_eq!(
+            parse_json(" 123 "),
             vec![{
                 Syntax::Atom {
                     content: "123".into(),
