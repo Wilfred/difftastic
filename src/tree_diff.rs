a71bf9d12ecbedf0f39e6b824dac7ef2dc17b095
Wilfred Hughes <me@wilfred.me.uk>
2021-05-01 23:33:28 -0700

    WIP greedy tree differ

--- src/tree_diff.rs
+++ src/tree_diff.rs
@@ -134,6 +134,98 @@ pub fn set_changed(lhs: &mut [Syntax], rhs: &mut [Syntax]) {
     walk_nodes(rhs, &mut lhs_subtrees, Added);
 }
 
+// Greedy tree differ.
+fn walk_nodes_ordered(
+    lhs: &mut [Syntax],
+    rhs: &mut [Syntax],
+    lhs_counts: &mut HashMap<Syntax, i64>,
+    rhs_counts: &mut HashMap<Syntax, i64>,
+) {
+    // TODO: A B C -> A X B C
+    let mut lhs_i = 0;
+    let mut rhs_i = 0;
+    loop {
+        match (lhs.get_mut(lhs_i), rhs.get_mut(rhs_i)) {
+            (Some(ref mut lhs_node), Some(ref mut rhs_node)) => {
+                // If they're equal, nothing to do.
+                if lhs_node == rhs_node {
+                    lhs_node.set_change_deep(Unchanged);
+                    rhs_node.set_change_deep(Unchanged);
+                    lhs_i += 1;
+                    rhs_i += 1;
+                    continue;
+                }
+
+                // Not equal. Do we have more instances of the LHS
+                // node? If so, we've removed some instances on the
+                // RHS, so assume this is a removal.
+                let lhs_count = *lhs_counts.get(lhs_node).unwrap_or(&0);
+                let rhs_count = *rhs_counts.get(lhs_node).unwrap_or(&0);
+                if lhs_count > rhs_count {
+                    lhs_node.set_change_deep(Removed);
+                    lhs_counts.insert((*lhs_node).clone(), lhs_count - 1);
+                    lhs_i += 1;
+                    continue;
+                }
+
+                // Do we have more instances of the RHS
+                // node? If so, we've added some instances on the
+                // RHS, so assume this is an addition.
+                let lhs_count = *lhs_counts.get(rhs_node).unwrap_or(&0);
+                let rhs_count = *rhs_counts.get(rhs_node).unwrap_or(&0);
+                if rhs_count > lhs_count {
+                    rhs_node.set_change_deep(Added);
+                    rhs_counts.insert((*rhs_node).clone(), rhs_count - 1);
+                    rhs_i += 1;
+                    continue;
+                }
+
+                // Same number: reordered nodes, or both nodes are
+                // novel to a single side.
+                match (&lhs_node, &rhs_node) {
+                    (Items { .. }, Items { .. }) => {
+                        todo!();
+                    }
+                    (Items { .. }, Atom { .. }) => {
+                        todo!();
+                    }
+                    (Atom { .. }, Items { .. }) => {
+                        todo!();
+                    }
+                    (Atom { .. }, Atom { .. }) => {
+                        lhs_node.set_change_deep(Removed);
+                        rhs_node.set_change_deep(Added);
+                        lhs_i += 1;
+                        rhs_i += 1;
+                        continue;
+                    }
+                }
+            }
+            (Some(lhs_node), None) => {
+                let rhs_count = *rhs_counts.get(lhs_node).unwrap_or(&0);
+                if rhs_count > 0 {
+                    lhs_node.set_change_deep(Moved);
+                    rhs_counts.insert(lhs_node.clone(), rhs_count - 1);
+                } else {
+                    lhs_node.set_change_deep(Removed);
+                }
+                lhs_i += 1;
+            }
+            (None, Some(rhs_node)) => {
+                let lhs_count = *lhs_counts.get(rhs_node).unwrap_or(&0);
+                if lhs_count > 0 {
+                    rhs_node.set_change_deep(Moved);
+                    lhs_counts.insert(rhs_node.clone(), lhs_count - 1);
+                } else {
+                    rhs_node.set_change_deep(Added);
+                }
+                rhs_i += 1;
+            }
+            (None, None) => break,
+        }
+    }
+}
+
 /// For every node in `nodes`, if it's in `subtrees`, mark it as
 /// Unchanged and remove it from `subtrees`.
 ///
