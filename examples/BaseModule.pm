d62545075045fc0ede8d84cb1c6502ea426f882c
ganezdragon <ganesh.raja.93@gmail.com>
2021-05-10 01:36:56 +0530

    something is working now

--- examples/BaseModule.pm
+++ examples/BaseModule.pm
@@ -5,3 +5,15 @@ use parent 'PerlModule';
 BaseModule::YeaThis->new->hello();
 
 my $simple = SimpleModule->new();
+
+
+my $event = {
+  user_id => Data::UUID->new()->Something($dbh, {}),
+};
+
+
+my $output = {
+  some_property => BaseModule::AtSomeWhere::GetSomething(121, {
+      KEY => $var->{there},
+  }),
+};
