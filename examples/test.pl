07e3acda70902ba80ce41683ebddf30d90b7de76
ganezdragon <ganesh.raja.93@gmail.com>
2021-05-14 13:37:12 +0530

    finally pod comments work now :)

--- examples/test.pl
+++ examples/test.pl
@@ -2,5 +2,25 @@ use strict 'refs';
 use warnings;
 use Data::Dumper;
 
-print %{$args};
+my $var = 'sdfas';
 
+print $var;
+
+
+=pod
+=head1 Heading Text
+=head2 Heading Text
+=head3 Heading Text
+=head4 Heading Text
+=over indentlevel
+=item stuff
+=back
+=begin format
+=end format
+=for format text...
+=encoding type
+=end
+
+
+print "hello beautiful";
+=cut
