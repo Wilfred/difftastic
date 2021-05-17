adf281bdc3f90c88bf7b793c8e9eaf5886680a17
Wilfred Hughes <me@wilfred.me.uk>
2021-05-16 19:46:31 -0700

    Restore basic CLI argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,72 @@
 mod json;
-mod tree_diff;
 mod lines;
+mod tree_diff;
+use clap::{App, Arg};
+use std::fs;
 
 use crate::json::parse_json;
 use crate::tree_diff::set_changed;
 
+fn read_or_die(path: &str) -> String {
+    match fs::read_to_string(path) {
+        Ok(src) => src,
+        Err(e) => {
+            match e.kind() {
+                std::io::ErrorKind::NotFound => {
+                    eprintln!("No such file: {}", path);
+                }
+                std::io::ErrorKind::PermissionDenied => {
+                    eprintln!("Permission denied when reading file: {}", path);
+                }
+                _ => {
+                    eprintln!("Could not read file: {} (error {:?})", path, e.kind());
+                }
+            };
+            std::process::exit(1);
+        }
+    }
+}
+
 fn main() {
-    let lhs = parse_json("[\"a\"]");
-    let rhs = parse_json("[\"a\" \"b\"]");
+    let matches = App::new("Difftastic")
+        .version("0.1")
+        .about("A word level diff tool that understands syntax!")
+        .author("Wilfred Hughes")
+        .arg(
+            Arg::with_name("LANGUAGE")
+                .long("lang")
+                .takes_value(true)
+                .help("Override the language parser"),
+        )
+        .arg(
+            Arg::with_name("LINES")
+                .long("context")
+                .takes_value(true)
+                .help("Number of lines of context (default 3)"),
+        )
+        .arg(
+            Arg::with_name("COLUMNS")
+                .long("width")
+                .takes_value(true)
+                .help("Override terminal width"),
+        )
+        .arg(
+            Arg::with_name("inline")
+                .long("inline")
+                .help("Prefer single column output"),
+        )
+        .arg(Arg::with_name("first").index(1).required(true))
+        .arg(Arg::with_name("second").index(2).required(true))
+        .get_matches();
+
+    let before_path = matches.value_of("first").unwrap();
+    let before_src = read_or_die(before_path);
+
+    let after_path = matches.value_of("second").unwrap();
+    let after_src = read_or_die(after_path);
+
+    let lhs = parse_json(&before_src);
+    let rhs = parse_json(&after_src);
 
     set_changed(&lhs, &rhs);
 
