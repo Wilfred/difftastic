b055eeb9baec96e86e3e96e2ae9d6cebdc7ce710
Kolja Lampe <razzeee@gmail.com>
2021-05-21 02:32:52 +0200

    Use automatically generated binding for rust package

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -19,13 +19,12 @@ fn main() {
     println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
     */
 
-    println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
     c_config.compile("parser");
+    println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
 
     // If your language uses an external scanner written in C++,
     // then include this block of code:
 
-    /*
     let mut cpp_config = cc::Build::new();
     cpp_config.cpp(true);
     cpp_config.include(&src_dir);
@@ -34,7 +33,6 @@ fn main() {
         .flag_if_supported("-Wno-unused-but-set-variable");
     let scanner_path = src_dir.join("scanner.cc");
     cpp_config.file(&scanner_path);
-    println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
     cpp_config.compile("scanner");
-    */
+    println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
 }
