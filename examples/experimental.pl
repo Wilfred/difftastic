5652a728f21b963f6796683c5662b9e74c3678ec
ganezdragon <ganesh.raja.93@gmail.com>
2021-05-21 21:21:40 +0530

    implementing try catch or aka named blocks

--- examples/experimental.pl
+++ examples/experimental.pl
@@ -1,4 +1,11 @@
 use feature 'switch';
 use v5.14;
+use Try::Tiny;
 
 
+try {
+  my $a = 1;
+}
+catch {
+  print "catch block";
+};
--- examples/test.pl
+++ examples/test.pl
@@ -1,28 +1,37 @@
 use strict 'refs';
 use warnings;
 use Data::Dumper;
+use Try::Tiny;
 
-my $var = 'some string';
+# my $var = 'some string';
 
-my $hashref = {
-  someThing => 'hey hey',
-};
+# my $hashref = {
+#   someThing => 'hey hey',
+# };
 
-my %hash = (
-  makes => 'sense',
-);
+# my %hash = (
+#   makes => 'sense',
+# );
 
 
-print 'one\'s true \\love';
+# print 'one\'s true \\love';
 
 # print "\na simple string $var and $hash->{someThing}, haha\n";
 
-print qq /jkj/;
+# print qq /jkj/;
 
-print qq {\n {df {fsdf {dsf }}} ( im on vacation};
+# print qq {\n {df {fsdf {dsf }}} ( im on vacation};
 
-print qq {sdf $hashref->{someThing} im on vacation};
+# print qq {sdf $hashref->{someThing} im on vacation};
 
-print qq{$var is better than no string\n};
+# print qq{$var is better than no string\n};
 
-print qq{\nDoes this make $hash{makes}, really?};
+# print qq{\nDoes this make $hash{makes}, really?};
+
+try {
+  my $a = 1;
+}
+catch {
+  print "catch block";
+};
+
