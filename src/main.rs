db0e1695a7c92e3a96769a87542e0f7a5cdfbf9c
Wilfred Hughes <me@wilfred.me.uk>
2021-05-30 11:05:02 -0700

    Basic side-by-side display of diff

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,41 @@ use std::fs;
 use typed_arena::Arena;
 
 use crate::json::parse_json;
+use crate::lines::enforce_length;
 use crate::tree_diff::{apply_colors, change_positions, set_changed};
+
+fn term_width() -> Option<usize> {
+    term_size::dimensions().map(|(w, _)| w)
+}
+
+fn horizontal_concat(left: &str, right: &str) -> String {
+    let left_str_lines: Vec<&str> = left.lines().collect();
+    let right_str_lines: Vec<&str> = right.lines().collect();
+
+    let mut i = 0;
+    let mut res = String::new();
+    let spacer = "  ";
+    loop {
+        match (left_str_lines.get(i), right_str_lines.get(i)) {
+            (Some(left_line), Some(right_line)) => {
+                res.push_str(left_line);
+                res.push_str(spacer);
+                res.push_str(right_line);
+            }
+            (Some(left_line), None) => {
+                res.push_str(left_line);
+            }
+            (None, Some(right_line)) => {
+                res.push_str(right_line);
+            }
+            (None, None) => break,
+        }
+        res.push('\n');
+        i += 1;
+    }
+
+    res
+}
 
 fn read_or_die(path: &str) -> String {
     match fs::read_to_string(path) {
@@ -66,6 +100,16 @@ fn main() {
     let after_path = matches.value_of("second").unwrap();
     let after_src = read_or_die(after_path);
 
+    let terminal_width = match matches.value_of("COLUMNS") {
+        Some(width) => usize::from_str_radix(width, 10).unwrap(),
+        None => term_width().unwrap_or(80),
+    };
+
+    let line_length = terminal_width / 2 - 10;
+    // TODO: enforce length after parsing.
+    let before_src = enforce_length(&before_src, line_length);
+    let after_src = enforce_length(&after_src, line_length);
+
     let arena = Arena::new();
     let lhs = parse_json(&arena, &before_src);
     let rhs = parse_json(&arena, &after_src);
@@ -73,8 +117,10 @@ fn main() {
     set_changed(&lhs, &rhs);
 
     let positions = change_positions(&lhs);
-    println!("{}\n", apply_colors(&before_src, true, &positions));
+    let lhs_colored = apply_colors(&before_src, true, &positions);
 
     let positions = change_positions(&rhs);
-    println!("{}", apply_colors(&after_src, false, &positions));
+    let rhs_colored = apply_colors(&after_src, false, &positions);
+
+    println!("{}", horizontal_concat(&lhs_colored, &rhs_colored));
 }
