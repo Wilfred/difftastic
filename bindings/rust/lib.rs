c9cca94a6c4b0057ab0cdb7d0ae901767d0e6c7f
Maksim Novikov <mnovikov.work@gmail.com>
2021-06-05 15:43:12 +0200

    Fix name in rust bindings

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -6,7 +6,7 @@
 //! ```
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_SQL::language()).expect("Error loading SQL grammar");
+//! parser.set_language(tree_sitter_sql::language()).expect("Error loading SQL grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
@@ -18,14 +18,14 @@
 use tree_sitter::Language;
 
 extern "C" {
-    fn tree_sitter_SQL() -> Language;
+    fn tree_sitter_sql() -> Language;
 }
 
 /// Get the tree-sitter [Language][] for this grammar.
 ///
 /// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
 pub fn language() -> Language {
-    unsafe { tree_sitter_SQL() }
+    unsafe { tree_sitter_sql() }
 }
 
 /// The content of the [`node-types.json`][] file for this grammar.
