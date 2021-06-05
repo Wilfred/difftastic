af4ca00cbd81d112ae89a19dcd0d886639a86c1f
Maksim Novikov <mnovikov.work@gmail.com>
2021-06-05 12:39:09 +0200

    Add some more syntax highlighting tests

--- test/highlight/select.sql
+++ test/highlight/select.sql
@@ -2,5 +2,22 @@ SELECT a, b::INT;
 -- <- keyword
 --         ^ operator
 --            ^ type
+
 SELECT foo(a)
 --      ^ function
+
+SELECT foo(a)
+FROM table1
+-- <- keyword
+
+SELECT foo(a)
+FROM table1
+WHERE a = b
+-- <- keyword
+--      ^ operator
+
+SELECT foo(a)
+FROM table1
+GROUP BY a, b
+-- <- keyword
+--    ^ keyword
