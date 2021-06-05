fcaec80e6f1fca21435807fdd8e6cb0a3c056d62
Maksim Novikov <mnovikov.work@gmail.com>
2021-06-05 15:32:23 +0200

    Update prettier config

--- scripts/extract_error.js
+++ scripts/extract_error.js
@@ -5,24 +5,22 @@ const parser = spawn("tree-sitter", ["parse", filename]);
 const fs = require("fs");
 
 let output = "";
-parser.stdout.on("data", (data) => {
+parser.stdout.on("data", data => {
   output = output + data;
 });
 
-parser.on("close", (code) => {
+parser.on("close", code => {
   console.log("Parse exited code: ", code);
   if (code == 0) return;
 
   const arr = output.split("\n");
   const test = "(ERROR [32, 29] - [32, 63])";
   const error = output.match(
-    /\(ERROR \[(?<start>\d+, \d+)\] - \[(?<end>\d+, \d+)\]\)/
+    /\(ERROR \[(?<start>\d+, \d+)\] - \[(?<end>\d+, \d+)\]\)/,
   );
   const { start, end } = error.groups;
-  const [startRow, startCol] = start
-    .split(", ")
-    .map((val) => parseInt(val, 10));
-  const [endRow, endCol] = end.split(", ").map((val) => parseInt(val, 10));
+  const [startRow, startCol] = start.split(", ").map(val => parseInt(val, 10));
+  const [endRow, endCol] = end.split(", ").map(val => parseInt(val, 10));
 
   fs.readFile(filename, "utf8", (err, data) => {
     if (err) {
