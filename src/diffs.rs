e1251944b907416ed8779ed4e221b3d9227acd11
Wilfred Hughes <me@wilfred.me.uk>
2021-06-07 18:35:20 -0700

    Remove more unused code

--- src/diffs.rs
+++ src/diffs.rs
@@ -1,10 +1,8 @@
 #![allow(dead_code)]
 
-use crate::lines::{LineNumber, NewlinePositions};
+use crate::lines::{LineNumber};
 use crate::positions::{SingleLineSpan, Span};
 use colored::*;
-use itertools::EitherOrBoth;
-use itertools::Itertools;
 use std::cmp::min;
 use std::collections::HashMap;
 
@@ -28,52 +26,6 @@ pub struct Change {
     /// enables us to line up changed lines between the two strings.
     pub opposite_line: LineNumber,
 }
-
-// pub fn difference_positions(before_src: &str, after_src: &str, lang: Language) -> Vec<Change> {
-//     let re = language_lexer(lang);
-//     let before_tokens = lex(&before_src, &re);
-//     let after_tokens = lex(&after_src, &re);
-
-//     let before_newlines = NewlinePositions::from(before_src);
-//     let after_newlines = NewlinePositions::from(after_src);
-
-//     let mut left_line = LineNumber::from(0);
-//     let mut right_line = LineNumber::from(0);
-//     let mut positions = vec![];
-
-//     for d in diff::slice(&before_tokens, &after_tokens) {
-//         match d {
-//             // Only present in the before, so has been removed.
-//             diff::Result::Left(l) => {
-//                 positions.push(Change {
-//                     kind: ChangeKind::Remove,
-//                     range: AbsoluteRange {
-//                         start: l.start,
-//                         end: l.start + l.text.len(),
-//                     },
-//                     opposite_line: right_line,
-//                 });
-//             }
-//             // Present in both.
-//             diff::Result::Both(l, r) => {
-//                 left_line = before_newlines.from_offset(l.start).line;
-//                 right_line = after_newlines.from_offset(r.start).line;
-//             }
-//             // Only present in the after.
-//             diff::Result::Right(r) => {
-//                 positions.push(Change {
-//                     kind: ChangeKind::Add,
-//                     range: AbsoluteRange {
-//                         start: r.start,
-//                         end: r.start + r.text.len(),
-//                     },
-//                     opposite_line: left_line,
-//                 });
-//             }
-//         }
-//     }
-//     positions
-// }
 
 /// Return a copy of `s` with this colour applied to the ranges specified.
 fn apply_bold_color(s: &str, ranges: &[Span], c: Color) -> String {
@@ -201,116 +153,4 @@ pub fn removed(differences: &[Change]) -> Vec<Change> {
         .filter(|c| c.kind == ChangeKind::Remove)
         .copied()
         .collect()
-}
-
-/// Return a copy of `src` and `after_src` with the regions in
-/// `changes` coloured.
-pub fn apply_color_at_changes(src: &str, changes: &[Change], color: Color) -> String {
-    let newlines = NewlinePositions::from(src);
-    let abs_ranges: Vec<_> = (changes).iter().map(|c| c.range).collect();
-    let ranges = newlines.from_ranges(&abs_ranges);
-    apply_color_by_line(&src, &ranges.spans, color)
-}
-
-/// Given a list of differences, build up a string of the form:
-///
-///   -line two old
-///   +line two new
-///   +line five new
-///
-pub fn highlight_differences_combined(
-    before_src: &str,
-    after_src: &str,
-    differences: &[Change],
-) -> String {
-    let before_newlines = NewlinePositions::from(before_src);
-    let after_newlines = NewlinePositions::from(after_src);
-
-    let before_abs_ranges: Vec<_> = removed(differences).iter().map(|c| c.range).collect();
-    let after_abs_ranges: Vec<_> = added(differences).iter().map(|c| c.range).collect();
-
-    let before_ranges = before_newlines.from_ranges(&before_abs_ranges);
-    let after_ranges = after_newlines.from_ranges(&after_abs_ranges);
-
-    let before_ranges = group_by_line(&before_ranges.spans);
-    let after_ranges = group_by_line(&after_ranges.spans);
-
-    let mut res = String::new();
-
-    let mut prev_line = None;
-
-    for (i, line) in before_src
-        .lines()
-        .zip_longest(after_src.lines())
-        .enumerate()
-    {
-        let (before_line, after_line) = match line {
-            EitherOrBoth::Both(left, right) => (Some(left), Some(right)),
-            EitherOrBoth::Left(left) => (Some(left), None),
-            EitherOrBoth::Right(right) => (None, Some(right)),
-        };
-
-        let before_line_formatted = match (before_ranges.get(&i), before_line) {
-            (Some(line_ranges), Some(before_line)) => {
-                let mut res = String::new();
-
-                let ranges: Vec<_> = line_ranges
-                    .iter()
-                    .map(|lr| Span {
-                        start: lr.start_col,
-                        end: lr.end_col,
-                    })
-                    .collect();
-
-                res.push_str(&"-".red().to_string());
-                res.push_str(&apply_bold_color(&before_line, &ranges, Color::Red));
-                res.push_str("\n");
-
-                Some(res)
-            }
-            _ => None,
-        };
-
-        let after_line_formatted = match (after_ranges.get(&i), after_line) {
-            (Some(line_ranges), Some(after_line)) => {
-                let mut res = String::new();
-
-                let ranges: Vec<_> = line_ranges
-                    .iter()
-                    .map(|lr| Span {
-                        start: lr.start_col,
-                        end: lr.end_col,
-                    })
-                    .collect();
-
-                res.push_str(&"+".green().to_string());
-                res.push_str(&apply_bold_color(&after_line, &ranges, Color::Green));
-                res.push_str("\n");
-
-                Some(res)
-            }
-            _ => None,
-        };
-
-        if before_line_formatted.is_some() || after_line_formatted.is_some() {
-            let print_line_num = match prev_line {
-                Some(prev_line) => i > prev_line + 1,
-                None => true,
-            };
-            if print_line_num {
-                res.push_str(&format!("@@ line {} @@", i).blue().to_string());
-                res.push_str("\n");
-            }
-            prev_line = Some(i);
-        }
-
-        if let Some(before_line_formatted) = before_line_formatted {
-            res.push_str(&before_line_formatted);
-        }
-        if let Some(after_line_formatted) = after_line_formatted {
-            res.push_str(&after_line_formatted);
-        }
-    }
-
-    res
 }
