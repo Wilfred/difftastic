9e820d22142583e3872934f5a436003c48320c02
Wilfred Hughes <me@wilfred.me.uk>
2021-06-08 00:18:06 -0700

    Write a per-line accurate style applier

--- src/style.rs
+++ src/style.rs
@@ -1,42 +1,106 @@
+use crate::lines::{LineNumber, NewlinePositions};
+use crate::positions::SingleLineSpan;
 use crate::tree_diff::{MatchKind, MatchedPos};
 use colored::*;
 use std::cmp::min;
+use std::collections::HashMap;
 
-pub fn apply_colors(s: &str, is_lhs: bool, positions: &[MatchedPos]) -> String {
-    let mut res = String::with_capacity(s.len());
+#[derive(Clone, Copy)]
+pub struct Style {
+    foreground: Color,
+    background: Option<Color>,
+}
+
+impl Style {
+    fn apply(&self, s: &str) -> String {
+        let mut res = s.color(self.foreground).bold();
+        if let Some(background) = self.background {
+            res = res.on_color(background);
+        };
+        res.to_string()
+    }
+}
+
+pub fn apply_line(line: &str, styles: &[(SingleLineSpan, Style)]) -> String {
+    if styles.is_empty() {
+        return line.dimmed().to_string();
+    }
+
+    let mut res = String::with_capacity(line.len());
     let mut i = 0;
-    for mp in positions {
-        if mp.pos.start >= s.len() {
+    for (span, style) in styles {
+        if span.start_col >= line.len() {
             break;
         }
 
-        // Dim text that doesn't have any matching positions.
-        if i < mp.pos.start {
-            res.push_str(&s[i..mp.pos.start].dimmed());
+        if i < span.start_col {
+            res.push_str(&line[i..span.start_col].dimmed());
         }
 
-        let color = match mp.kind {
-            MatchKind::Unchanged => Color::White,
-            _ if is_lhs => Color::BrightRed,
-            _ => Color::BrightGreen,
-        };
-        let colored = &s[mp.pos.start..min(s.len(), mp.pos.end)]
-            .color(color)
-            .bold();
-        if let MatchKind::Novel = mp.kind {
-            if is_lhs {
-                res.push_str(&colored.clone().on_red().black().to_string());
-            } else {
-                res.push_str(&colored.clone().on_green().black().to_string());
-            }
-        } else {
-            res.push_str(&colored.to_string());
-        }
-
-        i = mp.pos.end;
+        res.push_str(&style.apply(&line[span.start_col..min(line.len(), span.end_col)]));
+        i = span.end_col;
     }
-    if i < s.len() {
-        res.push_str(&s[i..s.len()]);
+
+    if i < line.len() {
+        res.push_str(&line[i..line.len()].dimmed());
     }
     res
+}
+
+fn group_by_line(
+    ranges: &[(SingleLineSpan, Style)],
+) -> HashMap<LineNumber, Vec<(SingleLineSpan, Style)>> {
+    let mut ranges_by_line: HashMap<_, Vec<_>> = HashMap::with_capacity(ranges.len());
+    for range in ranges {
+        if let Some(matching_ranges) = ranges_by_line.get_mut(&range.0.line) {
+            (*matching_ranges).push(*range);
+        } else {
+            ranges_by_line.insert(range.0.line, vec![*range]);
+        }
+    }
+
+    ranges_by_line
+}
+
+/// Apply the `Style`s to the spans specified. Dim any text that
+/// doesn't have any styles applied.
+///
+/// Tolerant against lines in `s` being shorter than the spans.
+pub fn apply(s: &str, styles: &[(SingleLineSpan, Style)]) -> String {
+    let mut ranges_by_line = group_by_line(styles);
+
+    let mut res = String::with_capacity(s.len());
+    for (i, line) in s.lines().enumerate() {
+        let ranges = ranges_by_line.remove(&i.into()).unwrap_or(vec![]);
+        res.push_str(&apply_line(line, &ranges));
+        res.push_str("\n");
+    }
+    res
+}
+
+pub fn apply_colors(s: &str, is_lhs: bool, positions: &[MatchedPos]) -> String {
+    let nl_pos = NewlinePositions::from(s);
+
+    let mut styles = vec![];
+    for pos in positions {
+        let style = match pos.kind {
+            MatchKind::Unchanged => Style {
+                foreground: Color::White,
+                background: None,
+            },
+            MatchKind::Moved => Style {
+                foreground: if is_lhs { Color::Red } else { Color::Green },
+                background: None,
+            },
+            MatchKind::Novel => Style {
+                foreground: Color::Black,
+                background: Some(if is_lhs { Color::Red } else { Color::Green }),
+            },
+        };
+        for line_pos in nl_pos.from_ranges(&[pos.pos]).spans {
+            styles.push((line_pos, style));
+        }
+    }
+
+    apply(s, &styles)
 }
