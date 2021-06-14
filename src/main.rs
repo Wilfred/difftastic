81372df4f8842543f9e77172c018117d4dfa8407
Wilfred Hughes <me@wilfred.me.uk>
2021-06-13 17:28:09 -0700

    Use lhs/rhs terminology in main()

--- src/main.rs
+++ src/main.rs
@@ -52,51 +52,47 @@ fn main() {
         .arg(Arg::with_name("second").index(2).required(true))
         .get_matches();
 
-    let before_path = matches.value_of("first").unwrap();
-    let before_src = read_or_die(before_path);
+    let lhs_path = matches.value_of("first").unwrap();
+    let lhs_src = read_or_die(lhs_path);
 
-    let after_path = matches.value_of("second").unwrap();
-    let after_src = read_or_die(after_path);
+    let rhs_path = matches.value_of("second").unwrap();
+    let rhs_src = read_or_die(rhs_path);
 
     let syntax_toml = read_or_die("syntax.toml");
 
-    let before_extension = Path::new(before_path)
+    let lhs_extension = Path::new(lhs_path)
         .extension()
         .and_then(OsStr::to_str)
         .unwrap();
 
-    let lang = lang_from_str(&syntax_toml, before_extension);
+    let lang = lang_from_str(&syntax_toml, lhs_extension);
 
     let terminal_width = match matches.value_of("COLUMNS") {
         Some(width) => usize::from_str_radix(width, 10).unwrap(),
         None => term_width().unwrap_or(80),
     };
 
-    let max_left_length = lhs_printable_width(&before_src, terminal_width);
-    let max_right_length = rhs_printable_width(&after_src, max_left_length, terminal_width);
+    let lhs_formatted_length = lhs_printable_width(&lhs_src, terminal_width);
+    let rhs_formatted_length = rhs_printable_width(&rhs_src, lhs_formatted_length, terminal_width);
 
     let arena = Arena::new();
-    let lhs = parse(&arena, &before_src, &lang);
-    let rhs = parse(&arena, &after_src, &lang);
+    let lhs = parse(&arena, &lhs_src, &lang);
+    let rhs = parse(&arena, &rhs_src, &lang);
 
     set_changed(&lhs, &rhs);
 
-    let before_column_width = printed_line_num_width(&before_src);
-    let before_src = enforce_length(
-        &before_src,
-        max_left_length - before_column_width,
-    );
-    let after_column_width = printed_line_num_width(&after_src);
-    let after_src = enforce_length(
-        &after_src,
-        max_right_length - after_column_width,
-    );
+    let lhs_column_width = printed_line_num_width(&lhs_src);
+    let lhs_content_width = lhs_formatted_length - lhs_column_width;
+    let before_src = enforce_length(&lhs_src, lhs_content_width);
+    let rhs_column_width = printed_line_num_width(&rhs_src);
+    let rhs_content_width = rhs_formatted_length - rhs_column_width;
+    let rhs_src = enforce_length(&rhs_src, rhs_content_width);
 
-    let lhs_positions = matched_positions(&before_src, &after_src, &lhs);
+    let lhs_positions = matched_positions(&before_src, &rhs_src, &lhs);
     let lhs_colored = apply_colors(&before_src, true, &lhs_positions);
 
-    let rhs_positions = matched_positions(&after_src, &before_src, &rhs);
-    let rhs_colored = apply_colors(&after_src, false, &rhs_positions);
+    let rhs_positions = matched_positions(&rhs_src, &before_src, &rhs);
+    let rhs_colored = apply_colors(&rhs_src, false, &rhs_positions);
 
     let mut groups = visible_groups(&lhs_positions, &rhs_positions);
     for group in &mut groups {
@@ -109,8 +105,8 @@ fn main() {
             &lhs_colored,
             &rhs_colored,
             &groups,
-            max_left_length - before_column_width,
-            before_column_width,
+            lhs_content_width,
+            lhs_column_width,
             terminal_width
         )
     );
