8bcfb5caf99aa9da0ea591a855c5e6ce32662c87
Stephan Seitz <stephan.seitz@fau.de>
2021-06-16 23:23:06 +0200

    Update cargo bindings

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -6,7 +6,7 @@
 //! ```
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_javascript::language()).expect("Error loading jinja2 grammar");
+//! parser.set_language(tree_sitter_commonlisp::language()).expect("Error loading jinja2 grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
@@ -18,20 +18,20 @@
 use tree_sitter::Language;
 
 extern "C" {
-    fn tree_sitter_jinja2() -> Language;
+    fn tree_sitter_commonlisp() -> Language;
 }
 
 /// Get the tree-sitter [Language][] for this grammar.
 ///
 /// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
 pub fn language() -> Language {
-    unsafe { tree_sitter_jinja2() }
+    unsafe { tree_sitter_commonlisp() }
 }
 
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
 /// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
-pub const NODE_TYPES: &'static str = include_str!("../../src/node-types.json");
+pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
 // Uncomment these to include any queries that this grammar contains
 
@@ -47,6 +47,6 @@ mod tests {
         let mut parser = tree_sitter::Parser::new();
         parser
             .set_language(super::language())
-            .expect("Error loading jinja2 language");
+            .expect("Error loading commonlisp language");
     }
 }
