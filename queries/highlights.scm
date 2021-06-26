364423376f17ef91a8492a82ab26084f2a535c5e
Uy Ha <hchanuy@gmail.com>
2021-06-26 02:16:55 +0200

    Remove incomplete queries

--- queries/highlights.scm
+++
@@ -1,9 +0,0 @@
-(bracket_content) @parameter
-(quoted_element) @parameter
-(unquoted_argument) @parameter
-(variable) @variable.builtin
-[
-  (normal_command)
-  (foreach_command)
-  (endforeach_command)
-]@function
