4c1f5694ef9c9c9b5614649c023378299d0d0f28
Maksim Novikov <mnovikov.work@gmail.com>
2021-07-04 13:09:36 +0200

    Expand builtin types highlighting

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -19,8 +19,15 @@
   (FALSE)
 ] @constant.builtin
 
-((type_cast
-  (type) @type.builtin)
+([
+  (type_cast
+   (type) @type.builtin)
+  (create_function_statement
+   (type) @type.builtin)
+  (create_function_statement
+   (create_function_parameters
+     (create_function_parameter (type) @type.builtin)))
+ ]
  (#match?
    @type.builtin
     "^(bigint|BIGINT|int8|INT8|bigserial|BIGSERIAL|serial8|SERIAL8|bit|BIT|varbit|VARBIT|boolean|BOOLEAN|bool|BOOL|box|BOX|bytea|BYTEA|character|CHARACTER|char|CHAR|varchar|VARCHAR|cidr|CIDR|circle|CIRCLE|date|DATE|float8|FLOAT8|inet|INET|integer|INTEGER|int|INT|int4|INT4|interval|INTERVAL|json|JSON|jsonb|JSONB|line|LINE|lseg|LSEG|macaddr|MACADDR|money|MONEY|numeric|NUMERIC|decimal|DECIMAL|path|PATH|pg_lsn|PG_LSN|point|POINT|polygon|POLYGON|real|REAL|float4|FLOAT4|smallint|SMALLINT|int2|INT2|smallserial|SMALLSERIAL|serial2|SERIAL2|serial|SERIAL|serial4|SERIAL4|text|TEXT|time|TIME|time|TIME|timestamp|TIMESTAMP|tsquery|TSQUERY|tsvector|TSVECTOR|txid_snapshot|TXID_SNAPSHOT)$"))
