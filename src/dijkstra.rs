Wilfred/difftastic

commit c3bbfa7fa3adc9bd7e6408592be77aeaf620082b

Decrease highest edge score
This is easier to reason about and reduces risk of overflow.

src/dijkstra.rs
@@ -90,7 +90,7 @@ impl Edge {
             // Matching nodes is always best.
             UnchangedNode(depth_difference) => min(40, *depth_difference),
             // Matching an outer delimiter is good.
-            UnchangedDelimiter(depth_difference) => 1000 + min(40, *depth_difference),
+            UnchangedDelimiter(depth_difference) => 100 + min(40, *depth_difference),
 
             // Replacing a comment is better than treating it as novel.
             ReplacedComment => 1500,
@@ -98,16 +98,16 @@ impl Edge {
             // Otherwise, we've added/removed a node.
             NovelAtomLHS { contiguous } | NovelAtomRHS { contiguous } => {
                 if *contiguous {
-                    2000
+                    200
                 } else {
-                    2001
+                    201
                 }
             }
             NovelDelimiterLHS { contiguous } | NovelDelimiterRHS { contiguous } => {
                 if *contiguous {
-                    2000
+                    200
                 } else {
-                    2001
+                    201
                 }
             }
         }

commit 6fa60f64b80443f06f3bee3626c4dee033b12a68

Prefer replacing comments over novel atoms
This was broken in c3bbfa7.

src/dijkstra.rs
@@ -95,7 +95,7 @@ impl Edge {
             UnchangedDelimiter(depth_difference) => 100 + min(40, *depth_difference),
 
             // Replacing a comment is better than treating it as novel.
-            ReplacedComment => 1500,
+            ReplacedComment => 150,
 
             // Otherwise, we've added/removed a node.
             NovelAtomLHS { contiguous } | NovelAtomRHS { contiguous } => {
@@ -824,5 +824,34 @@ mod tests {
                 },
             ]
         );
+    }
+    #[test]
+    fn replace_comment() {
+        let arena = Arena::new();
+
+        let lhs: Vec<&Syntax> = vec![Syntax::new_comment(
+            &arena,
+            pos_helper(1),
+            "the quick brown fox",
+        )];
+        init_info(&lhs);
+
+        let rhs: Vec<&Syntax> = vec![Syntax::new_comment(
+            &arena,
+            pos_helper(1),
+            "the quick brown cat",
+        )];
+        init_info(&rhs);
+
+        let start = Vertex {
+            lhs_syntax: lhs.get(0).map(|n| *n),
+            lhs_prev_novel: None,
+            rhs_syntax: rhs.get(0).map(|n| *n),
+            rhs_prev_novel: None,
+        };
+        let route = shortest_path(start);
+
+        let actions = route.iter().map(|(action, _)| *action).collect_vec();
+        assert_eq!(actions, vec![ReplacedComment]);
     }
 }