fa69fcc045bff24c39c82e0f612ec827a73dd12e
Wilfred Hughes <me@wilfred.me.uk>
2021-07-28 00:20:29 -0700

    Fix rustdoc warning

--- src/intervals.rs
+++ src/intervals.rs
@@ -1,7 +1,7 @@
 /// An interval that is inclusive of `from` but exclusive of `to`.
 ///
 /// This is a an alternative to `Range`, which has some API warts:
-/// https://ridiculousfish.com/blog/posts/least-favorite-rust-type.html
+/// <https://ridiculousfish.com/blog/posts/least-favorite-rust-type.html>
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Interval<T: Copy + PartialOrd> {
     pub start: T,
