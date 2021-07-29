667670cb87bc127ddad80dc7412437b0c7d10a15
Kolja Lampe <razzeee@gmail.com>
2021-07-30 00:39:21 +0200

    Update highlighting

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -21,15 +21,22 @@
 
 (port) @keyword.other.port.elm
 
-(type_annotation(lower_case_identifier) @entity.name.function.elm)
-(port_annotation(lower_case_identifier) @entity.name.function.elm)
+(type_annotation(lower_case_identifier) @function.elm)
+(port_annotation(lower_case_identifier) @function.elm)
+(function_declaration_left(lower_case_identifier) @function.elm)
+(function_call_expr target: (value_expr) @function.elm)
+
+(field_access_expr(value_expr(value_qid)) @local.function.elm)
+(lower_pattern) @local.function.elm
+(record_base_identifier) @local.function.elm
+
 
 (operator_identifier) @keyword.operator.elm
 (eq) @keyword.operator.assignment.elm
 
 
-"(" @punctuation.section.braces.begin
-")" @punctuation.section.braces.end
+"(" @punctuation.section.braces
+")" @punctuation.section.braces
 
 "|" @keyword.other.elm
 "," @punctuation.separator.comma.elm
@@ -41,22 +48,28 @@
 
 
 (type) @keyword.type.elm
+
 (type_declaration(upper_case_identifier) @storage.type.elm)
-(union_variant(upper_case_identifier) @constant.type-constructor.elm)
+(type_ref) @storage.type.elm
+(type_alias_declaration name: (upper_case_identifier) @storage.type.elm)
+
+(union_variant(upper_case_identifier) @union.elm)
+(union_pattern) @union.elm
+(value_expr(upper_case_qid(upper_case_identifier)) @union.elm)
 
 ; comments
-(line_comment) @comment.line.double-dash.elm
-(block_comment) @comment.block.elm
+(line_comment) @comment.elm
+(block_comment) @comment.elm
 
 ; strings
-(string_escape) @constant.character.escape.elm
+(string_escape) @character.escape.elm
 
-(open_quote) @punctuation.definition.string.begin.elm
-(close_quote) @punctuation.definition.string.end.elm
+(open_quote) @string.elm
+(close_quote) @string.elm
 (regular_string_part) @string.elm
 
-(open_char) @punctuation.definition.char.begin.elm
-(close_char) @punctuation.definition.char.end.elm
+(open_char) @char.elm
+(close_char) @char.elm
 
 
 ; glsl
