ff523a0beab943a8c273bf8f2c1c8a073b4490e6
Wilfred Hughes <me@wilfred.me.uk>
2021-08-15 16:52:25 -0700

    Factor out a build helper function

--- build.rs
+++ build.rs
@@ -1,12 +1,16 @@
 use std::path::PathBuf;
 
-fn main() {
-    let dir: PathBuf = ["vendor", "tree-sitter-rust", "src"].iter().collect();
+fn build(package_name: &str) {
+    let dir: PathBuf = ["vendor", package_name, "src"].iter().collect();
 
     cc::Build::new()
         .include(&dir)
         .file(dir.join("parser.c"))
         .file(dir.join("scanner.c"))
         .warnings(false) // ignore unused parameter warnings
-        .compile("tree-sitter-javascript");
+        .compile(package_name);
+}
+
+fn main() {
+    build("tree-sitter-rust");
 }
