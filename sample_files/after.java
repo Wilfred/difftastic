db8147d7caf01bef644fae06406f340194837f0d
Wilfred Hughes <me@wilfred.me.uk>
2021-09-04 19:04:21 -0700

    Add Java test file

---
+++ sample_files/after.java
@@ -0,0 +1,13 @@
+class Foo {
+  /**
+   * Hello world.
+   *
+   * <p>More stuff here.</p>
+   *
+   * @param stuff {@code String} foo bar
+   * @returns {@code int}
+   */
+  protected static int blah() {
+    throw new Exception("before");
+  }
+}
