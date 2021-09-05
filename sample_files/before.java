ceb42a6ad2e3424c65313ed8e7feb16c84275b0d
Wilfred Hughes <me@wilfred.me.uk>
2021-09-04 19:04:21 -0700

    Add Java test file

---
+++ sample_files/before.java
@@ -0,0 +1,13 @@
+class Foo {
+  /**
+   * Hello world.
+   *
+   * <p>More stuff here.</p>
+   *
+   * @param stuff {@code String} foo bar
+   * @returns {@code Other}
+   */
+  public static Other blah() {
+    throw new Exception("before");
+  }
+}
