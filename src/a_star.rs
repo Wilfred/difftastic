e4048ba3ed9dbaeb63ee47b215acc3edd7c17d15
Wilfred Hughes <me@wilfred.me.uk>
2021-09-11 15:51:55 -0700

    Simplify impl

--- src/a_star.rs
+++ src/a_star.rs
@@ -11,10 +11,9 @@ struct OrdVertex<'a> {
     distance: u64,
     /// The vertex we have just reached.
     current: Vertex<'a>,
-    /// The previous vertex and edge we took to reach this vertex.
-    prev: Option<(Vertex<'a>, Edge)>,
-    /// Our best guess as to how far we are from the destination.
-    remaining_estimate: u64,
+    /// Our best guess as to the total distance from start to end if
+    /// we go through this vertex.
+    total_estimate: u64,
 }
 
 impl<'a> PartialOrd for OrdVertex<'a> {
@@ -25,13 +24,13 @@ impl<'a> PartialOrd for OrdVertex<'a> {
 
 impl<'a> Ord for OrdVertex<'a> {
     fn cmp(&self, other: &Self) -> Ordering {
-        (self.distance + self.remaining_estimate).cmp(&(other.distance + other.remaining_estimate))
+        self.total_estimate.cmp(&other.total_estimate)
     }
 }
 
 impl<'a> PartialEq for OrdVertex<'a> {
     fn eq(&self, other: &Self) -> bool {
-        (self.distance + self.remaining_estimate) == (other.distance + other.remaining_estimate)
+        self.total_estimate == other.total_estimate
     }
 }
 impl<'a> Eq for OrdVertex<'a> {}
@@ -67,17 +66,16 @@ pub fn shortest_path(start: Vertex) -> Vec<(Edge, Vertex)> {
     // to flip comparisons.
     let mut heap: BinaryHeap<Reverse<_>> = BinaryHeap::new();
 
-    let remaining_estimate = estimated_distance_remaining(&start);
+    let total_estimate = estimated_distance_remaining(&start);
     heap.push(Reverse(OrdVertex {
         distance: 0,
         current: start,
-        prev: None,
-        remaining_estimate,
+        total_estimate,
     }));
 
     // TODO: this grows very big. Consider using IDA* to reduce memory
     // usage.
-    let mut predecessors: FxHashMap<Vertex, (u64, Edge, Vertex)> = FxHashMap::default();
+    let mut predecessors: FxHashMap<Vertex, (u64, Vertex, Edge)> = FxHashMap::default();
 
     let end;
     loop {
@@ -85,40 +83,29 @@ pub fn shortest_path(start: Vertex) -> Vec<(Edge, Vertex)> {
             Some(Reverse(OrdVertex {
                 distance,
                 current,
-                prev,
-                remaining_estimate: _,
+                total_estimate: _,
             })) => {
                 if current.is_end() {
-                    if let Some((prev_node, prev_edge)) = prev {
-                        predecessors.insert(current.clone(), (distance, prev_edge, prev_node));
-                    }
-
-                    // TODO: Is this correct? Can we terminate if we reach for the first time?
                     end = current;
                     break;
                 }
 
-                // We've found a route to `current`. Is it an
-                // improvement over previously seen routes?
-                let found_shorter_route = match predecessors.get(&current) {
-                    Some((prev_shortest, _, _)) => distance < *prev_shortest,
-                    _ => true,
-                };
+                for (edge, next) in neighbours(&current) {
+                    let distance_to_next = distance + edge.cost();
+                    let found_shorter_route = match predecessors.get(&next) {
+                        Some((prev_shortest, _, _)) => distance_to_next < *prev_shortest,
+                        _ => true,
+                    };
 
-                if found_shorter_route {
-                    if let Some((prev_node, prev_edge)) = prev {
-                        predecessors.insert(current.clone(), (distance, prev_edge, prev_node));
-                    }
+                    if found_shorter_route {
+                        predecessors
+                            .insert(next.clone(), (distance_to_next, current.clone(), edge));
 
-                    for (edge, next) in neighbours(&current) {
-                        let new_remaining_estimate = estimated_distance_remaining(&next);
-                        let distance_to_next = distance + edge.cost();
-
+                        let total_estimate = distance_to_next + estimated_distance_remaining(&next);
                         heap.push(Reverse(OrdVertex {
                             distance: distance_to_next,
-                            remaining_estimate: new_remaining_estimate,
                             current: next,
-                            prev: Some((current.clone(), edge)),
+                            total_estimate,
                         }));
                     }
                 }
@@ -131,7 +118,7 @@ pub fn shortest_path(start: Vertex) -> Vec<(Edge, Vertex)> {
     let mut res: Vec<(Edge, Vertex)> = vec![];
     loop {
         match predecessors.remove(&current) {
-            Some((_, edge, node)) => {
+            Some((_, node, edge)) => {
                 res.push((edge, node.clone()));
                 current = node;
             }
