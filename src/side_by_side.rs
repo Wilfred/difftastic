4eac9aa6a0d8ceb90b11cfa6dca5a5ad9f1ef9eb
Wilfred Hughes <me@wilfred.me.uk>
2021-11-02 23:41:26 -0700

    Remove dead code now we're using hunk display

--- src/side_by_side.rs
+++ src/side_by_side.rs
@@ -2,20 +2,14 @@
 
 use atty::Stream;
 use colored::{Color, Colorize};
-use std::{
-    cmp::{max, min},
-    collections::{HashMap, HashSet},
-};
+use std::collections::HashSet;
 
 use crate::{
     context::{add_context, opposite_positions},
     hunks::{aligned_lines_from_hunk, extract_lines, Hunk},
-    lines::{
-        codepoint_len, enforce_exact_length, enforce_max_length, format_line_num, LineGroup,
-        LineNumber,
-    },
-    style::{self, apply_colors, header},
-    syntax::{aligned_lines, MatchedPos},
+    lines::{enforce_exact_length, enforce_max_length, format_line_num, LineNumber},
+    style::{self, apply_colors},
+    syntax::MatchedPos,
 };
 
 const SPACER: &str = "  ";
@@ -38,39 +32,8 @@ fn split_lines_nonempty(s: &str) -> Vec<String> {
     lines
 }
 
-fn longest_visible_line_rhs(s: &str, groups: &[LineGroup]) -> usize {
-    let lines = split_lines_nonempty(s);
-    let mut longest = 1;
-
-    for group in groups {
-        if let Some(rhs_lines) = &group.rhs_lines {
-            for line_num in rhs_lines.start.0..rhs_lines.end.0 {
-                let current_len = codepoint_len(&lines[line_num]);
-                longest = max(longest, current_len);
-            }
-        }
-    }
-
-    longest
-}
-
 fn lhs_printable_width(terminal_width: usize) -> usize {
     (terminal_width - SPACER.len()) / 2
-}
-
-fn rhs_printable_width(
-    rhs: &str,
-    groups: &[LineGroup],
-    lhs_width: usize,
-    rhs_column_width: usize,
-    terminal_width: usize,
-) -> usize {
-    let longest_line_length = longest_visible_line_rhs(rhs, groups);
-    let longest_line = longest_line_length + rhs_column_width;
-
-    let space_available = (terminal_width - SPACER.len()) - lhs_width;
-
-    min(longest_line, space_available)
 }
 
 fn format_line_num_padded(line_num: LineNumber, column_width: usize) -> String {
@@ -84,138 +47,6 @@ fn format_missing_line_num(prev_num: LineNumber, column_width: usize) -> String 
         ".".repeat(num_digits),
         width = column_width - 1
     )
-}
-
-fn apply_group<S: AsRef<str>>(
-    lhs_lines: &[S],
-    rhs_lines: &[S],
-    group: &LineGroup,
-    lhs_line_matches: &HashMap<LineNumber, LineNumber>,
-    lhs_content_width: usize,
-    lhs_column_width: usize,
-    rhs_column_width: usize,
-    lhs_lines_with_novel: &HashSet<LineNumber>,
-    rhs_lines_with_novel: &HashSet<LineNumber>,
-) -> String {
-    let mut result = String::new();
-
-    let aligned = aligned_lines(group, lhs_line_matches);
-    let lhs_empty = aligned.iter().all(|(lhs, _)| lhs.is_none());
-
-    // Find the first line number shown, so we print an accurate
-    // number of dots for the first unmatched line even if it's the
-    // first line.
-    let mut lhs_prev_line_num = LineNumber(0);
-    for (lhs_line_num, _) in &aligned {
-        if let Some(lhs_line_num) = lhs_line_num {
-            lhs_prev_line_num = *lhs_line_num;
-            break;
-        }
-    }
-
-    let mut rhs_prev_line_num = LineNumber(0);
-    for (_, rhs_line_num) in &aligned {
-        if let Some(rhs_line_num) = rhs_line_num {
-            rhs_prev_line_num = *rhs_line_num;
-            break;
-        }
-    }
-
-    for (lhs_line_num, rhs_line_num) in aligned {
-        if !lhs_empty {
-            match lhs_line_num {
-                Some(lhs_line_num) => {
-                    if lhs_lines_with_novel.contains(&lhs_line_num) {
-                        result.push_str(
-                            &format_line_num_padded(lhs_line_num, lhs_column_width)
-                                .bright_red()
-                                .to_string(),
-                        );
-                    } else {
-                        result.push_str(&format_line_num_padded(lhs_line_num, lhs_column_width));
-                    }
-                    result.push_str(lhs_lines[lhs_line_num.0].as_ref());
-
-                    lhs_prev_line_num = lhs_line_num;
-                }
-                None => {
-                    result.push_str(&format_missing_line_num(
-                        lhs_prev_line_num,
-                        lhs_column_width,
-                    ));
-                    result.push_str(&" ".repeat(lhs_content_width));
-                }
-            }
-            result.push_str(SPACER);
-        }
-
-        match rhs_line_num {
-            Some(rhs_line_num) => {
-                if rhs_lines_with_novel.contains(&rhs_line_num) {
-                    result.push_str(
-                        &format_line_num_padded(rhs_line_num, rhs_column_width)
-                            .bright_green()
-                            .to_string(),
-                    );
-                } else {
-                    result.push_str(&format_line_num_padded(rhs_line_num, lhs_column_width));
-                }
-                result.push_str(rhs_lines[rhs_line_num.0].as_ref());
-
-                rhs_prev_line_num = rhs_line_num;
-            }
-            None => {
-                result.push_str(&format_missing_line_num(
-                    rhs_prev_line_num,
-                    rhs_column_width,
-                ));
-            }
-        }
-
-        result.push('\n');
-    }
-
-    result
-}
-
-/// Display all the lines in `lhs` and `rhs` that are mentioned in
-/// `groups`, horizontally concatenating the matched lines.
-fn apply_groups(
-    display_path: &str,
-    lang_name: &str,
-    lhs: &str,
-    rhs: &str,
-    groups: &[LineGroup],
-    lhs_line_matches: &HashMap<LineNumber, LineNumber>,
-    lhs_content_width: usize,
-    lhs_column_width: usize,
-    rhs_column_width: usize,
-    lhs_lines_with_novel: &HashSet<LineNumber>,
-    rhs_lines_with_novel: &HashSet<LineNumber>,
-) -> String {
-    let mut result = String::new();
-
-    for (i, group) in groups.iter().enumerate() {
-        result.push_str(&header(display_path, i + 1, groups.len(), lang_name));
-        result.push('\n');
-
-        result.push_str(&apply_group(
-            &split_lines_nonempty(lhs),
-            &split_lines_nonempty(rhs),
-            group,
-            lhs_line_matches,
-            lhs_content_width,
-            lhs_column_width,
-            rhs_column_width,
-            lhs_lines_with_novel,
-            rhs_lines_with_novel,
-        ));
-        if i != groups.len() - 1 {
-            result.push('\n');
-        }
-    }
-
-    result
 }
 
 fn display_width() -> usize {
@@ -244,73 +75,6 @@ fn display_single_column(src: &str, color: Color) -> String {
     }
 
     result
-}
-
-/// Display `lhs_src` and `rhs_src` in a side-by-side view with
-/// changed lines shown and highlighted.
-pub fn display(
-    display_path: &str,
-    lang_name: &str,
-    lhs_src: &str,
-    rhs_src: &str,
-    lhs_positions: &[MatchedPos],
-    rhs_positions: &[MatchedPos],
-    lhs_matched_lines: &HashMap<LineNumber, LineNumber>,
-    groups: &[LineGroup],
-) -> String {
-    if lhs_src == "" {
-        return display_single_column(rhs_src, Color::BrightGreen);
-    }
-    if rhs_src == "" {
-        return display_single_column(lhs_src, Color::BrightRed);
-    }
-
-    let lhs_column_width = format_line_num(groups.last().unwrap().max_visible_lhs()).len();
-    let rhs_column_width = format_line_num(groups.last().unwrap().max_visible_rhs()).len();
-
-    let terminal_width = term_width().unwrap_or(80);
-
-    let lhs_formatted_length = lhs_printable_width(terminal_width);
-    let rhs_formatted_length = rhs_printable_width(
-        rhs_src,
-        groups,
-        lhs_formatted_length,
-        rhs_column_width,
-        display_width(),
-    );
-
-    let lhs_content_width = lhs_formatted_length - lhs_column_width;
-    let rhs_content_width = rhs_formatted_length - rhs_column_width;
-
-    let lhs_src = enforce_exact_length(lhs_src, lhs_content_width);
-    let rhs_src = enforce_max_length(rhs_src, rhs_content_width);
-    let lhs_colored = apply_colors(&lhs_src, true, lhs_positions);
-    let rhs_colored = apply_colors(&rhs_src, false, rhs_positions);
-
-    let lhs_lines_with_novel: HashSet<LineNumber> = lhs_positions
-        .iter()
-        .filter(|mp| mp.kind.is_novel())
-        .map(|mp| mp.pos.line)
-        .collect();
-    let rhs_lines_with_novel: HashSet<LineNumber> = rhs_positions
-        .iter()
-        .filter(|mp| mp.kind.is_novel())
-        .map(|mp| mp.pos.line)
-        .collect();
-
-    apply_groups(
-        display_path,
-        lang_name,
-        &lhs_colored,
-        &rhs_colored,
-        groups,
-        lhs_matched_lines,
-        lhs_content_width,
-        lhs_column_width,
-        rhs_column_width,
-        &lhs_lines_with_novel,
-        &rhs_lines_with_novel,
-    )
 }
 
 fn merge_adjacent(
