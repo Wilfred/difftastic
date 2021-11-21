cecd0713aedaf5e68428cfbcf75218858b59195b
Alex Pinkus <alex.pinkus@gmail.com>
2021-11-20 16:56:24 -0800

    Parallelize locally and allow split repositories
    Adding functionality to specify "part X of Y" of a repository. This
    allows two things to happen:
    1. In CI, that repository will get split across multiple Github Actions
    hosts, each one having less work to do
    2. Running locally, the script will spin off each validation check as
    its own process and they will all return more quickly.
    
    This is necessary to add `firefox-ios` as a top repository since it has
    657 swift files, which would otherwise take 5 minutes on CI, and I'm not
    ready to wait that long.
    
    Fixes #49

--- scripts/top-repos.sh
+++ scripts/top-repos.sh
@@ -28,9 +28,9 @@ function checkout() {
 
   if [ ! -d "$repo" ]; then
     git clone --quiet --branch $tag --depth 1 "https://github.com/$url" "$repo" >/dev/null 2>/dev/null
-  fi
 
-  echo "Cloned repository into $(pwd)/$repo"
+    echo "Cloned repository into $(pwd)/$repo"
+  fi
 }
 
 # Function to validate that a passed-in git repository can be parsed using this parser.
@@ -38,27 +38,51 @@ function checkout() {
 # If the passed-in repository fails to parse in ways that do not match the `known_failures` script
 # data, this prints info about the failure and exits the script with a nonzero status code.
 function validate() {
-  repo=$1; url=$2; tag=$3
+  repo=$1; url=$2; tag=$3; part=$4; total=$5
 
-  cd $tmpdir
-  checkout $repo $url $tag
-  cd $parser_dir
+  if [ -z "$part" ] || [ -z "$total" ]; then
+    part=0
+    total=1
+  fi
 
-  all_files=$(find "$tmpdir/$repo" -name *.swift -not -path '*/Pods/*')
+  data_dir=$tmpdir/.$repo-data-$part
+  mkdir -p $data_dir || true
+
+  # Find the start and end of this section based on the passed-in `part` and `total`.
+  all_files=$data_dir/all_files.txt
+  find "$tmpdir/$repo" -name *.swift -not -path '*/Pods/*' > $all_files
+  file_count=$(wc -l < $all_files | tr -d ' ')
+
+  one_past_the_end=$((file_count + 1))
+  start=$(($((one_past_the_end * part)) / total))
+  next_part=$((part + 1))
+  next_start=$(($((one_past_the_end * next_part)) / total))
+  end=$((next_start - 1))
+  len=$((end - start))
+
   failed_files=$(
     IFS=$'\n'
-    for file in $all_files; do
+    for file in $(head -$end < $all_files | tail -$len); do
        npx tree-sitter parse -q "$file" 2>/dev/null
     done | { grep -v -f "$known_failures" || true; }
     unset IFS
   )
 
   if [[ "$failed_files" = *[![:space:]]* ]]; then
-    echo "Unexpected parse failure found in $repo:"
+    echo -en "Unexpected parse failure found in $repo "
+    if [ -z $4 ]; then
+        echo ":"
+    else
+        echo "block $start-$end:"
+    fi
+
     cat <<<"$failed_files"
+
     exit 1
-  else
+  elif [ -z $4 ]; then
     echo "Parsed $repo successfully!"
+  else
+    echo "Parsed $repo files $start-$end of $file_count successfully!"
   fi
 }
 
@@ -72,5 +96,13 @@ fi
 
 # Run `validate` on every repository in `top-repositories` sequentially.
 while read line ; do
-    validate $line
+    cd $tmpdir
+    checkout $line
+    cd $parser_dir
+    validate $line &
+    pids+=($!)
 done <<<"$repos"
+
+for pid in "${pids[@]}" ; do
+    wait $pid
+done
