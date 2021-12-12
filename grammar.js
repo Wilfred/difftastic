d045b04b66d51c0ba8671e7ce1ee23a9f286b7d7
かわえもん <me@kawaemon.dev>
2021-12-13 03:08:54 +0900

    Add Generic Associated Types support (#124)
    * tests: put tests for GAT
    
    * feat: support GAT
    
    * tests: add normal type parameter to generic associated type
    
    * tests: add checks for type binding
    
    * feat: support type binding to have type arguments
    
    * chore: regenerate parser
    
    * chore: fix formatting

--- grammar.js
+++ grammar.js
@@ -481,6 +481,7 @@ module.exports = grammar({
     associated_type: $ => seq(
       'type',
       field('name', $._type_identifier),
+      field('type_parameters', optional($.type_parameters)),
       field('bounds', optional($.trait_bounds)),
       ';'
     ),
@@ -786,6 +787,7 @@ module.exports = grammar({
 
     type_binding: $ => seq(
       field('name', $._type_identifier),
+      field('type_arguments', optional($.type_arguments)),
       '=',
       field('type', $._type)
     ),
@@ -1450,7 +1452,7 @@ module.exports = grammar({
     self: $ => 'self',
     super: $ => 'super',
     crate: $ => 'crate',
-    
+
     metavariable: $ => /\$[a-zA-Z_]\w*/
   }
 })
