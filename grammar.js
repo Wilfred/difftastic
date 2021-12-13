8aecfc06174007c4edd5e93bd67bff80703d3318
Jonathan Arnett <jonarnett90@gmail.com>
2021-12-13 01:24:34 -0500

    Rudimentary constant support

--- grammar.js
+++ grammar.js
@@ -2,26 +2,37 @@ const NEWLINE = /\r?\n/;
 
 module.exports = grammar({
   name: "gleam",
-  extras: ($) => [";", "\n", "\r\n", /\s/],
+  extras: ($) => [";", NEWLINE, /\s/],
   rules: {
+    /* General rules */
     source_file: ($) =>
-      repeat(seq(choice($.target_group, $._statement), NEWLINE)),
+      optional(
+        seq(
+          series_of(choice($.target_group, $._statement), NEWLINE),
+          optional(NEWLINE)
+        )
+      ),
+    _statement: ($) =>
+      choice(
+        $.import,
+        $.public_constant,
+        $.constant
+        /* $.type, */
+        /* $.function */
+      ),
+
+    /* Target groups */
     target_group: ($) =>
       seq(
         "if",
         field("target", $.target),
         "{",
-        field("body", repeat(seq($._statement, NEWLINE))),
+        optional(seq(series_of($._statement, NEWLINE), optional(NEWLINE))),
         "}"
       ),
-    _statement: ($) =>
-      choice(
-        $.import
-        /* $.constant, */
-        /* $.type, */
-        /* $.function */
-      ),
     target: ($) => choice("erlang", "javascript"),
+
+    /* Import statements */
     import: ($) =>
       seq(
         "import",
@@ -50,7 +61,74 @@ module.exports = grammar({
         )
       ),
     alias: ($) => $._name,
+
+    /* Constant statements */
+    public_constant: ($) => seq("pub", $._constant),
+    constant: ($) => $._constant,
+    _constant: ($) =>
+      seq(
+        "const",
+        field("name", alias($._name, $.identifier)),
+        optional($._type_annotation),
+        "=",
+        field("value", $._literal)
+      ),
+
+    /* Literals */
+    _literal: ($) =>
+      choice(
+        $.string,
+        $.float,
+        $.integer,
+        $.tuple,
+        $.list
+        // $.bitstring,
+        // $.record,
+        // $.remote_record
+      ),
+    string: ($) => /\"(?:\\[efnrt\"\\]|[^\"])*\"/,
+    float: ($) => /-?[0-9_]+\.[0-9_]+/,
+    integer: ($) => /-?[0-9_]+/,
+    tuple: ($) => seq("#", "(", series_of($._literal, ","), ")"),
+    list: ($) => seq("[", series_of($._literal, ","), "]"),
+
+    /* Types */
+    _type_annotation: ($) => seq(":", field("type", $._type)),
+    _type: ($) =>
+      choice(
+        alias($._discard_name, $.type_hole),
+        $.tuple_type,
+        $.fn_type,
+        $.type_constructor,
+        $.remote_type_constructor,
+        $.type_var
+      ),
+    tuple_type: ($) => seq("#", "(", optional(series_of($._type, ",")), ")"),
+    fn_type: ($) =>
+      seq(
+        "fn",
+        "(",
+        series_of(alias($._type, $.argument_type), ","),
+        ")",
+        "->",
+        alias($._type, $.return_type)
+      ),
+    type_constructor: ($) => $._type_constructor,
+    remote_type_constructor: ($) => seq($._name, ".", $._type_constructor),
+    _type_constructor: ($) =>
+      seq($._upname, optional(seq("(", series_of($._type, ","), ")"))),
+    type_var: ($) => $._name,
+
+    /* Reused types from the Gleam lexer */
+    _discard_name: ($) => /_[_0-9a-z]*/,
     _name: ($) => /[_a-z][_0-9a-z]*/,
     _upname: ($) => /[A-Z][0-9a-zA-Z]*/,
   },
 });
+
+// Shamelessly stolen "sep1" from tree-sitter-elixir, renamed to match a similar
+// function in the Gleam parser.
+// https://github.com/elixir-lang/tree-sitter-elixir/blob/de3ec57591aebf451e710fc9c984cf601258baf5/grammar.js#L817-L819
+function series_of(rule, separator) {
+  return seq(rule, repeat(seq(separator, rule)));
+}
