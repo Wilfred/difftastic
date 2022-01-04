4d6168ef7f72f976ec0cdf732a91ca1835c13f88
Sal Bakraa <SalBakraa@gmail.com>
2022-01-04 07:41:53 +0300

    fix(ASI): Improve how ASI behaves
    Checking for semicolons was moved to '_automatic_semicolons', since
    the grammar sometimes got confused with reading a semicolon delimiting
    single line statements.
    
    Furthermore, copied tree-sitter-javascripts updated ASI code while also
    adding handling for single line automatic semicolons. It appears that
    the kotlin compiler can add semicolons even if the statements are
    delimited by a space.
    e.g: `import foo import bar`, `if() foo else bar` are valid constructs.
    
    Closes: #45

--- src/scanner.c
+++ src/scanner.c
@@ -13,33 +13,34 @@ void tree_sitter_kotlin_external_scanner_reset(void *p) {}
 unsigned tree_sitter_kotlin_external_scanner_serialize(void *p, char *buffer) { return 0; }
 void tree_sitter_kotlin_external_scanner_deserialize(void *p, const char *b, unsigned n) {}
 
+static void skip(TSLexer *lexer) { lexer->advance(lexer, true); }
 static void advance(TSLexer *lexer) { lexer->advance(lexer, false); }
 
 static bool scan_whitespace_and_comments(TSLexer *lexer) {
   for (;;) {
     while (iswspace(lexer->lookahead)) {
-      advance(lexer);
+      skip(lexer);
     }
 
     if (lexer->lookahead == '/') {
-      advance(lexer);
+      skip(lexer);
 
       if (lexer->lookahead == '/') {
-        advance(lexer);
+        skip(lexer);
         while (lexer->lookahead != 0 && lexer->lookahead != '\n') {
-          advance(lexer);
+          skip(lexer);
         }
       } else if (lexer->lookahead == '*') {
-        advance(lexer);
+        skip(lexer);
         while (lexer->lookahead != 0) {
           if (lexer->lookahead == '*') {
-            advance(lexer);
+            skip(lexer);
             if (lexer->lookahead == '/') {
-              advance(lexer);
+              skip(lexer);
               break;
             }
           } else {
-            advance(lexer);
+            skip(lexer);
           }
         }
       } else {
@@ -66,34 +67,123 @@ bool tree_sitter_kotlin_external_scanner_scan(void *payload, TSLexer *lexer,
   lexer->result_symbol = AUTOMATIC_SEMICOLON;
   lexer->mark_end(lexer);
 
+  bool sameline = true;
   for (;;) {
-    if (!iswspace(lexer->lookahead)) return false;
-    if (lexer->lookahead == '\n') break;
-    // other whitespace (not newline)
-    advance(lexer);
-  }
-  // consume the '\n' from the break
-  advance(lexer);
+    if (lexer->eof(lexer))
+      return true;
 
-  if (!scan_whitespace_and_comments(lexer)) return false;
+    if (lexer->lookahead == ';') {
+      advance(lexer);
+      lexer->mark_end(lexer);
+      return true;
+    }
+
+    if (!iswspace(lexer->lookahead)) {
+      break;
+    }
+
+    if (lexer->lookahead == '\n') {
+      skip(lexer);
+
+      sameline = false;
+      break;
+    }
+
+    if (lexer->lookahead == '\r') {
+      skip(lexer);
+
+      if (lexer->lookahead == '\n') {
+        skip(lexer);
+      }
+
+      sameline = false;
+      break;
+    }
+
+    skip(lexer);
+  }
+
+  // Skip whitespace and comments
+  if (!scan_whitespace_and_comments(lexer))
+    return false;
+
+  if (sameline) {
+    switch (lexer->lookahead) {
+      case 'e':
+        return scan_for_word(lexer, "lse", 3);
+
+      case 'i':
+        return scan_for_word(lexer, "mport", 5);
+
+      case ';':
+        advance(lexer);
+        lexer->mark_end(lexer);
+        return true;
+
+      default:
+        return false;
+    }
+  }
 
   switch (lexer->lookahead) {
+    case ',':
+    case '.':
+    case ':':
+    case '*':
+    case '%':
+    case '>':
+    case '<':
+    case '=':
+    case '{':
+    case '[':
+    case '(':
+    case '?':
+    case '|':
+    case '&':
+    case '/':
+      return false;
+
+    // Insert a semicolon before `--` and `++`, but not before binary `+` or `-`.
+    // Insert before +/-Float
+    case '+':
+      skip(lexer);
+      if (lexer->lookahead == '+')
+        return true;
+      return iswdigit(lexer->lookahead);
+    case '-':
+      skip(lexer);
+      if (lexer->lookahead == '-')
+        return true;
+      return iswdigit(lexer->lookahead);
+
+    // Don't insert a semicolon before `!=`, but do insert one before a unary `!`.
+    case '!':
+      skip(lexer);
+      return lexer->lookahead != '=';
+
     // specific to Kotlin
     case 'e': // ex: else on next line after 'if' control body
       return scan_for_word(lexer, "lse", 3);
-    case '{': // ex: function body defined on next line after decl
 
-    // cases also in tree-sitter-javascript/src/scanner.c
-    case '.': // ex: method-chain calls on next line
-    case ':': // ex: inheritance clause defined on next line
-    case '=': // ex: fun/val definition on next line (or == binop)
-    case '?': // ex: elvis operator ?: on next line
-    case '^': // ex: binary operator between 2 exprs
-    case '|': // ex: binary operator between 2 exprs
-    case '&': // ex: binary operator between 2 exprs
+    // Don't insert a semicolon before `in` or `instanceof`, but do insert one
+    // before an identifier or an import.
+    case 'i':
+      skip(lexer);
+      if (lexer->lookahead != 'n')
+        return true;
 
-      return false;
+      skip(lexer);
+      if (!iswalpha(lexer->lookahead))
+        return false;
+
+      return scan_for_word(lexer, "stanceof", 8);
+
+      case ';':
+        advance(lexer);
+        lexer->mark_end(lexer);
+        return true;
+
+    default:
+      return true;
   }
-
-  return true;
 }
