d78ccdc6dab4885302f69e9c8799c21632c13fdf
Munif Tanjim <hello@muniftanjim.dev>
2022-01-14 22:48:35 +0600

    refactor: move to root directory

--- script/release.sh
+++ script/release.sh
@@ -2,18 +2,14 @@
 
 set -eu
 
-declare -r workspaces=("lua")
-
 echo "Running Build..."
-
 npm run build
-
 echo
+
 echo "Running Tests..."
-
 npm run test
-
 echo
+
 echo "Bumping Version..."
 declare -r curr_version=$(npx -c 'echo $npm_package_version')
 npm version --allow-same-version --no-git-tag-version $@
@@ -27,18 +23,11 @@ fi
 
 git add package.json
 
-for workspace in "${workspaces[@]}"; do
-  pushd "${workspace}" >/dev/null
-  echo "crate: tree-sitter-lua"
-  echo "v${next_version}"
-  cargo bump "${next_version}"
-  echo
-  popd >/dev/null
-
-  git add "${workspace}/Cargo.toml"
-  npm version --no-git-tag-version --workspace="${workspace}" "${next_version}"
-  git add "${workspace}/package.json"
-done
+echo "crate: tree-sitter-lua"
+echo "v${next_version}"
+cargo bump "${next_version}"
+git add "Cargo.toml"
+echo
 
 git commit -m "chore: release v${next_version}"
 git tag "v${next_version}" -m "v${next_version}"
