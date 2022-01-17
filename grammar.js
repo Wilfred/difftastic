61dd7cf8889aafc183689ffa565b6413ffc1d2f8
John Drouhard <john@jmdtech.org>
2022-01-16 20:24:52 -0600

    Add support for default comparisons and the three way comparison operator

--- grammar.js
+++ grammar.js
@@ -4,6 +4,7 @@ const PREC = Object.assign(C.PREC, {
   LAMBDA: 18,
   NEW: C.PREC.CALL + 1,
   STRUCTURED_BINDING: -1,
+  THREE_WAY: C.PREC.RELATIONAL + 1,
 })
 
 module.exports = grammar(C, {
@@ -895,7 +896,7 @@ module.exports = grammar(C, {
       '...'
     ),
 
-    sizeof_expression: ($, original) => choice(
+    sizeof_expression: ($, original) => prec.right(PREC.SIZEOF, choice(
       original,
       seq(
         'sizeof', '...',
@@ -903,6 +904,15 @@ module.exports = grammar(C, {
         field('value', $.identifier),
         ')'
       ),
+    )),
+
+    binary_expression: ($, original) => choice(
+      original,
+      prec.left(PREC.THREE_WAY, seq(
+        field('left', $._expression),
+        field('operator', '<=>'),
+        field('right', $._expression)
+      ))
     ),
 
     argument_list: $ => seq(
@@ -989,6 +999,7 @@ module.exports = grammar(C, {
         '+=', '-=', '*=', '/=', '%=', '^=', '&=', '|=',
         '<<', '>>', '>>=', '<<=',
         '==', '!=', '<=', '>=',
+        '<=>',
         '&&', '||',
         '++', '--',
         ',',
