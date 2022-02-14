7baf7b0f076658ee6584a5ef72750aa750d42013
Wilfred Hughes <me@wilfred.me.uk>
2022-02-13 17:25:58 -0800

    Usage notes on regression test script

--- sample_files/compare_all.sh
+++ sample_files/compare_all.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Run difftastic on all the sample files, so we can see if any output
+# has been changed.
+#
+# # Generate a new compare.result.
+# $ sample_files/compare_all.sh | tee sample_files/compare.result
+#
+# # Compare it with the previous one.
+# $ difft sample_files/compare.expected sample_files/compare.result
+#
+# # If changes were expected, update the .expected file.
+# $ cp sample_files/compare.result sample_files/compare.expected
+
 set -e
 
 echo "==> Building difftastic"
