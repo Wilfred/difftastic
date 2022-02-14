3986bae12e5399d8b0a9b9dc71d74dbe2a88684c
Wilfred Hughes <me@wilfred.me.uk>
2022-02-13 17:06:11 -0800

    Helper script for regression testing on sample files

---
+++ sample_files/compare_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+echo "==> Building difftastic"
+cargo build --release
+
+echo "==> Check outputs"
+for before_f in sample_files/*before.*; do
+    after_f=${before_f/before/after}
+    echo "$before_f $after_f"
+
+    difft_out=$(./target/release/difft --color=always "$before_f" "$after_f" | md5sum)
+    echo "$difft_out"
+    echo
+done
