25433a0c3ddfa0b2d7e899351b05040e976e9a06
Wilfred Hughes <me@wilfred.me.uk>
2022-03-02 22:52:55 -0800

    Only consider novel trees at the top level
    Helps with #148. On my machine this changes slow_after.rs from 4.4 to
    3.4 seconds, but only reduced instruction count from 12.8B to 10.4B.
    
    The one changed case in sample_files/ is more granular and more
    readable.

--- src/syntax.rs
+++ src/syntax.rs
@@ -247,6 +247,10 @@ impl<'a> Syntax<'a> {
         }
     }
 
+    pub fn parent(&self) -> Option<&'a Syntax<'a>> {
+        self.info().parent.get()
+    }
+
     pub fn next_sibling(&self) -> Option<&'a Syntax<'a>> {
         self.info().next_sibling.get()
     }
