cc384bfa6d1639a065721a16aa5c7f88730ee201
Wilfred Hughes <me@wilfred.me.uk>
2022-03-09 22:45:53 -0800

    Add shrinking and get tests passing

--- src/unchanged.rs
+++ src/unchanged.rs
@@ -8,8 +8,10 @@ pub fn mark_unchanged<'a>(
     lhs_nodes: &[&'a Syntax<'a>],
     rhs_nodes: &[&'a Syntax<'a>],
 ) -> Vec<(Vec<&'a Syntax<'a>>, Vec<&'a Syntax<'a>>)> {
+    let (lhs_nodes, rhs_nodes) = shrink_unchanged_at_ends(lhs_nodes, rhs_nodes);
+
     let mut possibly_changed = vec![];
-    mark_unchanged_toplevel(lhs_nodes, rhs_nodes, &mut possibly_changed);
+    mark_unchanged_toplevel(&lhs_nodes, &rhs_nodes, &mut possibly_changed);
     possibly_changed
 }
 
@@ -133,8 +135,200 @@ impl<X: Eq, Y> PartialEq for EqOnFirstItem<X, Y> {
 }
 impl<X: Eq, Y> Eq for EqOnFirstItem<X, Y> {}
 
+/// If we're comparing two lists that have the same delimiters, mark
+/// the delimiters as unchanged and return the children.
+fn shrink_unchanged_delimiters<'a>(
+    lhs_nodes: &[&'a Syntax<'a>],
+    rhs_nodes: &[&'a Syntax<'a>],
+) -> (Vec<&'a Syntax<'a>>, Vec<&'a Syntax<'a>>) {
+    if let (
+        [Syntax::List {
+            open_content: lhs_open,
+            children: lhs_children,
+            close_content: lhs_close,
+            ..
+        }],
+        [Syntax::List {
+            open_content: rhs_open,
+            children: rhs_children,
+            close_content: rhs_close,
+            ..
+        }],
+    ) = (lhs_nodes, rhs_nodes)
+    {
+        if lhs_open == rhs_open && lhs_close == rhs_close {
+            lhs_nodes[0].set_change(ChangeKind::Unchanged(rhs_nodes[0]));
+            rhs_nodes[0].set_change(ChangeKind::Unchanged(lhs_nodes[0]));
+
+            return shrink_unchanged_at_ends(lhs_children, rhs_children);
+        }
+    }
+
+    (Vec::from(lhs_nodes), Vec::from(rhs_nodes))
+}
+
+/// Skip syntax nodes at the beginning or end that are obviously
+/// unchanged.
+///
+/// Set the ChangeKind on the definitely changed nodes, and return the
+/// nodes that may contain changes.
+fn shrink_unchanged_at_ends<'a>(
+    lhs_nodes: &[&'a Syntax<'a>],
+    rhs_nodes: &[&'a Syntax<'a>],
+) -> (Vec<&'a Syntax<'a>>, Vec<&'a Syntax<'a>>) {
+    let mut lhs_nodes = lhs_nodes;
+    let mut rhs_nodes = rhs_nodes;
+
+    while let (Some(lhs_node), Some(rhs_node)) = (lhs_nodes.first(), rhs_nodes.first()) {
+        if lhs_node.content_id() == rhs_node.content_id() {
+            {
+                lhs_node.set_change_deep(ChangeKind::Unchanged(rhs_node));
+                rhs_node.set_change_deep(ChangeKind::Unchanged(lhs_node));
+            };
+
+            lhs_nodes = &lhs_nodes[1..];
+            rhs_nodes = &rhs_nodes[1..];
+        } else {
+            break;
+        }
+    }
+
+    while let (Some(lhs_node), Some(rhs_node)) = (lhs_nodes.last(), rhs_nodes.last()) {
+        if lhs_node.content_id() == rhs_node.content_id() {
+            lhs_node.set_change_deep(ChangeKind::Unchanged(rhs_node));
+            rhs_node.set_change_deep(ChangeKind::Unchanged(lhs_node));
+
+            lhs_nodes = &lhs_nodes[..lhs_nodes.len() - 1];
+            rhs_nodes = &rhs_nodes[..rhs_nodes.len() - 1];
+        } else {
+            break;
+        }
+    }
+
+    if lhs_nodes.len() == 1 && rhs_nodes.len() == 1 {
+        shrink_unchanged_delimiters(lhs_nodes, rhs_nodes)
+    } else {
+        (Vec::from(lhs_nodes), Vec::from(rhs_nodes))
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use crate::{
+        guess_language,
+        syntax::init_all_info,
+        tree_sitter_parser::{from_language, parse},
+    };
+    use typed_arena::Arena;
+
+    #[test]
+    fn test_unchanged_at_start() {
+        let arena = Arena::new();
+        let config = from_language(guess_language::Language::EmacsLisp);
+
+        let lhs_nodes = parse(&arena, "unchanged A B", &config);
+        let rhs_nodes = parse(&arena, "unchanged X", &config);
+        init_all_info(&lhs_nodes, &rhs_nodes);
+
+        let (lhs_after_skip, rhs_after_skip) = shrink_unchanged_at_ends(&lhs_nodes, &rhs_nodes);
+
+        assert_eq!(
+            lhs_nodes[0].change(),
+            Some(ChangeKind::Unchanged(rhs_nodes[0]))
+        );
+        assert_eq!(
+            rhs_nodes[0].change(),
+            Some(ChangeKind::Unchanged(lhs_nodes[0]))
+        );
+
+        assert_eq!(lhs_after_skip.len(), 2);
+        assert_eq!(rhs_after_skip.len(), 1);
+    }
+
+    #[test]
+    fn test_unchanged_at_end() {
+        let arena = Arena::new();
+        let config = from_language(guess_language::Language::EmacsLisp);
+
+        let lhs_nodes = parse(&arena, "A B unchanged", &config);
+        let rhs_nodes = parse(&arena, "X unchanged", &config);
+        init_all_info(&lhs_nodes, &rhs_nodes);
+
+        let (lhs_after_skip, rhs_after_skip) = shrink_unchanged_at_ends(&lhs_nodes, &rhs_nodes);
+
+        assert_eq!(
+            lhs_nodes[2].change(),
+            Some(ChangeKind::Unchanged(rhs_nodes[1]))
+        );
+        assert_eq!(
+            rhs_nodes[1].change(),
+            Some(ChangeKind::Unchanged(lhs_nodes[2]))
+        );
+
+        assert_eq!(lhs_after_skip.len(), 2);
+        assert_eq!(rhs_after_skip.len(), 1);
+    }
+
+    #[test]
+    fn test_unchanged_outer_delimiters() {
+        let arena = Arena::new();
+        let config = from_language(guess_language::Language::EmacsLisp);
+
+        let lhs_nodes = parse(&arena, "(A)", &config);
+        let rhs_nodes = parse(&arena, "(B)", &config);
+        init_all_info(&lhs_nodes, &rhs_nodes);
+
+        let (lhs_after_skip, rhs_after_skip) = shrink_unchanged_delimiters(&lhs_nodes, &rhs_nodes);
+
+        // The only possibly changed nodes are inside the lists.
+        assert_eq!(lhs_after_skip.len(), 1);
+        assert!(matches!(lhs_after_skip[0], Syntax::Atom { .. }));
+
+        assert_eq!(rhs_after_skip.len(), 1);
+        assert!(matches!(rhs_after_skip[0], Syntax::Atom { .. }));
+
+        // The outer list delimiters are unchanged.
+        assert_eq!(
+            lhs_nodes[0].change(),
+            Some(ChangeKind::Unchanged(rhs_nodes[0]))
+        );
+        assert_eq!(
+            rhs_nodes[0].change(),
+            Some(ChangeKind::Unchanged(lhs_nodes[0]))
+        );
+
+        // The inner items haven't had their change set yet.
+        assert_eq!(lhs_after_skip[0].change(), None);
+        assert_eq!(rhs_after_skip[0].change(), None);
+    }
+
+    #[test]
+    fn test_shrink_unchanged() {
+        let arena = Arena::new();
+        let config = from_language(guess_language::Language::EmacsLisp);
+
+        let lhs_nodes = parse(&arena, "unchanged-before (more-unchanged (A))", &config);
+        let rhs_nodes = parse(&arena, "unchanged-before (more-unchanged (B))", &config);
+        init_all_info(&lhs_nodes, &rhs_nodes);
+
+        let (lhs_after_skip, rhs_after_skip) = shrink_unchanged_at_ends(&lhs_nodes, &rhs_nodes);
+
+        // The only possibly changed nodes are inside the lists.
+        assert_eq!(lhs_after_skip.len(), 1);
+        assert!(matches!(lhs_after_skip[0], Syntax::Atom { .. }));
+
+        assert_eq!(rhs_after_skip.len(), 1);
+        assert!(matches!(rhs_after_skip[0], Syntax::Atom { .. }));
+
+        // The inner items haven't had their change set yet.
+        assert_eq!(lhs_after_skip[0].change(), None);
+        assert_eq!(rhs_after_skip[0].change(), None);
+    }
+}
+
+#[cfg(test)]
+mod tests2 {
     use super::*;
     use crate::{
         guess_language,
@@ -257,17 +451,25 @@ mod tests {
     }
 
     #[test]
-    fn test_unchanged_ids() {
+    fn test_split_definitely_unchanged() {
         let arena = Arena::new();
         let config = from_language(guess_language::Language::EmacsLisp);
 
-        let lhs_nodes = parse(&arena, "novel-lhs (unchanged ()) novel-lhs-2", &config);
-        let rhs_nodes = parse(&arena, "novel-rhs (unchanged ()) novel-rhs-2", &config);
+        let lhs_nodes = parse(
+            &arena,
+            "novel-lhs (unchanged (1 2 3 4 5 6 7 8 9 10)) novel-lhs-2",
+            &config,
+        );
+        let rhs_nodes = parse(
+            &arena,
+            "novel-rhs (unchanged (1 2 3 4 5 6 7 8 9 10)) novel-rhs-2",
+            &config,
+        );
         init_all_info(&lhs_nodes, &rhs_nodes);
 
-        let unchanged = mark_unchanged(&lhs_nodes, &rhs_nodes);
+        let res = mark_unchanged(&lhs_nodes, &rhs_nodes);
         assert_eq!(
-            unchanged,
+            res,
             vec![
                 (vec![lhs_nodes[0]], vec![rhs_nodes[0]]),
                 (vec![lhs_nodes[2]], vec![rhs_nodes[2]])
@@ -283,25 +485,6 @@ mod tests {
             Some(ChangeKind::Unchanged(lhs_nodes[1]))
         );
     }
-
-    #[test]
-    fn test_split_definitely_unchanged() {
-        let arena = Arena::new();
-        let config = from_language(guess_language::Language::EmacsLisp);
-
-        let lhs_nodes = parse(&arena, "novel-lhs (unchanged ()) novel-lhs-2", &config);
-        let rhs_nodes = parse(&arena, "novel-rhs (unchanged ()) novel-rhs-2", &config);
-        init_all_info(&lhs_nodes, &rhs_nodes);
-
-        let res = mark_unchanged(&lhs_nodes, &rhs_nodes);
-        assert_eq!(
-            res,
-            vec![
-                (vec![lhs_nodes[0]], vec![rhs_nodes[0]]),
-                (vec![lhs_nodes[2]], vec![rhs_nodes[2]])
-            ]
-        );
-    }
     #[test]
     fn test_split_definitely_unchanged_multiple() {
         let arena = Arena::new();
@@ -309,12 +492,12 @@ mod tests {
 
         let lhs_nodes = parse(
             &arena,
-            "novel-lhs unchanged-1 unchanged-2 novel-lhs-2",
+            "novel-lhs (unchanged-1 (1 2 3 4 5 6 7 8 9 10)) (unchanged-2 (1 2 3 4 5 6 7 8 9 10)) novel-lhs-2",
             &config,
         );
         let rhs_nodes = parse(
             &arena,
-            "novel-rhs unchanged-1 unchanged-2 novel-rhs-2",
+            "novel-rhs (unchanged-1 (1 2 3 4 5 6 7 8 9 10)) (unchanged-2 (1 2 3 4 5 6 7 8 9 10)) novel-rhs-2",
             &config,
         );
         init_all_info(&lhs_nodes, &rhs_nodes);
