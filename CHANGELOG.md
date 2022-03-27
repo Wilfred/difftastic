b6cd8c9a42b02a6fbb5ae8b1e015775e9a4a2402
Wilfred Hughes <me@wilfred.me.uk>
2022-03-26 23:50:55 -0700

    Update Clojure highlighting to match upstreamed PR
    See https://github.com/sogaiu/tree-sitter-clojure/pull/20

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,7 @@
 
 Difftastic no longer shows "1/1" when a file only has a single hunk.
 
-Improved Scala syntax highlighting.
+Improved Clojure and Scala syntax highlighting.
 
 When a file is entirely unchanged, difftastic now shows "no changes"
 even if it successfully parsed. Previously it would only show "no
--- vendor/highlights/clojure.scm
+++ vendor/highlights/clojure.scm
@@ -1,7 +1,32 @@
 ;; Basic, limited highlighting for Clojure.
 ;; TODO: submit upstream.
 
-(comment) @comment
-(str_lit) @string
+;; Literals
+
+(num_lit) @number
+
+[
+  (char_lit)
+  (str_lit)
+] @string
+
+[
+ (bool_lit)
+ (nil_lit)
+] @constant.builtin
 
 (kwd_lit) @constant
+
+;; Comments
+
+(comment) @comment
+
+;; Treat quasiquotation as operators for the purpose of highlighting.
+
+[
+ "'"
+ "`"
+ "~"
+ "@"
+ "~@"
+ ] @operator
