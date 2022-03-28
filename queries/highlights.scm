aceea17420f9620e62afcd0385ab61d6aeaa52c6
sogaiu <983021772@users.noreply.github.com>
2022-03-28 15:17:45 +0900

    Add queries/highlights.scm

---
+++ queries/highlights.scm
@@ -0,0 +1,23 @@
+(num_lit) @number
+
+[
+ (buf_lit)
+ (long_buf_lit)
+ (long_str_lit)
+ (str_lit)
+] @string
+
+[
+ (bool_lit)
+ (nil_lit)
+] @constant.builtin
+
+(kwd_lit) @constant
+
+;; Treat quasiquotation as operators for the purpose of highlighting.
+
+[
+ "'"
+ "~"
+ ","
+] @operator
