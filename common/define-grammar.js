47064d7f9e0154d0a7d7251a75994c603b742048
Wsevolod <kefirchik3@gmail.com>
2022-04-13 19:44:02 +0300

    Handle `from_clause` in `export type` statement (#208)

--- common/define-grammar.js
+++ common/define-grammar.js
@@ -273,7 +273,13 @@ module.exports = function defineGrammar(dialect) {
 
       export_statement: ($, previous) => choice(
         previous,
-        seq('export', 'type', $.export_clause, $._semicolon),
+        seq(
+          'export',
+          'type',
+          $.export_clause,
+          optional($._from_clause),
+          $._semicolon
+        ),
         seq('export', '=', $.expression, $._semicolon),
         seq('export', 'as', 'namespace', $.identifier, $._semicolon),
       ),
