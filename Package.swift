4140d566eb446658187cdd0b72de7581cc686408
Lukas Pistrol <lukas@pistrol.com>
2022-05-30 01:14:17 +0200

    added spm support

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "TreeSitterJava",
+    products: [
+        .library(name: "TreeSitterJava", targets: ["TreeSitterJava"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "TreeSitterJava",
+                path: ".",
+                exclude: [
+                    "binding.gyp",
+                    "bindings",
+                    "Cargo.toml",
+                    "corpus",
+                    "grammar.js",
+                    "LICENSE",
+                    "package.json",
+                    "README.md",
+                ],
+                sources: [
+                    "src/parser.c",
+                    "src/scanner.cc",
+                ],
+                resources: [
+                    .copy("queries")
+                ],
+                publicHeadersPath: "bindings/swift",
+                cSettings: [.headerSearchPath("src")])
+    ]
+) 
---
+++ bindings/swift/TreeSitterJava/java.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_JAVA_H_
+#define TREE_SITTER_JAVA_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern TSLanguage *tree_sitter_java();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_JAVA_H_ 
