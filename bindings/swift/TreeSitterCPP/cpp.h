38d8b495bd43977498f0eb122e0f9cfef8526d18
Lukas Pistrol <l.pistrol@gmail.com>
2022-05-30 20:35:53 +0200

    added spm support (#160)

---
+++ bindings/swift/TreeSitterCPP/cpp.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_CPP_H_
+#define TREE_SITTER_CPP_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern TSLanguage *tree_sitter_cpp();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_CPP_H_
