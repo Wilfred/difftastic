79fa4e729604746d8838d6368db8bc71ec86df64
6cdh <cd6cdh@gmail.com>
2022-06-07 20:53:03 +0800

    refactor the whole code This commit will reduce 80% generated code size and improve performance.

--- grammar.js
+++ grammar.js
@@ -1,48 +1,64 @@
-const INTRA_WHITERPACE = /[ \n\t\p{Zs}]/;
-const LINE_ENDING = /\n|\r|(\r\n)|(\r\u{85})|\u2028|\u{85}|/;
-
 const PREC = {
-  first: 100,
-  block_comment: 2,
-  symbol: -1,
+  first: $ => prec(100, $),
+  last: $ => prec(-1, $),
 };
+
+const LEAF = {
+  // Unicode whitespace
+  whitespace: /[ \r\n\t\f\v\p{Zs}\p{Zl}\p{Zp}]+/,
+  intra_whitespace: /[ \n\t\p{Zs}]/,
+  line_ending: /[\n\r\u{2028}\u{0085}]|(\r\n)|(\r\u{0085})/,
+  any_char: /.|[\r\n\u{85}\u{2028}\u{2029}]/,
+
+  symbol_element: /[^ \r\n\t\f\v\p{Zs}\p{Zl}\p{Zp}#;"'`,(){}\[\]\\]/,
+};
+
+function paren(rule) {
+  return choice(
+    seq('(', rule, ')'),
+    seq('[', rule, ']'),
+    seq('{', rule, '}'));
+}
 
 module.exports = grammar({
   name: "scheme",
 
-  extras: $ => [
-    // Unicode whitespace
-    /[\r\n\t\f\v ]|\p{Zs}|\p{Zl}|\p{Zp}/,
-    $.comment,
-
-    // `block_comment` have to be placed here.
-    // It's a workaround for 'no entry found for key' error,
-    // see https://github.com/tree-sitter/tree-sitter/issues/768
-    //
-    // We also removed it from `$.comment` to avoid conflict.
-    // Finally, we can't hide `block_comment`.
-    // It means we have two comment now: `comment` and `block_comment`.
-    $.block_comment,
-    $.directive,
-  ],
+  extras: _ => [],
 
   rules: {
-    program: $ => repeat($._datum),
-    _datum: $ => choice($._simple_datum, $._compound_datum),
+    program: $ => repeat($._token),
+
+    _token: $ =>
+      choice(
+        LEAF.whitespace,
+        $._all_comment,
+        $.directive,
+        $._datum),
+
+    _skip: $ => choice(LEAF.whitespace, $._all_comment),
+
+    _all_comment: $ =>
+      choice(
+        $.comment,
+        $.block_comment),
 
     comment: $ =>
       choice(
-        token(seq(";", /.*/)),
-        seq("#;", $._datum)),
+        /;.*/,
+        seq("#;", repeat($._skip), $._datum)),
 
     directive: $ =>
-      seq("#!", $.symbol),
+      seq("#!", repeat($._skip), $.symbol),
 
     block_comment: $ =>
-      prec(PREC.block_comment,
-        seq("#|",
-          repeat(/./),
-          prec(PREC.first, "|#"))),
+      seq("#|",
+        repeat(
+          choice(
+            PREC.first($.block_comment),
+            LEAF.any_char)),
+        PREC.first("|#")),
+
+    _datum: $ => choice($._simple_datum, $._compound_datum),
 
     // simple datum {{{
 
@@ -54,34 +70,7 @@ module.exports = grammar({
         $.string,
         $.symbol),
 
-    boolean: _ => choice("#t", "#f", "#T", "#F"),
-
-    character: _ =>
-      token(
-        choice(
-          "#\\space", "#\\newline",
-          "#\\alarm", "#\\backspace",
-          "#\\delete", "#\\esc",
-          "#\\linefeed", "#\\page",
-          "#\\return", "#\\space", "#\\tab", "#\\vtab",
-          "#\\nul",
-          seq("#\\x", /[0-9a-fA-F]+/),
-          /#\\./)),
-
-    string: $ =>
-      seq(
-        '"',
-        repeat(
-          choice(
-            $.escape_sequence,
-            token.immediate(prec(1, /[^"\\]/)))),
-        '"'),
-    escape_sequence: _ =>
-      token.immediate(
-        choice(
-          seq("\\", /["\\abfnrtv]/),
-          seq("\\", repeat(INTRA_WHITERPACE), LINE_ENDING, repeat(INTRA_WHITERPACE)),
-          seq("\\x", /[0-9a-fA-F]+/, ";"))),
+    boolean: _ => token(choice("#t", "#f", "#T", "#F")),
 
     number: _ =>
       token(
@@ -91,49 +80,37 @@ module.exports = grammar({
           number_base(10),
           number_base(16))),
 
-    symbol: _ => {
-      const initial =
+    character: _ =>
+      token(
+        seq(
+          "#\\",
+          choice(
+            "space", "newline",
+            "alarm", "backspace",
+            "delete", "esc",
+            "linefeed", "page",
+            "return", "space", "tab", "vtab",
+            "nul",
+            /x[0-9a-fA-F]+/,
+            /./))),
+
+    string: $ =>
+      seq(
+        '"',
+        repeat(
+          choice(
+            $.escape_sequence,
+            /[^"\\]+/)),
+        '"'),
+
+    escape_sequence: _ =>
+      token(
         choice(
-          /[a-zA-Z!$%&*/:<=>?^_~]/,
-          /\p{Co}/,
-          /\p{Ll}|\p{Lm}|\p{Lo}|\p{Lt}|\p{Lu}/,
-          /\p{Mn}|\p{Nl}|\p{No}/,
-          /\p{Pc}|\p{Pd}/,
-          /\p{Sc}|\p{Sk}|\p{Sm}|\p{So}/,
-          /\p{Po}/,
+          /\\["\\abfnrtv]/,
+          seq("\\", repeat(LEAF.intra_whitespace), LEAF.line_ending, repeat(LEAF.intra_whitespace)),
+          /\\x[0-9a-fA-F]+;/)),
 
-          // codepoint range but tree-sitter doesn't support.
-          // See https://unicode.org/Public/14.0.0/ucd/UnicodeData.txt
-          // and search "first>".
-          //
-          // The codepoint range should not exceed \u{FFFF} because
-          // tree-sitter doesn't support it.
-          //
-          // See Also https://github.com/tree-sitter/tree-sitter/issues/1432
-
-          /[\u3400-\u4DBF]/, // Lo: CJK Ideograph Extension A
-          /[\u4E00-\u9FFF]/, // Lo: CJK Ideograph
-          /[\uAC00-\uD7A3]/, // Lo: Hangul Syllable
-          /[\uE000-\uF8FF]/, // Co: Private Use
-
-          seq("\\x", /[0-9a-fA-F]+/, ";"));
-
-      const subsequent =
-        choice(
-          initial,
-          /[0-9.@+-]/,
-          /\p{Mc}|\p{Me}|\p{Nd}/);
-
-      return token(prec(PREC.symbol,
-        choice(
-          "...",
-          "+",
-          "-",
-          seq("->", repeat(subsequent)),
-          seq(
-            initial,
-            repeat(subsequent)))));
-    },
+    symbol: _ => PREC.last(token(repeat1(LEAF.symbol_element))),
 
     // simple datum }}}
 
@@ -145,13 +122,9 @@ module.exports = grammar({
         $.abbreviation,
         $.list),
 
-    list: $ =>
-      choice(
-        par(repeat($._datum)),
-        par(seq(repeat1($._datum), /\\./, $._datum))),
+    list: $ => paren(repeat($._token)),
 
-
-    abbreviation: $ => choice(seq($._abbreviation_prefix, $._datum)),
+    abbreviation: $ => seq($._abbreviation_prefix, repeat($._skip), $._datum),
     _abbreviation_prefix: _ => choice("'", "`", ",", ",@", "#'", "#`", "#,", "#,@"),
 
     _special_form: $ =>
@@ -159,19 +132,14 @@ module.exports = grammar({
         $.vector,
         $.byte_vector),
 
-    vector: $ => seq("#(", repeat($._datum), ")"),
+    vector: $ => seq("#(", repeat($._token), ")"),
 
-    byte_vector: $ => seq("#vu8(", repeat($.number), ")"),
+    byte_vector: $ => seq("#vu8(", repeat($._token), ")"),
     // compound datum }}}
   },
 });
 
-function par(rule) {
-  return choice(
-    seq('(', rule, ')'),
-    seq('[', rule, ']'),
-    seq('{', rule, '}'));
-}
+// number {{{
 
 function number_base(n) {
   const radixn = {
@@ -230,3 +198,5 @@ function number_base(n) {
 
   return seq(prefix, complex);
 }
+
+// number }}}
