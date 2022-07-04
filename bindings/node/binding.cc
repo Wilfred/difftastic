b766bd4896c4cd2bb9ae0f51f14bcec38842936e
Uy Ha <hchanuy@gmail.com>
2022-07-04 12:42:23 +0700

    fix: regenerate files for updated name
    Co-authored-by: ShootingStarDragons <ShootingStarDragons@protonmail.com>

--- bindings/node/binding.cc
+++ bindings/node/binding.cc
@@ -4,7 +4,7 @@
 
 using namespace v8;
 
-extern "C" TSLanguage * tree_sitter_CMake();
+extern "C" TSLanguage * tree_sitter_cmake();
 
 namespace {
 
@@ -17,12 +17,12 @@ void Init(Local<Object> exports, Local<Object> module) {
 
   Local<Function> constructor = Nan::GetFunction(tpl).ToLocalChecked();
   Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
-  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_CMake());
+  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_cmake());
 
-  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("CMake").ToLocalChecked());
+  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("cmake").ToLocalChecked());
   Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
 }
 
-NODE_MODULE(tree_sitter_CMake_binding, Init)
+NODE_MODULE(tree_sitter_cmake_binding, Init)
 
 }  // namespace
--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,4 +1,4 @@
-//! This crate provides CMake language support for the [tree-sitter][] parsing library.
+//! This crate provides cmake language support for the [tree-sitter][] parsing library.
 //!
 //! Typically, you will use the [language][language func] function to add this language to a
 //! tree-sitter [Parser][], and then use the parser to parse some code:
@@ -6,7 +6,7 @@
 //! ```
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_CMake::language()).expect("Error loading CMake grammar");
+//! parser.set_language(tree_sitter_cmake::language()).expect("Error loading cmake grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
@@ -18,14 +18,14 @@
 use tree_sitter::Language;
 
 extern "C" {
-    fn tree_sitter_CMake() -> Language;
+    fn tree_sitter_cmake() -> Language;
 }
 
 /// Get the tree-sitter [Language][] for this grammar.
 ///
 /// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
 pub fn language() -> Language {
-    unsafe { tree_sitter_CMake() }
+    unsafe { tree_sitter_cmake() }
 }
 
 /// The content of the [`node-types.json`][] file for this grammar.
@@ -47,6 +47,6 @@ mod tests {
         let mut parser = tree_sitter::Parser::new();
         parser
             .set_language(super::language())
-            .expect("Error loading CMake language");
+            .expect("Error loading cmake language");
     }
 }
