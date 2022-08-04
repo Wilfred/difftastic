05e3631c6a0701c1fa518b0fee7be95a2ceef5e2
Michael Davis <mcarsondavis@gmail.com>
2022-08-04 13:24:46 -0500

    Fix '#eq?' predicate in injections query
    Fixes #39

--- queries/injections.scm
+++ queries/injections.scm
@@ -2,6 +2,6 @@
 ((sigil
   (sigil_name) @_sigil_name
   (quoted_content) @injection.content)
- (#eq @_sigil_name "H")
+ (#eq? @_sigil_name "H")
  (#set! injection.language "heex")
  (#set! injection.combined))
