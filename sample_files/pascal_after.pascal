3c51f58d8e86b56cbef6550c0d0543c309b97f64
Wilfred Hughes <me@wilfred.me.uk>
2022-09-13 00:05:01 -0700

    Add Pascal support
    Fixes #365

---
+++ sample_files/pascal_after.pascal
@@ -0,0 +1,29 @@
+// From Rosetta Code https://rosettacode.org/wiki/Evaluate_binomial_coefficients#Delphi
+// Under CC-BY-SA license.
+
+program Binomial;
+
+{$APPTYPE CONSOLE}
+
+function BinomialCoff(N, K: Cardinal): Cardinal;
+var
+  L: Cardinal;
+
+begin
+  if N < K then
+    Result:= 0      // Error
+  else begin
+    Result:= 1;
+    L:= 0;
+    while L < K do begin
+      Result:= Result * (N - 1);
+      Inc(L);
+      Result:= Result div L;
+    end;
+  end;
+end;
+
+begin
+  Writeln('C(5,3) is ', BinomialCoff(5, 3));
+  ReadLn;
+end.
--- src/diff/sliders.rs
+++ src/diff/sliders.rs
@@ -67,10 +67,7 @@ fn prefer_outer_delimiter(language: guess_language::Language) -> bool {
         // For everything else, prefer the inner delimiter. These
         // languages have syntax like `foo(bar)` or `foo[bar]` where
         // the inner delimiter is more relevant.
-        Bash | C | CMake | CPlusPlus | CSharp | Css | Dart | Elixir | Elm | Elvish | Gleam | Go
-        | Hack | Haskell | Html | Java | JavaScript | Jsx | Julia | Kotlin | Lua | Make | Nix
-        | OCaml | OCamlInterface | Perl | Php | Python | Qml | Ruby | Rust | Scala | Swift
-        | Tsx | TypeScript | Yaml | Zig => false,
+        _ => false,
     }
 }
 
