3b8eb02597599ee38cfcc2c0fea76dc37a7472eb
Matt Massicotte <85322+mattmassicotte@users.noreply.github.com>
2022-09-30 06:20:27 -0400

    SPM support (#41)

---
+++ bindings/swift/TreeSitterElixir/elixir.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_ELIXIR_H_
+#define TREE_SITTER_ELIXIR_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern TSLanguage *tree_sitter_elixir();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_ELIXIR_H_
