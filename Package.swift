65d89b7bbb0541509159b1c1046c84cbea0e345d
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-10-05 13:33:55 -0400

    SPM support

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "TreeSitterPerl",
+    platforms: [.macOS(.v10_13), .iOS(.v11)],
+    products: [
+        .library(name: "TreeSitterPerl", targets: ["TreeSitterPerl"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "TreeSitterPerl",
+                path: ".",
+                exclude: [
+                    "binding.gyp",
+                    "bindings",
+                    "Cargo.toml",
+                    "examples",
+                    "grammar.js",
+                    "LICENSE",
+                    "package-lock.json",
+                    "package.json",
+                    "README.md",
+                    "src/grammar.json",
+                    "src/node-types.json",
+                ],
+                sources: [
+                    "src/parser.c",
+                    "src/scanner.cc",
+                ],
+                publicHeadersPath: "bindings/swift",
+                cSettings: [.headerSearchPath("src")])
+    ]
+)
