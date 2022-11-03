3ddebf46e6fe5e27fa03dc07a829a766b9979c8d
Max Brunsfeld <maxbrunsfeld@gmail.com>
2022-11-03 10:20:34 -0700

    Remove special cases for built-in attributes (#163)
    Parse all attribute using the general attribute syntax

--- grammar.js
+++ grammar.js
@@ -36,54 +36,6 @@ const numeric_types = [
 
 const primitive_types = numeric_types.concat(['bool', 'str', 'char'])
 
-const built_in_attributes = [
-  'cfg',
-  'cfg_attr',
-  'test',
-  'ignore',
-  'should_panic',
-  'derive',
-  'automatically_derived',
-  'macro_export',
-  'macro_use',
-  'proc_macro',
-  'proc_macro_derive',
-  'proc_macro_attribute',
-  'allow',
-  'warn',
-  'deny',
-  'forbid',
-  'deprecated',
-  'must_use',
-  'link',
-  'link_name',
-  'no_link',
-  'repr',
-  'crate_type',
-  'no_main',
-  'export_name',
-  'link_section',
-  'no_mangle',
-  'used',
-  'crate_name',
-  'inline',
-  'cold',
-  'no_builtins',
-  'target_feature',
-  'track_caller',
-  'doc',
-  'no_std',
-  'no_implicit_prelude',
-  'path',
-  'recursion_limit',
-  'type_length_limit',
-  'panic_handler',
-  'global_allocator',
-  'windows_subsystem',
-  'feature',
-  'non_exhaustive'
-]
-
 module.exports = grammar({
   name: 'rust',
 
@@ -259,7 +211,7 @@ module.exports = grammar({
     attribute_item: $ => seq(
       '#',
       '[',
-      $._attr,
+      $.attribute,
       ']'
     ),
 
@@ -267,51 +219,16 @@ module.exports = grammar({
       '#',
       '!',
       '[',
-      $._attr,
+      $.attribute,
       ']'
     ),
 
-    _attr: $ => choice(
-      alias($.built_in_attr, $.meta_item),
-      alias($.custom_attr, $.attr_item),
-    ),
-
-    custom_attr: $ => seq(
+    attribute: $ => seq(
       $._path,
       optional(choice(
         seq('=', field('value', $._expression)),
         field('arguments', alias($.delim_token_tree, $.token_tree))
       ))
-    ),
-
-    built_in_attr: $ => seq(
-      $._built_in_attr_path,
-      optional(choice(
-        seq('=', field('value', $._expression)),
-        field('arguments', $.meta_arguments)
-      ))
-    ),
-
-    _built_in_attr_path: $ => choice(
-      ...built_in_attributes.map(name => alias(name, $.identifier))
-    ),
-
-    meta_item: $ => seq(
-      $._path,
-      optional(choice(
-        seq('=', field('value', $._expression)),
-        field('arguments', $.meta_arguments)
-      ))
-    ),
-
-    meta_arguments: $ => seq(
-      '(',
-      sepBy(',', choice(
-        $.meta_item,
-        $._literal
-      )),
-      optional(','),
-      ')'
     ),
 
     mod_item: $ => seq(
