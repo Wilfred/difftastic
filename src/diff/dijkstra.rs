0eb70c84acc469fdc9eec787a7dd0e80dc63ab91
QuarticCat <QuarticCat@pm.me>
2022-11-20 22:38:52 +0800

    Fix arena reservation

--- src/diff/dijkstra.rs
+++ src/diff/dijkstra.rs
@@ -123,7 +123,7 @@ pub fn mark_syntax<'a>(
     let size_hint = lhs_node_count * rhs_node_count;
 
     let start = Vertex::new(lhs_syntax, rhs_syntax);
-    let vertex_arena = Bump::with_capacity(size_hint);
+    let vertex_arena = Bump::with_capacity(size_hint * std::mem::size_of::<Vertex>());
 
     let route = shortest_path(start, &vertex_arena, size_hint, graph_limit)?;
 
