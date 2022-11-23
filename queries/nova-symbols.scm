1d50be15d9976b10cb6b27133f9fa4b5a958f26e
Garrett D'Amore <garrett@damore.org>
2022-11-23 15:56:45 -0800

    Anchor some symbol names

--- queries/nova-symbols.scm
+++ queries/nova-symbols.scm
@@ -1,22 +1,20 @@
-((module_def (module_declaration) . (module_fqn) @name) @subtree (#set! role package))
+((module_def (module_declaration (module_fqn) @name)) @subtree (#set! role package))
 
-((struct_declaration (identifier) @name) @subtree (#set! role struct))
-((enum_declaration (identifier) @name) @subtree (#set! role enum))
+((struct_declaration (struct) . (identifier) @name) @subtree (#set! role struct))
+((enum_declaration (enum) . (identifier) @name) @subtree (#set! role enum))
 ((enum_member (identifier) @name) (#set! role enum-member))
 
-((class_declaration (identifier) @name) @subtree (#set! role class))
+((class_declaration (class) . (identifier) @name) @subtree (#set! role class))
 ((constructor (this) @name) @subtree (#set! role constructor))
 ((destructor (this) @name) @subtree (#set! role destructor))
 ((postblit (this) @name) @subtree (#set! role constructor))
 
-((manifest_constant (identifier) @name) @subtree (#set! role constant))
+((manifest_constant (enum) . (identifier) @name) @subtree (#set! role constant))
 
 ((function_declaration (identifier) @name) @subtree (#set! role function-or-method))
 
 ((aggregate_body (variable_declaration (declarator (identifier) @name) @subtree (#set! role property))))
 
-((variable_declaration (declarator (identifier) @name) @subtree (#set! role variable)))
-
-((union_declaration (identifier) @name) @subtree (#set! role union))
+((union_declaration (union) . (identifier) @name) @subtree (#set! role union))
 
 ((alias_declaration (alias_initializer . (identifier) @name) @subtree (#set! role type)))
