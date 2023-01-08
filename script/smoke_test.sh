31f172c2957c6c9bddd896f4b9f0cffe91b085f0
Anton Sviridov <keynmol@gmail.com>
2023-01-08 19:31:54 +0000

    Make smoke_test.sh script better for diffing results

--- script/smoke_test.sh
+++ script/smoke_test.sh
@@ -21,10 +21,18 @@ failed=0
 run_tree_sitter () {
   local source_dir=$1
   local expected=$2
-  echo "Parse $source_dir:"
-  out=$(npm exec -c "tree-sitter parse $source_dir/**/*.scala --quiet --stat" || true)
-  actual=$(echo $out | rev | cut -d' ' -f1 | rev | sed 's/.$//')
-  echo "$out"
+  local name=$3
+  cmd="npm exec -c 'tree-sitter parse $source_dir/**/*.scala --quiet --stat' | sort | sed 's%$source_dir%%g'"
+  echo
+  echo "Parse $source_dir: $cmd"
+  out=$((eval $cmd) || true)
+
+  if [ ! -e "$PRODUCE_REPORTS" ]; then
+    echo "$out" | sed G | sed -E 's/([0-9]+) ms//' | grep -v 'success percentage' > "report-$name.txt" 
+  fi
+
+  actual=$(echo "$out" | grep 'success percentage:' | rev | cut -d' ' -f1 | rev | sed 's/%//g' )
+  echo $actual
   if (( $(echo "$actual > $expected" |bc -l) )); then
     # See https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#example-creating-an-annotation-for-an-error
     echo -e "::notice file=grammar.js,line=1::ok, ${source_dir}: ${actual}%"
@@ -34,9 +42,9 @@ run_tree_sitter () {
   fi
 }
 
-run_tree_sitter $SCALA_SCALA_DIR/src/library/  $SCALA_SCALA_LIBRARY_EXPECTED
-run_tree_sitter $SCALA_SCALA_DIR/src/compiler/ $SCALA_SCALA_COMPILER_EXPECTED
-run_tree_sitter $DOTTY_DIR/compiler/           $DOTTY_COMPILER_EXPECTED
+run_tree_sitter $SCALA_SCALA_DIR/src/library/  $SCALA_SCALA_LIBRARY_EXPECTED   scala2-library
+run_tree_sitter $SCALA_SCALA_DIR/src/compiler/ $SCALA_SCALA_COMPILER_EXPECTED  scala2-compiler
+run_tree_sitter $DOTTY_DIR/compiler/           $DOTTY_COMPILER_EXPECTED        dotty-compiler
 
 if (( $failed > 0 )); then
   exit 1
