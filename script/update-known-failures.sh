88ed46b8c32782379d5cab8bc9ea111ca7ad7dcb
Boris Verkhovskiy <boris.verk@gmail.com>
2023-02-02 00:27:53 +0200

    Parse Bash's tests (#135)

--- script/update-known-failures.sh
+++ script/update-known-failures.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+shopt -s globstar
 
 tree-sitter parse -q -t   \
-  examples/**/*.sh        \
-  examples/**/*.bash      \
+  examples/bash-it/**/*.bash \
+  examples/bash-it/**/*.sh \
+  examples/bash/tests/*.tests \
+  examples/*.sh   \
   | egrep 'ERROR|MISSING' \
   | tee >(cut -d' ' -f1 | sort > script/known-failures.txt)
