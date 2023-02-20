49afd6f1431baa4648e06ba7d7a869b3c693b2e8
Amaan Qureshi <amaanq12@gmail.com>
2023-02-20 07:25:19 -0500

    feat: update bindings, Rust binding includes relevant queries

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -2,7 +2,7 @@ fn main() {
     let src_dir = std::path::Path::new("src");
 
     let mut c_config = cc::Build::new();
-    c_config.include(&src_dir);
+    c_config.include(src_dir);
     c_config
         .flag_if_supported("-Wno-unused-parameter")
         .flag_if_supported("-Wno-unused-but-set-variable")
