adb25ed7f5efe318fb8aa54abe821b9bd0d61b0b
Wilfred Hughes <me@wilfred.me.uk>
2023-02-22 00:01:36 -0800

    Add CLI test for --exit-code

--- tests/cli.rs
+++ tests/cli.rs
@@ -8,3 +8,19 @@ fn no_such_files() {
     cmd.arg("no_such_file").arg("no_such_file_either");
     cmd.assert().failure().code(2);
 }
+
+#[test]
+fn has_changes_default_exit_code() {
+    let mut cmd = Command::cargo_bin("difft").unwrap();
+
+    cmd.arg("sample_files/simple_before.js").arg("sample_files/simple_after.js");
+    cmd.assert().success();
+}
+
+#[test]
+fn has_changes_requested_exit_code() {
+    let mut cmd = Command::cargo_bin("difft").unwrap();
+
+    cmd.arg("--exit-code").arg("sample_files/simple_before.js").arg("sample_files/simple_after.js");
+    cmd.assert().failure().code(1);
+}
