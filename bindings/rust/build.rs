88a677b791012b95210bac5c8967c0dac5e776e4
Amaan Qureshi <amaanq12@gmail.com>
2023-02-21 22:27:43 -0500

    style: update rust bindings with README and better file docs

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -10,31 +10,6 @@ fn main() {
     let parser_path = src_dir.join("parser.c");
     c_config.file(&parser_path);
 
-    // If your language uses an external scanner written in C,
-    // then include this block of code:
-
-    /*
-    let scanner_path = src_dir.join("scanner.c");
-    c_config.file(&scanner_path);
-    println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
-    */
-
     c_config.compile("parser");
     println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
-
-    // If your language uses an external scanner written in C++,
-    // then include this block of code:
-
-    /*
-    let mut cpp_config = cc::Build::new();
-    cpp_config.cpp(true);
-    cpp_config.include(&src_dir);
-    cpp_config
-        .flag_if_supported("-Wno-unused-parameter")
-        .flag_if_supported("-Wno-unused-but-set-variable");
-    let scanner_path = src_dir.join("scanner.cc");
-    cpp_config.file(&scanner_path);
-    cpp_config.compile("scanner");
-    println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
-    */
 }
--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,4 +1,9 @@
-//! This crate provides smali language support for the [tree-sitter][] parsing library.
+// ------------------------------------------------------------------------------------------------
+// Copyright © 2023, Amaan Qureshi <amaanq12@gmail.com>, Yotam Nachum <me@yotam.net>
+// See the LICENSE file in this repo for license details.
+// ------------------------------------------------------------------------------------------------
+
+//! This crate provides Smali language support for the [tree-sitter][] parsing library.
 //!
 //! Typically, you will use the [language][language func] function to add this language to a
 //! tree-sitter [Parser][], and then use the parser to parse some code:
@@ -6,7 +11,7 @@
 //! ```
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_smali::language()).expect("Error loading smali grammar");
+//! parser.set_language(tree_sitter_smali::language()).expect("Error loading Smali grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
@@ -28,17 +33,28 @@ pub fn language() -> Language {
     unsafe { tree_sitter_smali() }
 }
 
+/// The source of the Rust tree-sitter grammar description.
+pub const GRAMMAR: &str = include_str!("../../grammar.js");
+
+/// The folds query for this language.
+pub const FOLDS_QUERY: &str = include_str!("../../queries/folds.scm");
+
+/// The syntax highlighting query for this language.
+pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
+
+/// The indents query for this language.
+pub const INDENTS_QUERY: &str = include_str!("../../queries/indents.scm");
+
+/// The injection query for this language.
+pub const INJECTIONS_QUERY: &str = include_str!("../../queries/injections.scm");
+
+/// The symbol tagging query for this language.
+pub const LOCALS_QUERY: &str = include_str!("../../queries/locals.scm");
+
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
 /// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
 pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
-
-// Uncomment these to include any queries that this grammar contains
-
-pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
-pub const INJECTIONS_QUERY: &str = include_str!("../../queries/injections.scm");
-pub const LOCALS_QUERY: &str = include_str!("../../queries/locals.scm");
-// pub const TAGS_QUERY: &'static str = include_str!("../../queries/tags.scm");
 
 #[cfg(test)]
 mod tests {
@@ -47,6 +63,6 @@ mod tests {
         let mut parser = tree_sitter::Parser::new();
         parser
             .set_language(super::language())
-            .expect("Error loading smali language");
+            .expect("Error loading Smali grammar");
     }
 }
