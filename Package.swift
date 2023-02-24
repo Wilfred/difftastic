fb8e0f54ef01bedd0b64cc140a6afaaa91f14ff2
Nicholas Hubbard <nhubbard@users.noreply.github.com>
2023-02-24 09:04:37 -0500

    Fix Package.swift indentation

--- Package.swift
+++ Package.swift
@@ -2,35 +2,35 @@
 import PackageDescription
 
 let package = Package(
-	name: "TreeSitterLatex",
-	products: [
-		.library(name: "TreeSitterLatex", targets: ["TreeSitterLatex"]),
-	],
-	dependencies: [],
-	targets: [
-		.target(name: "TreeSitterLatex",
-				path: ".",
-				exclude: [
-					"binding.gyp",
-					"bindings",
-					"benches",
-					".github",
-					"examples",
-					"Cargo.toml",
-					"test",
-					"grammar.js",
-					"LICENSE",
-					"Makefile",
-					"package.json",
-					"README.md",
-					"src/grammar.json",
-					"src/node-types.json",
-				],
-				sources: [
-					"src/parser.c",
-					"src/scanner.c",
-				],
-				publicHeadersPath: "bindings/swift",
-				cSettings: [.headerSearchPath("src")])
-	]
+    name: "TreeSitterLatex",
+    products: [
+        .library(name: "TreeSitterLatex", targets: ["TreeSitterLatex"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "TreeSitterLatex",
+                path: ".",
+                exclude: [
+                    "binding.gyp",
+                    "bindings",
+                    "benches",
+                    ".github",
+                    "examples",
+                    "Cargo.toml",
+                    "test",
+                    "grammar.js",
+                    "LICENSE",
+                    "Makefile",
+                    "package.json",
+                    "README.md",
+                    "src/grammar.json",
+                    "src/node-types.json",
+                ],
+                sources: [
+                    "src/parser.c",
+                    "src/scanner.c",
+                ],
+                publicHeadersPath: "bindings/swift",
+                cSettings: [.headerSearchPath("src")])
+    ]
 )
