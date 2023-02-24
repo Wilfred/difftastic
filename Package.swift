217200041929591f2745af35dc5d1decc586da00
Nicholas Hubbard <nhubbard@users.noreply.github.com>
2023-02-24 08:56:56 -0500

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+	name: "TreeSitterLatex",
+	products: [
+		.library(name: "TreeSitterLatex", targets: ["TreeSitterLatex"]),
+	],
+	dependencies: [],
+	targets: [
+		.target(name: "TreeSitterLatex",
+				path: ".",
+				exclude: [
+					"binding.gyp",
+					"bindings",
+					"benches",
+					".github",
+					"examples",
+					"Cargo.toml",
+					"test",
+					"grammar.js",
+					"LICENSE",
+					"Makefile",
+					"package.json",
+					"README.md",
+					"src/grammar.json",
+					"src/node-types.json",
+				],
+				sources: [
+					"src/parser.c",
+					"src/scanner.c",
+				],
+				publicHeadersPath: "bindings/swift",
+				cSettings: [.headerSearchPath("src")])
+	]
+)
---
+++ bindings/swift/TreeSitterLatex/latex.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_LATEX_H_
+#define TREE_SITTER_LATEX_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern TSLanguage *tree_sitter_latex();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_LATEX_H_
