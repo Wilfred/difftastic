16456ce9a127de5d9f53ae305baa2b68e84c5ae3
Amaan Qureshi <amaanq12@gmail.com>
2023-05-18 19:20:33 -0400

    fix: comments can have multiple delimiting backslashes

--- grammar.js
+++ grammar.js
@@ -1002,7 +1002,7 @@ module.exports = grammar({
 
     // http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
     comment: $ => token(choice(
-      seq('//', /(\\(.|\r?\n)|[^\\\n])*/),
+      seq('//', /(\\+(.|\r?\n)|[^\\\n])*/),
       seq(
         '/*',
         /[^*]*\*+([^/*][^*]*\*+)*/,
