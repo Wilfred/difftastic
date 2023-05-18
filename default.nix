56f5844d6cb86f301d234c1f4fe709abdf25e52c
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-05-18 22:21:48 +0200

    chore: Update nixpkgs to 22.11 and add python3 to devshell

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 let
   nixpkgs = fetchTarball {
     name = "nixpkgs";
-    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/21.05.tar.gz";
-    sha256 = "1ckzhh24mgz6jd1xhfgx0i9mijk6xjqxwsshnvq789xsavrmsc36";
+    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/22.11.tar.gz";
+    sha256 = "11w3wn2yjhaa5pv20gbfbirvjq6i3m7pqrq2msf0g7cv44vijwgw";
   };
   pkgs = import nixpkgs { };
 in
@@ -13,6 +13,7 @@ pkgs.mkShell {
     gcc
     clang
     libiconv
+    python3
   ];
   shellHook = ''
     PATH=./node_modules/.bin:$PATH
