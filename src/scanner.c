73c508c95cad2c95782d79d4c5ff24ac78db71ec
6cdh <cd6cdh@gmail.com>
2023-06-24 09:07:23 +0800

    feat(scanner): improve scanner.c and fix bugs

--- src/scanner.c
+++ src/scanner.c
@@ -3,50 +3,53 @@
 #include <string.h>
 #include <tree_sitter/parser.h>
 
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
-
 enum TokenType {
     HERE_STRING_BODY,
 };
 
+// a hand written string implmentation
+// data[0], data[1], ..., data[len-1] is the content of string
+// data[len] is `\0` for typical string `char*` compatibility
+// So 0 <= len < cap
 typedef struct {
     size_t len;
     size_t cap;
     char *data;
 } String;
 
-static String string_new() {
-    void *tmp = calloc(1, sizeof(char) * 17);
-    if (tmp == NULL) {
-        fprintf(stderr, "Failed to allocate memory\n");
+static void check_alloc(void *ptr) {
+    if (ptr == NULL) {
+        fprintf(stderr, "Scanner: Failed to allocate memory\n");
         exit(EXIT_FAILURE);
     }
-    return (String){
-        .cap = 16, .len = 0, .data = tmp};  // 17 for null terminator
 }
 
-static void string_resize(String *vec, uint32_t cap) {
-    void *block = realloc(vec->data, (cap + 1) * sizeof(vec->data[0]));
-    if (block == NULL) {
-        fprintf(stderr, "Failed to allocate memory\n");
-        exit(EXIT_FAILURE);
-    }
+static String string_new(void) {
+    size_t init_len = 16;
+    // (init_len + 1) for null terminator
+    size_t cap = init_len + 1;
+    void *tmp = calloc(1, sizeof(char) * cap);
+    check_alloc(tmp);
+    return (String){.cap = cap, .len = 0, .data = tmp};
+}
+
+static void string_resize(String *vec, size_t new_cap) {
+    void *block = realloc(vec->data, new_cap * sizeof(char));
+    check_alloc(block);
     vec->data = block;
-    memset(
-        vec->data + vec->len, 0, ((cap + 1) - vec->len) * sizeof(vec->data[0]));
-    vec->cap = cap;
-}
-
-static void string_grow(String *vec, uint32_t cap) {
-    if (vec->cap < cap) {
-        string_resize(vec, cap);
-    }
+    vec->cap = new_cap;
+    memset(vec->data + vec->len, 0, (new_cap - vec->len) * sizeof(char));
 }
 
 static void string_push(String *vec, int32_t elem) {
     if (vec->len + sizeof(elem) >= vec->cap) {
-        string_resize(vec, MAX(16, vec->len * 2));
+        // vec->cap * 2 + 1 > vec->len + sizeof(elem) always holds
+        // as vec->cap > 16
+        string_resize(vec, vec->cap * 2 + 1);
     }
+    // NOTE: we don't consider little-endian/big-endian here
+    // the character in string is only for compare.
+    // They only need to be store in consistent way
     memcpy(vec->data + vec->len, &elem, sizeof(elem));
     vec->len += sizeof(elem);
 }
@@ -54,22 +57,16 @@ static void string_push(String *vec, int32_t elem) {
 static void string_free(String *vec) {
     if (vec->data != NULL) {
         free(vec->data);
+        vec->data = NULL;
+        vec->len = 0;
+        vec->cap = 0;
     }
-    vec->data = NULL;
-    vec->len = 0;
-    vec->cap = 0;
 }
 
 static void string_clear(String *vec) {
+    memset(vec->data, 0, vec->len * sizeof(char));
     vec->len = 0;
-    memset(vec->data, 0, (vec->len + 1) * sizeof(char));
 }
-
-typedef struct {
-    bool valid;
-    String terminator_str;
-    String current_line;
-} Scanner;
 
 static void advance(TSLexer *lexer) {
     lexer->advance(lexer, false);
@@ -85,24 +82,10 @@ static bool isnewline(int32_t chr) {
     return chr == '\n';
 }
 
-static void read_terminator(Scanner *scanner, TSLexer *lexer) {
-    while (true) {
-        if (isnewline(lexer->lookahead)) {
-            return;
-        }
-        if (lexer->eof(lexer)) {
-            scanner->valid = false;
-            return;
-        }
-        string_push(&scanner->terminator_str, lexer->lookahead);
-        advance(lexer);
-    }
-}
-
 // `read_line` read strings until a newline or EOF
-static void read_line(Scanner *scanner, TSLexer *lexer) {
+static void read_line(String *str, TSLexer *lexer) {
     while (!isnewline(lexer->lookahead) && !lexer->eof(lexer)) {
-        string_push(&scanner->current_line, lexer->lookahead);
+        string_push(str, lexer->lookahead);
         advance(lexer);
     }
 }
@@ -110,42 +93,39 @@ static void read_line(Scanner *scanner, TSLexer *lexer) {
 // Suppose terminator is `T`, newline (\n) is `$`,
 // It should accept "#<<T$T" or "#<<T$...$T", where `...`
 // is the string content.
-bool scan(Scanner *scanner, TSLexer *lexer, const bool *valid_symbols) {
+bool scan(TSLexer *lexer, const bool *valid_symbols) {
     if (!valid_symbols[HERE_STRING_BODY]) {
         return false;
     }
 
-    string_clear(&scanner->terminator_str);
-    read_terminator(scanner, lexer);
+    String terminator = string_new();
+    read_line(&terminator, lexer);
 
-    if (!scanner->valid) {
+    if (lexer->eof(lexer)) {
         return false;
     }
 
     // skip `\n`
     skip(lexer);
+
+    String current_line = string_new();
     while (true) {
-        string_clear(&scanner->current_line);
-        read_line(scanner, lexer);
-        if (strcmp(scanner->terminator_str.data, scanner->current_line.data) ==
-            0) {
+        read_line(&current_line, lexer);
+        if (strcmp(terminator.data, current_line.data) == 0) {
             lexer->result_symbol = HERE_STRING_BODY;
             return true;
         }
         if (lexer->eof(lexer)) {
             return false;
         }
+        string_clear(&current_line);
         // skip `\n`
         skip(lexer);
     }
 }
 
 void *tree_sitter_racket_external_scanner_create() {
-    Scanner *scanner = calloc(1, sizeof(Scanner));
-    scanner->valid = true;
-    scanner->terminator_str = string_new();
-    scanner->current_line = string_new();
-    return scanner;
+    return NULL;
 }
 
 unsigned tree_sitter_racket_external_scanner_serialize(void *payload,
@@ -161,13 +141,8 @@ void tree_sitter_racket_external_scanner_deserialize(void *payload,
 bool tree_sitter_racket_external_scanner_scan(void *payload,
                                               TSLexer *lexer,
                                               const bool *valid_symbols) {
-    Scanner *scanner = (Scanner *)payload;
-    return scan(scanner, lexer, valid_symbols);
+    return scan(lexer, valid_symbols);
 }
 
 void tree_sitter_racket_external_scanner_destroy(void *payload) {
-    Scanner *scanner = (Scanner *)payload;
-    string_free(&scanner->terminator_str);
-    string_free(&scanner->current_line);
-    free(scanner);
 }
