90af85a39b914cca7ca67399995277b2ca7da70e
Wilfred Hughes <me@wilfred.me.uk>
2023-06-28 10:35:30 -0700

    Expand text slider logic

--- src/diff/text_sliders.rs
+++ src/diff/text_sliders.rs
@@ -1,11 +1,18 @@
+use itertools::Itertools;
+
 use crate::parse::syntax::MatchedPos;
 
-pub fn fix_text_line_sliders(mps: &[MatchedPos]) {
+pub fn fix_sliders(lines: &[String], lhs_mps: &[MatchedPos], rhs_mps: &[MatchedPos]) {
     // GOAL: Join contiguous novel regions where possible.
     //
     // Iterate through novel regions pairwise, and if there's a gap
     // between them that also occurs after the second novel region,
     // slide.
+    let lhs_novel_regions = novel_regions(lhs_mps);
+
+    for (prev_region, next_region) in lhs_novel_regions.iter().tuple_windows() {
+        if can_slide(lines, lhs_mps, prev_region.1, next_region.0, next_region.1) {}
+    }
 }
 
 /// Given the position of two novel regions, see if the unchanged line
@@ -35,4 +42,37 @@ fn can_slide(
     let line_after = unchanged_mp_after.pos.line.0;
 
     lines[line_between as usize] == lines[line_after as usize]
+}
+
+/// Return a vec of tuples representing the start and end index
+/// (inclusive) of each novel region in `mps`.
+fn novel_regions(mps: &[MatchedPos]) -> Vec<(usize, usize)> {
+    let mut res = vec![];
+
+    let mut region_start = None;
+    for (i, mp) in mps.iter().enumerate() {
+        if mp.kind.is_novel() {
+            match region_start {
+                Some(_) => {
+                    // We are continuing a novel region.
+                }
+                None => {
+                    // Starting a new novel region.
+                    region_start = Some(i);
+                }
+            }
+        } else {
+            if let Some(region_start_i) = region_start {
+                // Reached the end of a novel region.
+                res.push((region_start_i, i - 1));
+                region_start = None;
+            }
+        }
+    }
+
+    if let Some(region_start) = region_start {
+        res.push((region_start, mps.len() - 1));
+    }
+
+    res
 }
