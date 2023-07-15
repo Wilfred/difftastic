2616034f78ffa83ca6a521ebd7eee1868cb5c14c
Michael Davis <mcarsondavis@gmail.com>
2023-07-15 17:33:57 -0500

    Fix rust binding build script (#59)

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -9,8 +9,11 @@ fn main() {
         .flag_if_supported("-Wno-trigraphs");
     let parser_path = src_dir.join("parser.c");
     c_config.file(&parser_path);
+    println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
 
     let scanner_path = src_dir.join("scanner.c");
     c_config.file(&scanner_path);
     println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
+
+    c_config.compile("parser-scanner");
 }
