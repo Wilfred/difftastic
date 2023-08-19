ccc1b8e1eb7d087b9032c684fd91938dd0730ab1
Gerbais-Nief Eddie <eddie.gerbais-nief@proton.me>
2023-08-19 15:44:37 +0200

    refactor step 2

--- corpus/test.scm
+++ corpus/test.scm
@@ -1,5 +1,5 @@
 =====================
-Test 001
+Test 000
 =====================
 a b
 ---------------------
@@ -10,7 +10,7 @@ a b
 
 
 =====================
-Test 002
+Test 001
 =====================
 a b#a b
 ---------------------
@@ -20,3 +20,524 @@ a b#a b
 	(text)
 	(ident)
 	(text))
+
+
+=====================
+Test 002
+=====================
+#if b {} else {}
+---------------------
+
+(source_file
+	(branch
+		(ident)
+		(block)
+		(block)))
+
+
+=====================
+Test 003
+=====================
+#if b {} elsa {}
+---------------------
+
+(source_file
+	(branch
+		(ident)
+		(block))
+	(text)
+	(text))
+
+
+=====================
+Test 004
+=====================
+Hello #(a + b) World!
+---------------------
+
+(source_file
+	(text)
+	(group
+		(add
+			(ident)
+			(ident)))
+	(text))
+
+
+=====================
+Test 005
+=====================
+#(a + b + c)
+---------------------
+
+(source_file
+	(group
+		(add
+			(add
+				(ident)
+				(ident))
+			(ident))))
+
+
+=====================
+Test 006
+=====================
+#(a + b * c)
+---------------------
+
+(source_file
+	(group
+		(add
+			(ident)
+			(mul
+				(ident)
+				(ident)))))
+
+
+=====================
+Test 007
+=====================
+Hello #a + b World!
+---------------------
+
+(source_file
+	(text)
+	(ident)
+	(text)
+	(text)
+	(text))
+
+
+=====================
+Test 008
+=====================
+#(a * b + c)
+---------------------
+
+(source_file
+	(group
+		(add
+			(mul
+				(ident)
+				(ident))
+			(ident))))
+
+
+=====================
+Test 009
+=====================
+#(a() + c)
+---------------------
+
+(source_file
+	(group
+		(add
+			(call
+				item: (ident)
+				(group))
+			(ident))))
+
+
+=====================
+Test 010
+=====================
+#a() Hello
+---------------------
+
+(source_file
+	(call
+		item: (ident)
+		(group))
+	(text))
+
+
+=====================
+Test 011
+=====================
+#a()Hello
+---------------------
+
+(source_file
+	(call
+		item: (ident)
+		(group))
+	(text))
+
+
+=====================
+Test 012
+=====================
+#a!Hello
+---------------------
+
+(source_file
+	(ident)
+	(text))
+
+
+=====================
+Test 013
+=====================
+#a()()Hello
+---------------------
+
+(source_file
+	(call
+		item: (call
+			item: (ident)
+			(group))
+		(group))
+	(text))
+
+
+=====================
+Test 014
+=====================
+#[Hello World!]
+---------------------
+
+(source_file
+	(content
+		(text)
+		(text)))
+
+
+=====================
+Test 015
+=====================
+#{a}
+---------------------
+
+(source_file
+	(block
+		(ident)))
+
+
+=====================
+Test 016
+=====================
+#(a, b)
+---------------------
+
+(source_file
+	(group
+		(ident)
+		(ident)))
+
+
+=====================
+Test 017
+=====================
+#(a, c: b)
+---------------------
+
+(source_file
+	(group
+		(ident)
+		(tagged
+			field: (ident)
+			(ident))))
+
+
+=====================
+Test 018
+=====================
+#hello(a, c: b)
+---------------------
+
+(source_file
+	(call
+		item: (ident)
+		(group
+			(ident)
+			(tagged
+				field: (ident)
+				(ident)))))
+
+
+=====================
+Test 019
+=====================
+#hello(a, c: b)World
+---------------------
+
+(source_file
+	(call
+		item: (ident)
+		(group
+			(ident)
+			(tagged
+				field: (ident)
+				(ident))))
+	(text))
+
+
+=====================
+Test 020
+=====================
+#hello()[World]
+---------------------
+
+(source_file
+	(call
+		item:
+		(call
+			item: (ident)
+			(group))	
+		(content
+			(text))))
+
+
+=====================
+Test 021
+=====================
+#if a {}
+
+else {}
+---------------------
+
+(source_file
+	(branch
+		test: (ident)
+		(block)
+		(block)))
+
+
+=====================
+Test 022
+=====================
+#if a {}
+elsa {}
+---------------------
+
+(source_file
+	(branch
+		test: (ident)
+		(block))
+	(text)
+	(text))
+
+
+=====================
+Test 023
+=====================
+#if a {} elsee {}
+---------------------
+
+(source_file
+	(branch
+		test: (ident)
+		(block))
+	(text)
+	(text))
+
+
+=====================
+Test 024
+=====================
+a b
+c
+
+d e
+---------------------
+
+(source_file
+	(text)
+	(text)
+	(text)
+	(break)
+	(text)
+	(text))
+
+
+=====================
+Test 025
+=====================
+#if a{}
+
+
+else[]
+---------------------
+
+(source_file
+	(branch
+		test: (ident)
+		(block)
+		(content)))
+
+
+=====================
+Test 026
+=====================
+#if a[]{}
+---------------------
+
+(source_file
+	(branch
+		test: (call
+			item: (ident)
+			(content))
+		(block)))
+
+
+=====================
+Test 027
+=====================
+#if a []{}
+---------------------
+
+(source_file
+	(branch
+		test: (ident)
+		(content))
+	(text))
+
+
+=====================
+Test 028
+=====================
+#if(a)[]{}
+---------------------
+
+(source_file
+	(branch
+		test: (call
+			item: (group
+				(ident))
+			(content))
+		(block)))
+
+
+=====================
+Test 029
+=====================
+#if(a) + b[]{}
+---------------------
+
+(source_file
+	(branch
+		test: (add
+			(group
+				(ident))
+			(call
+				item: (ident)
+				(content)))
+		(block)))
+
+
+=====================
+Test 030
+=====================
+*hello* world
+---------------------
+
+(source_file
+	(strong
+		(text))
+	(text))
+
+
+=====================
+Test 031
+=====================
+_hello_ world
+---------------------
+
+(source_file
+	(emph
+		(text))
+	(text))
+
+
+=====================
+Test 032
+=====================
+*hello _world_*
+---------------------
+
+(source_file
+	(strong
+		(text)
+		(emph
+			(text))))
+
+
+=====================
+Test 033
+=====================
+#ifa {}
+---------------------
+
+(source_file
+	(ident)
+	(text))
+
+
+=====================
+Test 034
+=====================
+#if a {} else {}()
+---------------------
+
+(source_file
+	(call
+		item: (branch
+			test: (ident)
+			(block)
+			(block))
+		(group)))
+
+
+=====================
+Test 035
+=====================
+#let a = b
+---------------------
+
+(source_file
+	(let
+		pattern: (ident)
+		value: (ident)))
+
+
+=====================
+Test 036
+=====================
+#let a = b; Hello
+---------------------
+
+(source_file
+	(let
+		pattern: (ident)
+		value: (ident))
+	(text))
+
+
+=====================
+Test 037
+=====================
+#a; Hello
+---------------------
+
+(source_file
+	(ident)
+	(text))
+
+
+=====================
+Test 038
+=====================
+#{
+	let a = b
+	a + a
+}
+---------------------
+
+(source_file
+	(block
+		(let
+			pattern: (ident)
+			value: (ident))
+	(add
+		(ident)
+		(ident))))
--- grammar.js
+++ grammar.js
@@ -1,45 +1,125 @@
+function ws($) {
+  return optional($._space_la);
+}
 module.exports = grammar({
   name: 'typst',
   extras: $ => [],
-  inline: $ => [],
-  conflicts: $ => [],
+  conflicts: $ => [
+    [$._code],
+    [$.let],
+    [$.tagged, $._expr],
+    [$._item, $._expr],
+  ],
+  // word: $ => $._token_else,
   rules: {
-    source_file: $ => optional($._content),
-    _content: $ => repeat1(choice(
+    source_file: $ => repeat($._markup),
+
+    _token_dlim: $ => /[ ]*(\n|;)/,
+    break: $ => /\n([ \t]*\n)+/,
+    _space_la: $ => /[ \n]+/,
+    _anti_else: $ => /[ \n]*else[^ \t\{\[]/,
+
+    _token_numsign: $ => '#',
+    _token_else: $ => /[ \n]*else/,
+    _token_plus: $ => /[ \n]*\+/,
+    _token_star: $ => /[ \n]*\*/,
+    _token_assign: $ => /[ \n]*\*/,
+
+    _char_any: $ => /./,
+
+    _markup: $ => choice(
       $._code,
       $.text,
+      $.break,
+      $.strong,
+      $.emph,
       $._space_la,
-    )),
-
-    _token_dot: $ => '.',
-
-    // this rule matches exactly 1 symbol
-    _text_any: $ => choice(
-
-      // text matches only non-markup simbols and exclude spaces
-      /[^# \t\n\]]/,
-
-      $._token_dot,
     ),
 
-    // next to a space, no restriction for text
-    text: $ => prec.right(repeat1($._text_any)),
+    text: $ => prec.right(repeat1(choice(
+      $._anti_else,
+      $._char_any,
+    ))),
 
-    // contains 0 line
-    _space_l0: $ => /[ \t]+/,
-    // contains exactly 1 line
-    _space_l1: $ => /\n/,
-    // contains any combination of spaces and lines
-    // TODO: check with `prec.right`
-    _space_la: $ => prec.right(repeat1(choice($._space_l0, $._space_l1))),
+    strong: $ => prec.left(seq('*', repeat($._markup), '*')),
+    emph: $ => prec.left(seq('_', repeat($._markup), '_')),
 
-    _code: $ => seq('#', $._expr),
+    _code: $ => seq($._token_numsign, choice($._item, $._stmt), optional($._token_dlim)),
+
+    _item: $ => choice(
+      $.ident,
+      $.branch,
+      $.block,
+      $.group,
+      $.call,
+      $.content,
+    ),
+
+    _stmt: $ => choice(
+      $.let,
+    ),
 
     _expr: $ => choice(
       $.ident,
+      $.branch,
+      $.block,
+      $.group,
+      $.add,
+      $.mul,
+      $.call,
+      $.content,
+      $.let,
+    ),
+
+    _pattern: $ => choice(
+      $.ident,
+      $.group,
     ),
 
     ident: $ => /[a-z]+/,
+    add: $ => prec.left(1, seq($._expr, $._token_plus, ws($), $._expr)),
+    mul: $ => prec.left(2, seq($._expr, $._token_star, ws($), $._expr)),
+    call: $ => seq(field('item', $._item), choice($.content, $.group)),
+    tagged: $ => seq(field('field', $.ident), ws($), ':', ws($), $._expr),
+    content: $ => seq('[', repeat($._markup), ']'),
+    group: $ => seq(
+      '(',
+      repeat(seq(ws($), choice($.tagged, $._expr), ws($), ',')),
+      ws($),
+      optional(seq(choice($.tagged, $._expr), ws($))),
+      ')'
+    ),
+    block: $ => seq(
+      '{',
+      repeat(seq(
+        optional(seq(ws($), $._expr)),
+        choice($._token_dlim, $._space_la),
+      )),
+      optional($._expr),
+      '}'
+    ),
+    branch: $ => prec.left(1, seq(
+      'if',
+      ws($), 
+      field('test', $._expr), 
+      ws($),
+      choice($.block, $.content),
+      optional(seq($._token_else,
+        ws($),
+        choice($.block, $.content)
+      )),
+    )),
+    let: $ => prec(0, seq(
+      'let',
+      ws($),
+      field('pattern', $._pattern),
+      optional(seq(
+        ws($),
+        '=',
+        ws($),
+        field('value', $._expr)
+      ))
+    )),
   },
 });
 
