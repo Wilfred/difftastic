15c59b69360f8120d241c0dde2b948b7cfc37b3a
Amaan Qureshi <amaanq12@gmail.com>
2023-08-20 03:33:17 -0400

    chore: format scanner

--- src/scanner.c
+++ src/scanner.c
@@ -2,52 +2,58 @@
 #include <wctype.h>
 
 enum TokenType {
-  DESCENDANT_OP,
+    DESCENDANT_OP,
 };
 
 void *tree_sitter_css_external_scanner_create() { return NULL; }
-void tree_sitter_css_external_scanner_destroy(void *p) {}
-void tree_sitter_css_external_scanner_reset(void *p) {}
-unsigned tree_sitter_css_external_scanner_serialize(void *p, char *buffer) { return 0; }
-void tree_sitter_css_external_scanner_deserialize(void *p, const char *b, unsigned n) {}
 
-bool tree_sitter_css_external_scanner_scan(void *payload, TSLexer *lexer, const bool *valid_symbols) {
-  if (iswspace(lexer->lookahead) && valid_symbols[DESCENDANT_OP]) {
-    lexer->result_symbol = DESCENDANT_OP;
+void tree_sitter_css_external_scanner_destroy(void *payload) {}
 
-    lexer->advance(lexer, true);
-    while (iswspace(lexer->lookahead)) {
-      lexer->advance(lexer, true);
-    }
-    lexer->mark_end(lexer);
+void tree_sitter_css_external_scanner_reset(void *payload) {}
 
-    if (
-      lexer->lookahead == '#' ||
-      lexer->lookahead == '.' ||
-      lexer->lookahead == '[' ||
-      lexer->lookahead == '-' ||
-      lexer->lookahead == '*' ||
-      iswalnum(lexer->lookahead)
-    ) {
-      return true;
-    }
+unsigned tree_sitter_css_external_scanner_serialize(void *payload,
+                                                    char *buffer) {
+    return 0;
+}
 
-    if (lexer->lookahead == ':') {
-      lexer->advance(lexer, false);
-      if (iswspace(lexer->lookahead)) return false;
-      for (;;) {
-        if (
-          lexer->lookahead == ';' ||
-          lexer->lookahead == '}' ||
-          lexer->eof(lexer)
-        ) return false;
-        if (lexer->lookahead == '{') {
-          return true;
+void tree_sitter_css_external_scanner_deserialize(void *payload,
+                                                  const char *buffer,
+                                                  unsigned length) {}
+
+bool tree_sitter_css_external_scanner_scan(void *payload, TSLexer *lexer,
+                                           const bool *valid_symbols) {
+    if (iswspace(lexer->lookahead) && valid_symbols[DESCENDANT_OP]) {
+        lexer->result_symbol = DESCENDANT_OP;
+
+        lexer->advance(lexer, true);
+        while (iswspace(lexer->lookahead)) {
+            lexer->advance(lexer, true);
         }
-        lexer->advance(lexer, false);
-      }
-    }
-  }
+        lexer->mark_end(lexer);
 
-  return false;
+        if (lexer->lookahead == '#' || lexer->lookahead == '.' ||
+            lexer->lookahead == '[' || lexer->lookahead == '-' ||
+            lexer->lookahead == '*' || iswalnum(lexer->lookahead)) {
+            return true;
+        }
+
+        if (lexer->lookahead == ':') {
+            lexer->advance(lexer, false);
+            if (iswspace(lexer->lookahead)) {
+                return false;
+            }
+            for (;;) {
+                if (lexer->lookahead == ';' || lexer->lookahead == '}' ||
+                    lexer->eof(lexer)) {
+                    return false;
+                }
+                if (lexer->lookahead == '{') {
+                    return true;
+                }
+                lexer->advance(lexer, false);
+            }
+        }
+    }
+
+    return false;
 }
