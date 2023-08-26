a16aed19564fb4446725e01f6c64a5a2d99fb55d
FUTATSUKI Yasuhito <futatuki@yf.bsdclub.org>
2023-08-27 00:02:36 +0900

    Implement -u option with no effect
    If "svn diff --diff-cmd" is called without -x option, svn executes
    external diff command with -u option. So this allows to use
    difft as external diff for Apache Subversion, without extra options.

--- src/options.rs
+++ src/options.rs
@@ -151,6 +151,13 @@ fn app() -> clap::Command {
                 .required(false),
         )
         .arg(
+            // Dummy 'unified' flag of POSIX/GNU diff utility for svn support:
+            // This takes no effect.
+            Arg::new("nop_unified")
+                .short('u')
+                .hide(true)
+        )
+        .arg(
             Arg::new("label").short('L')
                 .long("label")
                 .takes_value(true)
