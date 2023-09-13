928b00e92e4f494126fb60b34f50daa915c632ac
Leorize <leorize+oss@disroot.org>
2023-09-13 15:38:12 +0000

    change long string opening from regex to literal choice
    Apparently this helps with writing injection queries.
    
    Ref: https://github.com/alaviss/tree-sitter-nim/pull/32#issuecomment-1717850053

--- grammar.js
+++ grammar.js
@@ -1395,7 +1395,8 @@ module.exports = grammar({
       ),
     _raw_string_escape: () => token.immediate('""'),
 
-    _long_string_literal: $ => seq(/[rR]?"""/, $._long_string_body),
+    _long_string_literal: $ =>
+      seq(choice('"""', 'r"""', 'R"""'), $._long_string_body),
 
     _long_string_body: $ =>
       seq(
