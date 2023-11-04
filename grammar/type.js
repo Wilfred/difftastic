31e4bfdcda1ff473df27bb6ccdff28b72ef27057
postsolar <120750161+postsolar@users.noreply.github.com>
2023-11-04 05:17:33 +0200

    Better nodes for annotated quantifiers

--- grammar/type.js
+++ grammar/type.js
@@ -25,7 +25,7 @@ module.exports = {
   _tyvar: $ =>
     choice(
       $._tyvar_no_annotation,
-      $._tyvar_annotated
+      alias($._tyvar_annotated, $.annotated_type_variable)
     ),
 
   // ----- Quantifiers --------------------------------------------------------
