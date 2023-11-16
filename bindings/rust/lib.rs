4add3bb47bf71b19b6ed5340d54ee86a5aea72cc
Dheeraj R Reddy <dheeraj98reddy@gmail.com>
2023-11-16 09:42:00 +0530

    feat: add tags query to Rust bindings

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -54,6 +54,9 @@ pub const HIGHLIGHT_QUERY: &str = include_str!("../../queries/highlights.scm");
 /// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
 pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
+/// The symbol tagging query for this language.
+pub const TAGS_QUERY: &'static str = include_str!("../../queries/tags.scm");
+
 #[cfg(test)]
 mod tests {
     #[test]
