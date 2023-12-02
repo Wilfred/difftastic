5566f8ce8fc24186fad06170bbb3c8d97c935d74
Garrett D'Amore <garrett@damore.org>
2023-12-01 21:11:55 -0800

    fixes #19 add a test for static assert with multiple args

--- test/corpus/assert.scm
+++ test/corpus/assert.scm
@@ -49,3 +49,30 @@ void f() {
                   (int_literal))
                 (expression
                   (string_literal))))))))))
+
+================================================================================
+Static Assert Expression Multiple arguments
+================================================================================
+
+void f() {
+	static assert(0, "xyz");
+}
+--------------------------------------------------------------------------------
+
+(source_file
+  (function_declaration
+    (type
+      (void))
+    (identifier)
+    (parameters)
+    (function_body
+      (block_statement
+        (static_assert
+          (static)
+          (assert_expression
+            (assert)
+            (assert_arguments
+              (expression
+                (int_literal))
+              (expression
+                (string_literal)))))))))
