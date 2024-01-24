ff827256689408165e0580d1f509770dcd342a1c
Amaan Qureshi <amaanq12@gmail.com>
2024-01-24 12:16:17 -0500

    chore: clang tidy

--- src/scanner.c
+++ src/scanner.c
@@ -1,6 +1,7 @@
+#include "tree_sitter/parser.h"
+
 #include <assert.h>
 #include <string.h>
-#include <tree_sitter/parser.h>
 #include <wctype.h>
 
 enum TokenType { RAW_STRING_DELIMITER, RAW_STRING_CONTENT };
@@ -25,8 +26,9 @@ static bool scan_raw_string_delimiter(Scanner *scanner, TSLexer *lexer) {
         // We already checked this when scanning content, but this is how we
         // know when to stop. We can't stop at ", because R"""hello""" is valid.
         for (int i = 0; i < scanner->delimiter_length; ++i) {
-            if (lexer->lookahead != scanner->delimiter[i])
+            if (lexer->lookahead != scanner->delimiter[i]) {
                 return false;
+            }
             advance(lexer);
         }
         reset(scanner);
@@ -68,9 +70,8 @@ static bool scan_raw_string_content(Scanner *scanner, TSLexer *lexer) {
             if (delimiter_index == scanner->delimiter_length) {
                 if (lexer->lookahead == '"') {
                     return true;
-                } else {
-                    delimiter_index = -1;
                 }
+                delimiter_index = -1;
             } else {
                 if (lexer->lookahead == scanner->delimiter[delimiter_index]) {
                     delimiter_index++;
