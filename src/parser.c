edb25bebc4e2ac0f227ce216a55d2489d98115a5
PrestonKnopp <prestonknopp@gmail.com>
2024-04-01 22:05:45 -0700

    tree-sitter generate

--- src/parser.c
+++ src/parser.c
@@ -5,7 +5,7 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
-#define LANGUAGE_VERSION 13
+#define LANGUAGE_VERSION 14
 #define STATE_COUNT 78
 #define LARGE_STATE_COUNT 2
 #define SYMBOL_COUNT 37
@@ -292,6 +292,87 @@ static const TSSymbol ts_alias_sequences[PRODUCTION_ID_COUNT][MAX_ALIAS_SEQUENCE
 
 static const uint16_t ts_non_terminal_alias_map[] = {
   0,
+};
+
+static const TSStateId ts_primary_state_ids[STATE_COUNT] = {
+  [0] = 0,
+  [1] = 1,
+  [2] = 2,
+  [3] = 2,
+  [4] = 4,
+  [5] = 5,
+  [6] = 4,
+  [7] = 7,
+  [8] = 8,
+  [9] = 9,
+  [10] = 10,
+  [11] = 11,
+  [12] = 12,
+  [13] = 13,
+  [14] = 14,
+  [15] = 15,
+  [16] = 16,
+  [17] = 17,
+  [18] = 18,
+  [19] = 19,
+  [20] = 20,
+  [21] = 21,
+  [22] = 22,
+  [23] = 23,
+  [24] = 24,
+  [25] = 25,
+  [26] = 26,
+  [27] = 27,
+  [28] = 28,
+  [29] = 29,
+  [30] = 30,
+  [31] = 31,
+  [32] = 32,
+  [33] = 20,
+  [34] = 34,
+  [35] = 11,
+  [36] = 36,
+  [37] = 37,
+  [38] = 38,
+  [39] = 39,
+  [40] = 40,
+  [41] = 41,
+  [42] = 42,
+  [43] = 43,
+  [44] = 38,
+  [45] = 40,
+  [46] = 46,
+  [47] = 47,
+  [48] = 48,
+  [49] = 39,
+  [50] = 50,
+  [51] = 43,
+  [52] = 52,
+  [53] = 52,
+  [54] = 42,
+  [55] = 48,
+  [56] = 56,
+  [57] = 57,
+  [58] = 58,
+  [59] = 21,
+  [60] = 17,
+  [61] = 23,
+  [62] = 14,
+  [63] = 13,
+  [64] = 16,
+  [65] = 18,
+  [66] = 19,
+  [67] = 12,
+  [68] = 15,
+  [69] = 69,
+  [70] = 70,
+  [71] = 71,
+  [72] = 72,
+  [73] = 73,
+  [74] = 74,
+  [75] = 75,
+  [76] = 76,
+  [77] = 77,
 };
 
 static bool ts_lex(TSLexer *lexer, TSStateId state) {
@@ -1794,6 +1875,7 @@ extern const TSLanguage *tree_sitter_godot_resource(void) {
       tree_sitter_godot_resource_external_scanner_serialize,
       tree_sitter_godot_resource_external_scanner_deserialize,
     },
+    .primary_state_ids = ts_primary_state_ids,
   };
   return &language;
 }
