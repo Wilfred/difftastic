f9d1c8f86cc505e52a718ee8da8359c1c3cdfd39
Stephan Seitz <sseitz@nvidia.com>
2024-04-06 22:12:44 +0200

    feat: add bindings for Swift, C, Python, Go

---
+++ bindings/python/tree_sitter_commonlisp/__init__.py
@@ -0,0 +1,5 @@
+"Commonlisp grammar for tree-sitter"
+
+from ._binding import language
+
+__all__ = ["language"]
---
+++ bindings/swift/TreeSitterCommonlisp/commonlisp.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_COMMONLISP_H_
+#define TREE_SITTER_COMMONLISP_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_commonlisp(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_COMMONLISP_H_
