eb68645662a3f7bf7fdd4bcb9531585f54c8570e
Amaan Qureshi <amaanq12@gmail.com>
2024-04-08 18:09:12 -0400

    fix: rename highlights query constant

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -41,7 +41,7 @@ pub fn language() -> Language {
 pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
 /// The syntax highlighting query for this language.
-pub const HIGHLIGHT_QUERY: &str = include_str!("../../queries/highlights.scm");
+pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
 
 /// The symbol tagging query for this language.
 pub const TAGS_QUERY: &str = include_str!("../../queries/tags.scm");
