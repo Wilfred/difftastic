2064ffaf0af640004ea8da2969348a47b5825929
Dan Cross <cross@oxidecomputer.com>
2025-09-16 17:47:49 +0000

    Do not use jemalloc on illumos
    jemalloc does not work on illumos; it's use is
    already conditional on environments that are not
    msvc, so extend the conditionals involved to also
    exclude illumos.

--- src/main.rs
+++ src/main.rs
@@ -91,10 +91,10 @@ use crate::parse::syntax;
 ///
 /// For reference, Jemalloc uses 10-20% more time (although up to 33%
 /// more instructions) when testing on sample files.
-#[cfg(not(target_env = "msvc"))]
+#[cfg(not(any(target_env = "msvc", target_os = "illumos")))]
 use tikv_jemallocator::Jemalloc;
 
-#[cfg(not(target_env = "msvc"))]
+#[cfg(not(any(target_env = "msvc", target_os = "illumos")))]
 #[global_allocator]
 static GLOBAL: Jemalloc = Jemalloc;
 
