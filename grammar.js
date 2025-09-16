bcfe5e6beabd82c95f1cdb88751661f6ac1aa7fb
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2025-09-16 10:51:34 +0200

    Change comment parsing to be handled by grammar.js, simplify scanner.c
    Close #42

--- grammar.js
+++ grammar.js
@@ -40,13 +40,6 @@ module.exports = grammar({
     $._string_name_start,
     $._node_path_start,
 
-    // Mark comments as external tokens so that the external scanner is always
-    // invoked, even if no external token is expected. This allows for better
-    // error recovery, because the external scanner can maintain the overall
-    // structure by returning dedent tokens whenever a dedent occurs, even
-    // if no dedent is expected.
-    $.comment,
-
     // Allow the external scanner to check for the validity of closing brackets
     // so that it can avoid returning dedent tokens between brackets.
     "]",
