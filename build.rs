6fbd425b55c27b5b76442d29c619bc70a15b2695
Antonin Delpeuch <antonin@delpeuch.eu>
2025-10-04 12:29:47 +0200

    Unvendor tree-sitter-dart

--- build.rs
+++ build.rs
@@ -83,11 +83,6 @@ fn main() {
             extra_files: vec![],
         },
         TreeSitterParser {
-            name: "tree-sitter-dart",
-            src_dir: "vendored_parsers/tree-sitter-dart-src",
-            extra_files: vec!["scanner.c"],
-        },
-        TreeSitterParser {
             name: "tree-sitter-devicetree",
             src_dir: "vendored_parsers/tree-sitter-devicetree-src",
             extra_files: vec![],
