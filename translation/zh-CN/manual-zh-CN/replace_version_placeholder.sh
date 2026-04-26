5f082d7555272ed64455d1a1e6e72512cfc2b9cf
Wilfred Hughes <me@wilfred.me.uk>
2026-04-26 23:56:53 +0100

    Use symlink to version placeholder script to avoid duplication

--- translation/zh-CN/manual-zh-CN/replace_version_placeholder.sh
+++ translation/zh-CN/manual-zh-CN/replace_version_placeholder.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-
-DFT_VERSION=$(cargo read-manifest | jq -r .version)
-
-jq .[1] | jq '.sections[0].Chapter.content |= sub("DFT_VERSION_HERE"; "'$DFT_VERSION'")'
+../../../manual/replace_version_placeholder.sh
