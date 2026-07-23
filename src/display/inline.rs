d8efbdb148ec932e2d783d7c6c584b47b3848f20
Nicolas Arsenault <arsenault.nic@gmail.com>
2026-07-23 10:25:06 -0400

    Fix inline display

--- src/display/inline.rs
+++ src/display/inline.rs
@@ -1,5 +1,9 @@
 //! Inline, or "unified" diff display.
 
+use line_numbers::LineNumber;
+
+use std::cmp;
+
 use crate::constants::Side;
 use crate::display::context::{
     calculate_after_context, calculate_before_context, opposite_positions,
@@ -67,6 +71,7 @@ pub(crate) fn print(
     // Calculate the maximum line number width for alignment
     let lhs_line_nums_width = format_line_num(lhs_src.max_line()).len();
     let rhs_line_nums_width = format_line_num(rhs_src.max_line()).len();
+    let column_width = cmp::max(lhs_line_nums_width, rhs_line_nums_width);
 
     for (i, hunk) in hunks.iter().enumerate() {
         println!(
@@ -83,6 +88,116 @@ pub(crate) fn print(
 
         let hunk_lines = hunk.lines.clone();
 
+        let mut lhs_previous: Option<usize> = None;
+        let mut i = 0;
+
+        // Print the before context
+        let (mut previous_lhs_line_nbr, mut previous_rhs_line_nbr) = print_before_lines(
+            display_options,
+            &lhs_colored_lines,
+            &opposite_to_lhs,
+            &opposite_to_rhs,
+            column_width,
+            &hunk_lines,
+            &mut lhs_previous,
+            i,
+            3,
+        );
+
+        loop {
+            // print gaps
+            let (lhs_line, rhs_line) = &hunk_lines[i];
+
+            println!("{:#?}", hunk_lines[i]);
+
+            let diff = match (lhs_line, rhs_line) {
+                (Some(lhs), Some(rhs)) => {
+                    cmp::min(
+                        lhs.as_usize() - previous_lhs_line_nbr,
+                        rhs.as_usize() - previous_rhs_line_nbr,
+                    ) - 1
+                }
+                (Some(lhs), None) => lhs.as_usize() - previous_lhs_line_nbr - 1,
+                (None, Some(rhs)) => rhs.as_usize() - previous_rhs_line_nbr - 1,
+                (None, None) => panic!("somethings wrong"),
+            };
+
+            if i > 0 && diff > 0 {
+                (previous_lhs_line_nbr, previous_rhs_line_nbr) = print_before_lines(
+                    display_options,
+                    &lhs_colored_lines,
+                    &opposite_to_lhs,
+                    &opposite_to_rhs,
+                    column_width,
+                    &hunk_lines,
+                    &mut lhs_previous,
+                    i,
+                    diff,
+                );
+            }
+
+            // print lhs lines
+            let mut last_lhs_line_nbr: usize = 0;
+            let mut j = i;
+            while let (Some(line_nbr), _) = hunk_lines[j] {
+                if j >= hunk_lines.len()
+                    || last_lhs_line_nbr != 0 && line_nbr.as_usize() > last_lhs_line_nbr + 1
+                {
+                    break;
+                }
+                print!(
+                    "{}{}",
+                    apply_combine_line_number(
+                        Some(line_nbr),
+                        None,
+                        false,
+                        column_width,
+                        display_options
+                    ),
+                    &lhs_colored_lines[line_nbr.as_usize()],
+                );
+                j += 1;
+                last_lhs_line_nbr = line_nbr.as_usize();
+                previous_lhs_line_nbr = line_nbr.as_usize();
+                if j >= hunk_lines.len() {
+                    break;
+                }
+            }
+
+            // print rhs lines
+            let mut last_rhs_line_nbr: usize = 0;
+            last_rhs_line_nbr = 0;
+            while let (_, Some(line_nbr)) = hunk_lines[i] {
+                if i >= hunk_lines.len()
+                    || last_rhs_line_nbr > 0 && line_nbr.as_usize() > last_rhs_line_nbr + 1
+                {
+                    break;
+                }
+                print!(
+                    "{}{}",
+                    apply_combine_line_number(
+                        None,
+                        Some(line_nbr),
+                        false,
+                        column_width,
+                        display_options
+                    ),
+                    &rhs_colored_lines[line_nbr.as_usize()],
+                );
+                i += 1;
+                last_rhs_line_nbr = line_nbr.as_usize();
+                previous_rhs_line_nbr = line_nbr.as_usize();
+                if i >= hunk_lines.len() {
+                    break;
+                }
+            }
+            i = cmp::max(i, j);
+            if i >= hunk_lines.len() {
+                break;
+            }
+        }
+
+        // Print the after context
         let before_lines = calculate_before_context(
             &hunk_lines,
             &opposite_to_lhs,
@@ -93,70 +208,105 @@ pub(crate) fn print(
             &[&before_lines[..], &hunk_lines[..]].concat(),
             &opposite_to_lhs,
             &opposite_to_rhs,
-            // TODO: repeatedly calculating the maximum is wasteful.
             lhs_src.max_line(),
             rhs_src.max_line(),
-            display_options.num_context_lines as usize,
+            display_options.num_context_lines as usize - 1,
         );
-
-        for (lhs_line, _) in before_lines {
-            if let Some(lhs_line) = lhs_line {
+        for (lhs_line, rhs_line) in &after_lines {
+            if let Some(rhs_line_nbr) = rhs_line {
                 print!(
-                    "{}   {}",
-                    apply_line_number_color(
-                        &format_line_num_padded(lhs_line, lhs_line_nums_width),
+                    "{}{}",
+                    apply_combine_line_number(
+                        *lhs_line,
+                        *rhs_line,
                         false,
-                        Side::Left,
-                        display_options,
+                        column_width,
+                        display_options
                     ),
-                    lhs_colored_lines[lhs_line.as_usize()]
+                    rhs_colored_lines[rhs_line_nbr.as_usize()]
                 );
             }
         }
 
-        for (lhs_line, _) in &hunk_lines {
-            if let Some(lhs_line) = lhs_line {
-                print!(
-                    "{}   {}",
-                    apply_line_number_color(
-                        &format_line_num_padded(*lhs_line, lhs_line_nums_width),
-                        true,
-                        Side::Left,
-                        display_options,
-                    ),
-                    lhs_colored_lines[lhs_line.as_usize()]
-                );
-            }
-        }
-        for (_, rhs_line) in &hunk_lines {
-            if let Some(rhs_line) = rhs_line {
-                print!(
-                    "   {}{}",
-                    apply_line_number_color(
-                        &format_line_num_padded(*rhs_line, rhs_line_nums_width),
-                        true,
-                        Side::Right,
-                        display_options,
-                    ),
-                    rhs_colored_lines[rhs_line.as_usize()]
-                );
-            }
-        }
-
-        for (_, rhs_line) in &after_lines {
-            if let Some(rhs_line) = rhs_line {
-                print!(
-                    "   {}{}",
-                    apply_line_number_color(
-                        &format_line_num_padded(*rhs_line, rhs_line_nums_width),
-                        false,
-                        Side::Right,
-                        display_options,
-                    ),
-                    rhs_colored_lines[rhs_line.as_usize()]
-                );
-            }
-        }
         println!();
     }
+}
+
+fn print_before_lines(
+    display_options: &DisplayOptions,
+    lhs_colored_lines: &Vec<String>,
+    opposite_to_lhs: &hashbrown::HashMap<
+        LineNumber,
+        std::collections::HashSet<LineNumber, rustc_hash::FxBuildHasher>,
+        std::hash::BuildHasherDefault<rustc_hash::FxHasher>,
+    >,
+    opposite_to_rhs: &hashbrown::HashMap<
+        LineNumber,
+        std::collections::HashSet<LineNumber, rustc_hash::FxBuildHasher>,
+        std::hash::BuildHasherDefault<rustc_hash::FxHasher>,
+    >,
+    column_width: usize,
+    hunk_lines: &Vec<(Option<LineNumber>, Option<LineNumber>)>,
+    lhs_previous: &mut Option<usize>,
+    i: usize,
+    context: usize,
+) -> (usize, usize) {
+    let before_lines = calculate_before_context(
+        &hunk_lines[i..],
+        opposite_to_lhs,
+        opposite_to_rhs,
+        context - 1, // somehow this print 3 line before
+    );
+
+    let mut lhs_line: Option<LineNumber> = None;
+    let mut rhs_line: Option<LineNumber> = None;
+    for (lhs_ln, rhs_ln) in before_lines {
+        lhs_line = lhs_ln;
+        rhs_line = rhs_ln;
+        if let Some(lhs_line_nbr) = lhs_line {
+            *lhs_previous = Some(lhs_line_nbr.as_usize());
+            print!(
+                "{}{}",
+                apply_combine_line_number(lhs_line, rhs_line, false, column_width, display_options),
+                lhs_colored_lines[lhs_line_nbr.as_usize()]
+            );
+        }
+    }
+
+    (
+        lhs_line.unwrap_or(LineNumber::from(0)).as_usize(),
+        rhs_line.unwrap_or(LineNumber::from(0)).as_usize(),
+    )
+}
+
+fn apply_combine_line_number(
+    lhs_line: Option<LineNumber>,
+    rhs_line: Option<LineNumber>,
+    is_novel: bool,
+    column_width: usize,
+    display_options: &DisplayOptions,
+) -> String {
+    let lhs_line_nbr = if let Some(lhs_line) = lhs_line {
+        apply_line_number_color(
+            &format_line_num_padded(lhs_line, column_width),
+            is_novel,
+            Side::Left,
+            display_options,
+        )
+    } else {
+        format!("{:^width$}", " ", width = column_width)
+    };
+
+    let rhs_line_nbr = if let Some(rhs_line) = rhs_line {
+        apply_line_number_color(
+            &format_line_num_padded(rhs_line, column_width),
+            is_novel,
+            Side::Right,
+            display_options,
+        )
+    } else {
+        format!("{:^width$}", " ", width = column_width)
+    };
+
+    format!("{}{}", lhs_line_nbr, rhs_line_nbr)
 }
